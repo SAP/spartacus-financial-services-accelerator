4094a868996d5d0f6504c33921e21fa9b7d8e535
NC\iilic <i.ilic@msgnetconomy.net>
2018-12-13 11:48:32 +0100

    FSA-3395: Changes accordion logic

--- projects/fsastorefrontlib/src/lib/accordion/accordion.component.ts
+++ projects/fsastorefrontlib/src/lib/accordion/accordion.component.ts
@@ -10,24 +10,15 @@ export class AccordionComponent  implements AfterContentInit {
   @ContentChildren(PanelComponent) panels: QueryList<PanelComponent>;
 
   ngAfterContentInit() {
-    // Open the first panel
     this.panels.toArray()[0].opened = true;
-    // Loop through all panels
     this.panels.toArray().forEach((panel: PanelComponent) => {
-      // subscribe panel toggle event
       panel.toggle.subscribe(() => {
-        // Open the panel
         this.openPanel(panel);
       });
     });
   }
 
   openPanel(panel: PanelComponent) {
-    console.log(panel);
-    // close all panels
-    this.panels.toArray().forEach(p => p.opened = false);
-    // open the selected panel
-    panel.opened = true;
-    console.log(panel.opened);
+    panel.opened === true ? panel.opened = false : panel.opened = true;
   }
 }
