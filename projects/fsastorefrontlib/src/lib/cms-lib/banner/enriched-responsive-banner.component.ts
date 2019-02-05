f93d0a1cb09ccad7351599fec0d16d395823a980
kzivkovic <k.zivkovic@msgnetconomy.net>
2019-02-05 11:35:09 +0100

    FSA-3657: Added heading and styled text to enriched banner component

--- projects/fsastorefrontlib/src/lib/cms-lib/banner/enriched-responsive-banner.component.ts
+++ projects/fsastorefrontlib/src/lib/cms-lib/banner/enriched-responsive-banner.component.ts
@@ -1,12 +1,50 @@
-import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { ResponsiveBannerComponent } from '@spartacus/storefront';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CmsBannerComponent, CmsConfig } from '@spartacus/core';
+import { CmsComponentData, ResponsiveBannerComponent } from '@spartacus/storefront';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+export interface CmsEnrichedResponsiveBannerComponent extends CmsBannerComponent {
+  headingText?: string;
+  styledText?: string;
+  url?: string;
+}
 
 @Component({
-    selector: 'fsa-enriched-responsive-banner',
-    templateUrl: './enriched-responsive-banner.component.html',
-    styleUrls: ['./enriched-responsive-banner.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'fsa-enriched-responsive-banner',
+  templateUrl: './enriched-responsive-banner.component.html',
+  styleUrls: ['./enriched-responsive-banner.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class EnrichedResponsiveBannerComponent extends ResponsiveBannerComponent {
+  constructor(
+    public component: CmsComponentData<CmsEnrichedResponsiveBannerComponent>,
+    protected config: CmsConfig
+  ) {
+    super(component, config);
+  }
 
+  getHeadingText(): Observable<string> {
+    return this.component.data$.pipe(
+      map(data => {
+        return data.headingText;
+      })
+    );
+  }
+
+  getStyledText(): Observable<string> {
+    return this.component.data$.pipe(
+      map(data => {
+        return data.styledText;
+      })
+    );
+  }
+
+  getUrl(): Observable<string> {
+    return this.component.data$.pipe(
+      map(data => {
+        return data.urlLink;
+      })
+    );
+  }
 }
