476c6cbdf91d45a1c9ab91cfd4ddb2d24c19cd2c
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2019-02-22 14:14:51 +0100

    FSA-3642: Removed unecessary imports, refactored variable

--- projects/fsastorefrontlib/src/lib/checkout/assets/components/quote-review/quote-review.component.ts
+++ projects/fsastorefrontlib/src/lib/checkout/assets/components/quote-review/quote-review.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Subject, asapScheduler, pipe, of, from, interval, merge, fromEvent, Subscription } from 'rxjs';
+import { Observable } from 'rxjs';
 import { Cart, CartService, OccConfig } from '@spartacus/core';
 
 
@@ -32,7 +32,7 @@ export class QuoteReviewComponent implements OnInit {
   getProductInfo(info: string) {
     this.cartService.getActive().subscribe((data) => {
       this.product = data.deliveryOrderGroups[0].entries[0].product;
-      info === 'name' ?  this.targetData = this.product.categories[0].name : this.targetData = this.product.images[1].url;
+      this.targetData = info === 'name' ? this.product.categories[0].name : this.product.images[1].url;
     });
     return this.targetData;
   }
