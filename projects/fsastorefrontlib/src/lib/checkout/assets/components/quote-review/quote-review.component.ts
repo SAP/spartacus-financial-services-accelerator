1c3e6c362c697123cf035d46b84294e4a0ccca67
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2019-02-22 14:07:05 +0100

    FSA-3642: Shortend JSON traversing in HTML template

--- projects/fsastorefrontlib/src/lib/checkout/assets/components/quote-review/quote-review.component.ts
+++ projects/fsastorefrontlib/src/lib/checkout/assets/components/quote-review/quote-review.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Observable, Subject, asapScheduler, pipe, of, from, interval, merge, fromEvent, Subscription } from 'rxjs';
 import { Cart, CartService, OccConfig } from '@spartacus/core';
 
 
@@ -12,17 +12,28 @@ export class QuoteReviewComponent implements OnInit {
 
   cart$: Observable<Cart>;
   cartLoaded$: Observable<boolean>;
+  product: any;
+  targetData: string;
 
   constructor(
     protected cartService: CartService,
-    private config: OccConfig,
+    private config: OccConfig
     ) { }
 
   ngOnInit() {
     this.cart$ = this.cartService.getActive();
     this.cartLoaded$ = this.cartService.getLoaded();
   }
+
   public getBaseUrl() {
     return this.config.server.baseUrl || '';
+  }
+
+  getProductInfo(info: string) {
+    this.cartService.getActive().subscribe((data) => {
+      this.product = data.deliveryOrderGroups[0].entries[0].product;
+      info === 'name' ?  this.targetData = this.product.categories[0].name : this.targetData = this.product.images[1].url;
+    });
+    return this.targetData;
   }
 }
