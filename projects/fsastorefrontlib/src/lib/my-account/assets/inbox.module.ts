d9a9df749bf7269f9b4f8310a90fd6880afea137
kzivkovic <k.zivkovic@msgnetconomy.net>
2019-02-22 17:05:33 +0100

    FSA-3607: Adds import to inbox module

--- projects/fsastorefrontlib/src/lib/my-account/assets/inbox.module.ts
+++ projects/fsastorefrontlib/src/lib/my-account/assets/inbox.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { NgSelectModule } from '@ng-select/ng-select';
-import { ComponentsModule } from '@spartacus/storefront';
+import { ComponentsModule, CmsModule } from '@spartacus/storefront';
 import { InboxTabComponent } from '../../cms-lib/account/inbox/inbox-tab/inbox-tab.component';
 import { InboxComponent } from '../../cms-lib/account/inbox/inbox.component';
 import { OccInboxService } from '../../occ/inbox/inbox.service';
@@ -11,7 +11,7 @@ import { InboxDataService } from './services/inbox-data.service';
 import { InboxService } from './services/inbox.service';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, FormsModule, NgSelectModule, ComponentsModule],
+  imports: [CommonModule, RouterModule, FormsModule, NgSelectModule, ComponentsModule, CmsModule],
   declarations: [InboxComponent, InboxTabComponent],
   exports: [InboxComponent, InboxTabComponent],
   entryComponents: [InboxComponent, InboxTabComponent],
