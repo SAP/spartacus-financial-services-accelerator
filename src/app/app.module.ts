6a1c6459bb0bc7a91972ee024f84a5d010a74e60
b.jevtic <b.jevtic@msgnetconomy.net>
2019-02-27 15:44:54 +0100

    FSA-3685: routesConfig, cmscomponents and layout slots moved from appmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FSAStorefrontModule } from '@fsa/storefront';
-import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
 
+import { AppComponent } from './app.component';
+import { FSAStorefrontModule } from '@fsa/storefront';
 
 @NgModule({
   declarations: [
@@ -14,58 +14,12 @@ import { AppComponent } from './app.component';
       server: {
         baseUrl: 'https://financialservices.local:9002'
       },
-      routesConfig: {
-        translations: {
-          default: {
-            'comparisonTable': { paths: ['comparisonTable/:categoryCode'] },
-            'add-options': { paths: ['add-options'] }
-          }
-        }
-      },
       site: {
         baseSite: 'insurance'
       },
       authentication: {
         client_id: 'financial_customer',
         client_secret: 'secret'
-      },
-      cmsComponents: {
-        CMSInboxComponent: { selector: 'fsa-inbox' },
-        EnrichedResponsiveBannerComponent: { selector: 'fsa-enriched-responsive-banner' },
-        CMSViewPoliciesComponent: { selector: 'fsa-view-policies' },
-        CMSViewQuotesComponent: { selector: 'fsa-view-quotes' },
-        FinancialServicesProductFeatureComponent: { selector: 'fsa-product-feature' },
-        CMSMultiComparisonTabContainer: { selector: 'fsa-comparison-table-container' },
-        CMSComparisonTabComponent: { selector: 'fsa-comparison-table-tab' },
-        ComparisonPanelCMSComponent: { selector: 'fsa-comparison-table-panel' }
-      },
-      layoutSlots: {
-        InsuranceLandingPageTemplate: {
-          slots: [
-            'Section1',
-            'Section2A',
-            'Section2B',
-            'Section2C',
-            'Section3',
-            'Section4',
-            'Section5'
-          ]
-        },
-        FSCategoryPageTemplate: {
-          slots: [
-            'Section1',
-            'Section2A',
-            'Section2B',
-            'Section3',
-            'Section4'
-          ]
-        },
-        MultiTabsCategoryPageTemplate: {
-          slots: [
-            'Section1',
-            'Section2',
-          ]
-        }
       }
     })
   ],
