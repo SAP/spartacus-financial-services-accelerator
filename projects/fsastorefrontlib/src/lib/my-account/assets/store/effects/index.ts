330f0fd53473da5fca5ee7440cda0b170a4cd7d3
b.jevtic <b.jevtic@msgnetconomy.net>
2019-02-28 16:27:31 +0100

    FSA-3685: Merge conflict resolved v2

--- projects/fsastorefrontlib/src/lib/my-account/assets/store/effects/index.ts
+++ projects/fsastorefrontlib/src/lib/my-account/assets/store/effects/index.ts
@@ -2,10 +2,9 @@ import {ClaimEffects} from './claim.effect';
 import { QuoteEffects } from './quote.effect';
 import { PolicyEffects } from './policy.effect';
 import { InboxEffects } from './inbox.effect';
-
-export const effects: any[] = [QuoteEffects, PolicyEffects, ClaimEffects, InboxEffects];
 import { FSComponentEffects } from './fs-component.effect';
-export const effects: any[] = [QuoteEffects, PolicyEffects, ClaimEffects, FSComponentEffects];
+
+export const effects: any[] = [QuoteEffects, PolicyEffects, ClaimEffects, InboxEffects, FSComponentEffects];
 
 export * from './quote.effect';
 export * from './policy.effect';
