1542783a60a9bb25233ef1ee06fd5a4a14fea8aa
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2019-03-07 13:42:43 +0100

    FSA-3643: Added spinner

---
+++ projects/fsastorefrontlib/src/lib/cms-lib/comparison-table/comparison-table.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class ComparisonTableService {
+  billingEvents = [
+    {
+      code: 'sfsfsdf',
+      name: 'dfsdf'
+    }
+  ];
+  constructor() { }
+}
