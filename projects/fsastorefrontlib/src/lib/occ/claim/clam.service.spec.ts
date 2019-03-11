b98397175aeabf4dad69559bd0cbbedc72ebfefe
Nikola Radulovic <n.radulovic@msgnetconomy.net>
2019-03-11 15:24:51 +0100

    FSA-3730: Adds tests for occ services (policies, claims and quotes)

---
+++ projects/fsastorefrontlib/src/lib/occ/claim/clam.service.spec.ts
@@ -0,0 +1,58 @@
+import { HttpClientModule, HttpRequest } from '@angular/common/http';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
+import { async, TestBed } from '@angular/core/testing';
+import { OccClaimService } from './claim.service';
+import { OccConfig } from '@spartacus/core';
+
+const userId = '123';
+
+const usersEndpoint = '/users';
+const claimsEndpoint = '/claims';
+
+const MockOccModuleConfig: OccConfig = {
+  server: {
+    baseUrl: '',
+    occPrefix: ''
+  },
+
+  site: {
+    baseSite: ''
+  }
+};
+
+describe('OccClaimsService', () => {
+  let service: OccClaimService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule, HttpClientTestingModule],
+      providers: [
+        OccClaimService,
+        { provide: OccConfig, useValue: MockOccModuleConfig }
+      ]
+    });
+
+    service = TestBed.get(OccClaimService);
+    httpMock = TestBed.get(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
+  });
+
+  describe('getClaims', () => {
+    it('should fetch user Claims', async(() => {
+      service.getClaims(userId).subscribe();
+      httpMock.expectOne((req: HttpRequest<any>) => {
+        return (
+          req.url === usersEndpoint + `/${userId}` + claimsEndpoint &&
+          req.method === 'GET'
+        );
+      }, `GET method and url`);
+    }));
+  });
+});
