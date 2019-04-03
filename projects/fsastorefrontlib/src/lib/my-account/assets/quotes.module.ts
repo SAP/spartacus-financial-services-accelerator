99d1bfa8df531e041cfad4df3946c2ca3059672d
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2019-04-03 15:49:22 +0200

    FSA-3833: Working with jsp workaround

--- projects/fsastorefrontlib/src/lib/my-account/assets/quotes.module.ts
+++ projects/fsastorefrontlib/src/lib/my-account/assets/quotes.module.ts
@@ -31,7 +31,7 @@ const routes: Routes = [
     RouterModule.forChild(routes),
     ConfigModule.withConfig(<CmsConfig>{
       cmsComponents: {
-        InsuranceJspIncludeComponent: { selector: 'fsa-quotes' },
+        AccountMyQuotesSPAComponent: { selector: 'fsa-quotes' },
       }
     })
   ],
