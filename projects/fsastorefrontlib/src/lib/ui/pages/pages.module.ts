42f0488592a80806e469ed2544ee68e6b648235e
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2019-04-05 16:53:27 +0200

    FSA-3833: Deleted add-options layouts

--- projects/fsastorefrontlib/src/lib/ui/pages/pages.module.ts
+++ projects/fsastorefrontlib/src/lib/ui/pages/pages.module.ts
@@ -3,13 +3,11 @@ import { CommonModule } from '@angular/common';
 // ContentPage
 import { FSCategoryPageModule } from './fs-category-page/fs-category-page.module';
 import { QuotesReviewPageModule } from './quotes-review-page/quotes-review-page.module';
-import { AddOptionsPageModule } from './add-options-page/add-options-page.module';
 import { ComparisonTablePageModule } from './comparison-table-page/comparison-table-page.module';
 import { CheckoutPageModule } from './checkout-page/checkout-page.module';
 
 const pageModules = [
   FSCategoryPageModule,
-  AddOptionsPageModule,
   QuotesReviewPageModule,
   ComparisonTablePageModule,
   CheckoutPageModule
