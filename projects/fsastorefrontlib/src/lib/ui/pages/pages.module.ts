00438c492a7b4e6f48eae98c53481ff634c89fb9
Nikola Radulovic <n.radulovic@msgnetconomy.net>
2019-04-08 10:44:13 +0200

    FSA-3833: Removes comparison table and quote review page layouts

--- projects/fsastorefrontlib/src/lib/ui/pages/pages.module.ts
+++ projects/fsastorefrontlib/src/lib/ui/pages/pages.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 // ContentPage
 import { FSCategoryPageModule } from './fs-category-page/fs-category-page.module';
-import { QuotesReviewPageModule } from './quotes-review-page/quotes-review-page.module';
-import { ComparisonTablePageModule } from './comparison-table-page/comparison-table-page.module';
 import { CheckoutPageModule } from './checkout-page/checkout-page.module';
 
 const pageModules = [
   FSCategoryPageModule,
-  QuotesReviewPageModule,
-  ComparisonTablePageModule,
   CheckoutPageModule
 ];
 
