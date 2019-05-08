f91ec0acce8030a1745a24f3b2eda660bb8f4b88
NC\iilic <i.ilic@msgnetconomy.net>
2019-05-08 15:57:55 +0200

    FSA-3889: Adds translation module

--- projects/fsastorefrontlib/src/lib/cms-lib/account/account.module.ts
+++ projects/fsastorefrontlib/src/lib/cms-lib/account/account.module.ts
@@ -7,12 +7,16 @@ import { CmsModule, ComponentsModule } from '@spartacus/storefront';
 import { effects } from '../../my-account/assets/store';
 import { CMSViewPoliciesComponent } from './view-policies/view-policies.component';
 import { CMSViewQuotesComponent } from './view-quotes/view-quotes.component';
+import {
+  I18nModule
+} from '@spartacus/core';
 
 @NgModule({
   imports: [
     CommonModule,
     CmsModule,
     RouterModule,
+    I18nModule,
     EffectsModule.forFeature(effects),
     ComponentsModule,
     ConfigModule.withConfig(<CmsConfig>{
