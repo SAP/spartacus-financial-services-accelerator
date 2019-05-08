e5adb1d542b8f5817e42cc8e367481f2d5b2100b
NC\iilic <i.ilic@msgnetconomy.net>
2019-05-08 14:54:47 +0200

    FSA-3889: Adds localization files

---
+++ projects/fsastorefrontlib/src/translations/index.ts
@@ -0,0 +1,8 @@
+import { TranslationResources } from '@spartacus/core';
+import { common } from './en/common.en';
+
+export const translations: TranslationResources = {
+  en: {
+    common
+  },
+};
