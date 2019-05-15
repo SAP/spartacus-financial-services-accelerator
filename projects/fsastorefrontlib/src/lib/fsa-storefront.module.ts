cdfd4f61c2f88ebc99e9edee9dd4044ef4d1b947
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2019-05-15 10:51:12 +0200

    FSA-3937: All items in the Header visible and working, styling left

--- projects/fsastorefrontlib/src/lib/fsa-storefront.module.ts
+++ projects/fsastorefrontlib/src/lib/fsa-storefront.module.ts
@@ -1,6 +1,7 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { provideConfig, ConfigModule } from '@spartacus/core';
 
+
 import { MyAccountModule } from './my-account/index';
 import { StorefrontModuleConfig, StorefrontModule, PageComponentModule } from '@spartacus/storefront';
 import { UiModule } from './ui/index';
@@ -13,7 +14,51 @@ import { CmsLibModule } from './cms-lib/cms-lib.module';
     CmsLibModule,
     MyAccountModule,
     StorefrontModule,
-    ConfigModule.forRoot()
+    ConfigModule.forRoot(),
+    ConfigModule.withConfig({
+      layoutSlots: {
+        header: {
+          md: {
+            slots: [
+              'SiteLogo',
+              'SearchBox',
+              'HeaderLinksSlot',
+              'MiniCart',
+              'NavigationBar'
+            ],
+          },
+          xs: {
+            slots: ['PreHeader', 'SiteLogo', 'SearchBox'],
+          },
+        },
+        navigation: {
+          xs: {
+            slots: ['HeaderLinksSlot', 'NavigationBar'],
+          },
+        },
+      },
+      cmsStructure: {
+        components: {
+          HamburgerMenuComponent: {
+            typeCode: 'HamburgerMenuComponent',
+            flexType: 'HamburgerMenuComponent',
+          },
+          LoginComponent: {
+            typeCode: 'LoginComponent',
+            flexType: 'LoginComponent',
+            uid: 'LoginComponent'
+          }
+        },
+        slots: {
+          PreHeader: {
+            componentIds: ['HamburgerMenuComponent']
+          },
+          HeaderLinksSlot: {
+            componentIds: ['LoginComponent']
+          }
+        }
+      }
+    })
   ],
   exports: [
     StorefrontModule,
