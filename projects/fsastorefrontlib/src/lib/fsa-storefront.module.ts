18b3a8166fc67dd3ab1da2be54b21da12f2fec77
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2019-05-21 15:26:05 +0200

    FSA-3937: Header fixed on test system

--- projects/fsastorefrontlib/src/lib/fsa-storefront.module.ts
+++ projects/fsastorefrontlib/src/lib/fsa-storefront.module.ts
@@ -7,45 +7,90 @@ import { MyAccountModule } from './my-account/index';
 import { UiModule } from './ui/index';
 import { CmsLibModule } from './cms-lib/cms-lib.module';
 import { fstranslations } from '../translations';
-import { fsaLayoutConfig, fsaCmsStructure } from './default-fsa.config';
+
+// import { fsaLayoutConfig, fsaCmsStructure } from './default-fsa.config';
 
 @NgModule({
-  imports: [
-    UiModule,
-    PageComponentModule,
-    CmsLibModule,
-    MyAccountModule,
-    StorefrontModule,
-    ConfigModule.forRoot(),
-    ConfigModule.withConfig({
-       i18n: {
-        resources: {
-          en: translations.en
+ imports: [
+   UiModule,
+   PageComponentModule,
+   CmsLibModule,
+   MyAccountModule,
+   StorefrontModule,
+   ConfigModule.forRoot(),
+   ConfigModule.withConfig({
+      i18n: {
+       resources: {
+         en: translations.en
+       }
+     }
+   }),
+   ConfigModule.withConfig({
+     i18n: {
+       resources: {
+         en: fstranslations.en
+        }
+     }
+   }),
+   ConfigModule.withConfig({
+    breakpoints: {
+      lg: 1400
+    },
+    layoutSlots: {
+      header: {
+        md: {
+          slots: [
+            'SiteLogo',
+            'SearchBox',
+            'HeaderLinksSlot',
+            'NavigationBar'
+          ],
+        },
+        xs: {
+          slots: ['PreHeader', 'SiteLogo', 'SearchBox'],
+        },
+      },
+      navigation: {
+        xs: {
+          slots: ['HeaderLinksSlot', 'NavigationBar'],
+        },
+      },
+    },
+    cmsStructure: {
+      components: {
+        HamburgerMenuComponent: {
+          typeCode: 'HamburgerMenuComponent',
+          flexType: 'HamburgerMenuComponent',
+        },
+        LoginComponent: {
+          typeCode: 'LoginComponent',
+          flexType: 'LoginComponent',
+          uid: 'LoginComponent'
         }
       },
-      ...fsaLayoutConfig,
-      ...fsaCmsStructure
-    }),
-    ConfigModule.withConfig({
-      i18n: {
-        resources: {
-          en: fstranslations.en
-         }
+      slots: {
+        PreHeader: {
+          componentIds: ['HamburgerMenuComponent']
+        },
+        HeaderLinksSlot: {
+          componentIds: ['LoginComponent']
+        }
       }
-    })
-  ],
-  exports: [
-    StorefrontModule,
-    MyAccountModule,
-    CmsLibModule
-  ],
-  declarations: []
+    }
+  })
+ ],
+ exports: [
+   StorefrontModule,
+   MyAccountModule,
+   CmsLibModule
+ ],
+ declarations: []
 })
 export class FSAStorefrontModule {
-  static withConfig(config?: StorefrontModuleConfig): ModuleWithProviders {
-    return {
-      ngModule: FSAStorefrontModule,
-      providers: [provideConfig(config)]
-    };
-  }
+ static withConfig(config?: StorefrontModuleConfig): ModuleWithProviders {
+   return {
+     ngModule: FSAStorefrontModule,
+     providers: [provideConfig(config)]
+   };
+ }
 }
