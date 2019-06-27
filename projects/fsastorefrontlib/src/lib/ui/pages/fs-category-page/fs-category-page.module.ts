5faf67f86ddcea97c7baf31571c5178d93ca1a5d
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2019-06-27 15:04:04 +0200

    FSA-4055: replaced imagecontainer with container, deleted some commented code

--- projects/fsastorefrontlib/src/lib/ui/pages/fs-category-page/fs-category-page.module.ts
+++ projects/fsastorefrontlib/src/lib/ui/pages/fs-category-page/fs-category-page.module.ts
@@ -1,48 +1,38 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import {  CmsPageGuard, PageLayoutModule, PageLayoutComponent } from '@spartacus/storefront';
-import { RoutesConfig, RoutingConfig, ConfigModule } from '@spartacus/core';
+import {  CmsPageGuard, PageLayoutModule, PageLayoutComponent, BannerComponent } from '@spartacus/storefront';
+import { RoutesConfig, RoutingConfig, ConfigModule, CmsConfig } from '@spartacus/core';
 
-// const routes: Routes = [
+const routes: Routes = [
 //   {
 //     path: null,
 //     canActivate: [CmsPageGuard],
 //     component: PageLayoutComponent,
-//     data: { cxRoute: 'category' }
+//     data: { cxPath: 'category' }
 //   },
 //   {
 //     path: 'Insurance-Products/:categoryTitle/c/:categoryCode',
-//     redirectTo: 'category',
+//     redirectTo: null,
 //     data: { cxRedirectTo: 'category' }
 //   },
 //   {
 //     path: 'Banking-Products/:categoryTitle/c/:categoryCode',
-//     redirectTo: 'category',
+//     redirectTo: null,
 //     data: { cxRedirectTo: 'category' }
 //   },
 //   {
 //     path: 'c/:categoryCode',
 //     canActivate: [CmsPageGuard],
-//     redirectTo: 'category',
+//     redirectTo: null,
 //     data: { cxRedirectTo: 'checkout/' }
 //   }
-// ];
+];
 
 @NgModule({
   imports: [
     CommonModule,
     // RouterModule.forChild(routes),
-    // ConfigModule.withConfig({
-    //   routing: {
-    //     routes: {
-    //       category: {
-    //         paths: [null],
-    //         paramsMapping: { categoryCode: 'code' },
-    //       }
-    //     }
-    //   }
-    // }),
     PageLayoutModule
   ]
 })
