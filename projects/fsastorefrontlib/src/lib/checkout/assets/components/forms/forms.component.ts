c0c8a3da9534f4441d1a47f88be4c67c968252f8
NC\iilic <i.ilic@msgnetconomy.net>
2019-07-12 10:48:22 +0200

    yforms : adds logic for form object creation

--- projects/fsastorefrontlib/src/lib/checkout/assets/components/forms/forms.component.ts
+++ projects/fsastorefrontlib/src/lib/checkout/assets/components/forms/forms.component.ts
@@ -8,14 +8,23 @@ export interface ObjectLiteral {
   styleUrls: ['./forms.component.scss']
 })
 export class FormsComponent {
+  
   constructor() { }
-  private productPriceDescriptors: Array<ObjectLiteral>
-
-   ngOnInit() {
-   }
+  formDataArray: any = [];
+  test = {
+    "productPriceDescriptors": this.formDataArray
+   };
+   
    onClickSubmit(formData) {
-    this.productPriceDescriptors = formData.map(item => {
-      console.log(item);
-    })
+    Object.entries(formData).forEach(entry => {
+      let key = entry[0];
+      let value = entry[1];
+      let obj = {
+        "key" : key,
+        "value" : value
+      }
+      this.formDataArray.push(obj);
+    });
+    console.log(this.test);
    }
 }
