b49738554f8677a3273625988f7f99dada5bbc97
Nikola Radulovic <n.radulovic@msgnetconomy.net>
2019-07-16 12:09:16 +0200

    FSA-4061: Fixes linting issues

--- projects/fsastorefrontlib/src/lib/my-account/assets/components/policy-details/policy-details.component.spec.ts
+++ projects/fsastorefrontlib/src/lib/my-account/assets/components/policy-details/policy-details.component.spec.ts
@@ -57,7 +57,7 @@ describe('PolicyDetailsComponent', () => {
         declarations: [
           PolicyDetailsComponent
         ]
-      }).compileComponents();  
+      }).compileComponents();
     }));
 
     beforeEach(() => {
