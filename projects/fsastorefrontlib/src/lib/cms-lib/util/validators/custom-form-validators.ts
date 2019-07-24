adb12d379dbe985fd1163e2325080790a372f06b
Divna Popovic <d.popovic@msgnetconomy.net>
2019-07-24 14:11:34 +0200

    FSA-4106: Validation of registration form refactored to be more generic
    Methods for validation of fields refactored in order to be more generic and reusable for other checkout forms that will be implemented in angular

--- projects/fsastorefrontlib/src/lib/cms-lib/util/validators/custom-form-validators.ts
+++ projects/fsastorefrontlib/src/lib/cms-lib/util/validators/custom-form-validators.ts
@@ -1,33 +1,32 @@
 import { AbstractControl, ValidationErrors } from '@angular/forms';
 
 export class CustomFormValidators {
-    static passwordValidator(control: AbstractControl): ValidationErrors | null {
-        const password = control.value as string;
-        return password.match( /^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[!@#$%^*()_\-+{};:.,]).{6,}$/)
-          ? null
-          : { InvalidPassword: true };
-      }
+    static passwordRegex = /^(?=.*?[A-Z])(?=.*?[0-9])(?=.*?[!@#$%^*()_\-+{};:.,]).{6,}$/;
+    // tslint:disable-next-line:max-line-length
+    static emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    static emailValidator(control: AbstractControl): ValidationErrors | null {
-        const email = control.value as string;
-        return email.match(
-            // Email Standard RFC 5322:
-            /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/ // tslint:disable-line
-        )
-            ? null
-            : { InvalidEmail: true };
+    static regexValidator(formattRegex) {
+        return (control: AbstractControl): ValidationErrors | null => {
+            const date = control.value as string;
+            return date.match(formattRegex) ? null : { InvalidFormat: true };
+        };
     }
 
-    static dateFormatValidator(control: AbstractControl): ValidationErrors | null {
-        const date = control.value as string;
-        return date.match(/^\\d{4}-\\d{2}-\\d{2}$/) ? null : { InvalidDate: true };
+    static matchFields(controlName: string, controlName2: string) {
+        return (control: AbstractControl): { NotEqual: boolean } => {
+            if (control.get(controlName).value !== control.get(controlName2).value) {
+                return { NotEqual: true };
+            }
+        };
     }
 
-    static dateOfBirthValidator(control: AbstractControl): ValidationErrors | null {
-        const dateString = control.value as string;
-        const userAge = new Date(dateString);
-        const today = new Date();
-        const age = new Date(today.getFullYear() - 18, today.getMonth(), today.getDate());
-        return userAge < age ? null : { InvalidDate: true };
+    static dateOfBirthValidator(minAge: number) {
+        return (control: AbstractControl): ValidationErrors | null => {
+            const dateString = control.value as string;
+            const userAge = new Date(dateString);
+            const today = new Date();
+            const age = new Date(today.getFullYear() - minAge, today.getMonth(), today.getDate());
+            return userAge < age ? null : { InvalidDate: true };
+        };
     }
 }
