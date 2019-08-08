17c40ee2c60ce1ab6793ef7ec3d3379b9b5dc63e
Bojan Jevtic <b.jevtic@msgnetconomy.net>
2019-08-08 12:09:26 +0200

    FSA-4157: Update profile redirection

--- projects/fsastorefrontlib/src/lib/my-account/assets/components/update-profile/fs-update-profile.component.ts
+++ projects/fsastorefrontlib/src/lib/my-account/assets/components/update-profile/fs-update-profile.component.ts
@@ -1,11 +1,40 @@
 import { Component } from '@angular/core';
 import { UpdateProfileComponent } from '@spartacus/storefront';
 import { FSUser } from 'projects/fsastorefrontlib/src/lib/occ-models';
-import { Observable } from 'rxjs';
+import { Observable, Subscription } from 'rxjs';
+import { GlobalMessageService, UserService, RoutingService, GlobalMessageType, User } from '@spartacus/core';
 
 @Component({
   templateUrl: './fs-update-profile.component.html'
 })
 export class FSUpdateProfileComponent extends UpdateProfileComponent {
+
   user$: Observable<FSUser>;
+  private fsSubscription = new Subscription();
+
+  constructor(
+    private fsRoutingService: RoutingService,
+    private fsUserService: UserService,
+    private fsGlobalMessageService: GlobalMessageService
+  ) {
+    super(fsRoutingService, fsUserService, fsGlobalMessageService);
+  }
+
+  onSuccess(success: boolean): void {
+    if (success) {
+      this.fsGlobalMessageService.add(
+        { key: 'updateProfileForm.profileUpdateSuccess' },
+        GlobalMessageType.MSG_TYPE_CONFIRMATION
+      );
+    }
+  }
+
+  onSubmit({ userUpdates }: { userUpdates: User }): void {
+    super.onSubmit({userUpdates});
+    this.fsSubscription.add(
+      this.fsUserService
+        .getUpdatePersonalDetailsResultSuccess()
+        .subscribe(success => this.onSuccess(success))
+    );
+  }
 }
