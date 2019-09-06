47ec641b1f1a597044073620d1b195e4be22581a
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2019-09-06 12:24:16 +0200

    FSA-4230: yet another small refactor

--- projects/fsastorefrontlib/src/lib/checkout/assets/components/forms/dynamic-form/models/field-config.interface.ts
+++ projects/fsastorefrontlib/src/lib/checkout/assets/components/forms/dynamic-form/models/field-config.interface.ts
@@ -14,7 +14,7 @@ export interface FieldConfig {
   disabled?: boolean;
   label?: string;
   name?: string;
-  options?: any[];
+  options?: string[];
   placeholder?: string;
   type: string;
   validation?: ValidatorFn[];
