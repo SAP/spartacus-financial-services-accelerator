dc9d6cdae8eb0dc0630691a001fe4a407d955f42
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2019-09-06 17:18:33 +0200

    FSA-4230: Removed excess component

--- projects/fsastorefrontlib/src/lib/checkout/assets/components/forms/dynamic-form/components/form-select-optional/form-select-optional.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-import { FormGenericComponent } from '../form-generic.component';
-
-@Component({
-  selector: 'fsa-form-select-optional',
-  templateUrl: './form-select-optional.component.html'
-
-})
-export class FormSelectOptionalComponent extends FormGenericComponent { }
