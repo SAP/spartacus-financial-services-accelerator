b91a74371169e93b4f86a61e5520355eab4b3162
NC\iilic <i.ilic@msgnetconomy.net>
2019-09-11 11:45:50 +0200

    FSA-4244: Removes unnecessary imports

--- projects/fsastorefrontlib/src/lib/checkout/assets/components/user-identification/user-identification.module.ts
+++ projects/fsastorefrontlib/src/lib/checkout/assets/components/user-identification/user-identification.module.ts
@@ -1,21 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { NgSelectModule } from '@ng-select/ng-select';
 import { CmsConfig, ConfigModule, I18nModule, RoutesConfig, RoutingConfig } from '@spartacus/core';
-import { MediaModule, SpinnerModule } from '@spartacus/storefront';
 import { SelectIdentificationComponent } from './select-identification/select-identification.component';
 
 @NgModule({
     imports: [
         CommonModule,
-        FormsModule,
         I18nModule,
         RouterModule,
-        NgSelectModule,
-        SpinnerModule,
-        MediaModule,
         ConfigModule.withConfig(<CmsConfig | RoutesConfig | RoutingConfig>{
             cmsComponents: {
                 UserIdentificationFlex: {
