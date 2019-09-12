717c5afce01bb56a55157bcc92f64ac91c135954
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2019-09-12 14:29:31 +0200

    FSA-4230:  Fixed grammar issues

--- projects/fsastorefrontlib/src/translations/en/forms.en.ts
+++ projects/fsastorefrontlib/src/translations/en/forms.en.ts
@@ -1,16 +1,16 @@
 export const forms = {
-    forms: {
-        enterValidValue: 'Please enter valid value!',
-        dateInPast: 'Date must be in past!',
-        dateInFuture: 'Date must be in future!',
-        lessThan100K: 'Value must be less than 100.000',
-        dateOfBirthMinimumAge: 'Must be over 18 years old',
-        lessThan150: 'Value must be less than 150',
-        enterValidEmail: 'Please enter a valid email',
-        vehicleValue: 'Vehicle value must be a number between 3.000 and 1.000.000.',
-        dateOfBirth: {
-            label: 'Date of Birth'
-        }
-
+  forms: {
+    enterValidValue: 'Please enter valid value',
+    dateInPast: 'Date must be in the past',
+    dateInFuture: 'Date must be in the future',
+    lessThan100K: 'Value must be less than 100.000',
+    dateOfBirthMinimumAge: 'Must be over 18 years old',
+    lessThan150: 'Value must be less than 150',
+    enterValidEmail: 'Please enter a valid email',
+    vehicleValue: 'Vehicle value must be a number between 3.000 and 1.000.000',
+    dateOfBirth: {
+      label: 'Date of Birth'
     }
+
+  }
 };
