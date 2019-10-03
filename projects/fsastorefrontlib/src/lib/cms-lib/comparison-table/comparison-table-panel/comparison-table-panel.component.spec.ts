08b347bfc50be2d8669c87cb61a2f6f541c000be
Nikola Radulovic <nikola.radulovic92@gmail.com>
2019-10-03 13:39:02 +0200

    NO-TASK: Adds initial test for comparison panel

---
+++ projects/fsastorefrontlib/src/lib/cms-lib/comparison-table/comparison-table-panel/comparison-table-panel.component.spec.ts
@@ -0,0 +1,68 @@
+import { Input, Component } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CmsComponent } from '@spartacus/core';
+import { of } from 'rxjs';
+import { CmsComponentData } from '@spartacus/storefront';
+import { ComparisonPanelCMSComponent } from 'fsastorefrontlib/lib/occ-models';
+import { ComparisonTablePanelComponent } from './comparison-table-panel.component';
+import { NgbTooltipModule } from '@ng-bootstrap/ng-bootstrap';
+import { OccBillingTimeService } from './../../../occ/billing-time/billing-time.service';
+
+
+@Component({
+    // tslint:disable
+    selector: 'fsa-comparison-table-panel-item',
+    template: '' 
+})
+export class MockComparisonTablePanelItem {
+    @Input() productCode: string;
+    @Input() billingTimes: any;
+}
+
+class MockOccBillingTimeService {
+
+}
+
+describe('ComparisonTablePanelComponent', () => {
+  let comparisonTablePanel: ComparisonTablePanelComponent;
+  let fixture: ComponentFixture<ComparisonTablePanelComponent>;
+
+  const componentData: ComparisonPanelCMSComponent = {
+    uid: 'TestComparisonTablePanelComponent',
+    typeCode: 'ComparisonPanelCMSComponent'
+  };
+
+  const MockCmsComponentData = <CmsComponentData<CmsComponent>>{
+    data$: of(componentData),
+    uid: 'testComponent',
+  };
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [NgbTooltipModule],
+      declarations: [
+        ComparisonTablePanelComponent,
+        MockComparisonTablePanelItem
+      ],
+      providers: [
+        {
+            provide: CmsComponentData,
+            useValue: MockCmsComponentData,
+        },
+        {
+            provide: OccBillingTimeService,
+            useValue: MockOccBillingTimeService,
+        }
+      ],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ComparisonTablePanelComponent);
+    comparisonTablePanel = fixture.componentInstance;
+  });
+
+  it('should create comparison panel component', () => {
+    expect(comparisonTablePanel).toBeTruthy();
+  });
+});
---
+++ projects/fsastorefrontlib/src/lib/cms-lib/comparison-table/comparison-table-tab/comparison-table-tab.component.spec.ts
@@ -0,0 +1,56 @@
+import { Directive, Input } from '@angular/core';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { CmsComponent, ContentSlotComponentData } from '@spartacus/core';
+import { of } from 'rxjs';
+import { CmsComponentData } from '@spartacus/storefront';
+import { CMSComparisonTabComponent } from '../../../occ-models';
+import { ComparisonTableTabComponent } from './comparison-table-tab.component';
+
+
+@Directive({
+    // tslint:disable
+    selector: '[cxComponentWrapper]',
+})
+export class MockComponentWrapperDirective {
+    @Input() cxComponentWrapper: ContentSlotComponentData;
+}
+
+describe('ComparisonTableContainerComponent', () => {
+  let comaparisonTableTabComponent: ComparisonTableTabComponent;
+  let fixture: ComponentFixture<ComparisonTableTabComponent>;
+
+  const componentData: CMSComparisonTabComponent = {
+    uid: 'TestComparisonTabComponent',
+    typeCode: 'CMSComparisonTabComponent'
+  };
+
+  const MockCmsComponentData = <CmsComponentData<CmsComponent>>{
+    data$: of(componentData),
+    uid: 'testComponent',
+  };
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      imports: [],
+      declarations: [
+        ComparisonTableTabComponent,
+        MockComponentWrapperDirective
+      ],
+      providers: [
+        {
+            provide: CmsComponentData,
+            useValue: MockCmsComponentData,
+        }
+      ],
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ComparisonTableTabComponent);
+    comaparisonTableTabComponent = fixture.componentInstance;
+  });
+
+  it('should create comparison tab component', () => {
+    expect(comaparisonTableTabComponent).toBeTruthy();
+  });
+});
