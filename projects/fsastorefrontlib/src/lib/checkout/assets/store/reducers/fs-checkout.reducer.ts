47abe2cc8d409ea1ba0184c237b7db708f63618c
Ksenija Zivkovic <k.zivkovic@msgnetconomy.net>
2019-10-08 08:38:39 +0200

    FSA-4368: Fixed using existing payment.

--- projects/fsastorefrontlib/src/lib/checkout/assets/store/reducers/fs-checkout.reducer.ts
+++ projects/fsastorefrontlib/src/lib/checkout/assets/store/reducers/fs-checkout.reducer.ts
@@ -1,15 +1,26 @@
-import { FSCheckoutStepsState } from '../fs-checkout-state';
-import { FSCheckoutAction } from '../actions/fs-checkout.action';
+import { Address, CheckoutActions, DeliveryMode, Order } from '@spartacus/core';
 import * as fromAction from '../actions';
+import { FSCheckoutAction } from '../actions/fs-checkout.action';
+import { FSCheckoutStepsState } from '../fs-checkout-state';
 
 export const initialState: FSCheckoutStepsState = {
   legalInformation: false,
   identificationType: false,
+  address: {},
+  deliveryMode: {
+    supported: {},
+    selected: '',
+  },
+  paymentDetails: {},
+  orderDetails: {},
 };
 
 export function reducer(
   state = initialState,
-  action: FSCheckoutAction
+  action:
+    | FSCheckoutAction
+    | CheckoutActions.CheckoutAction
+    | CheckoutActions.CheckoutClearMiscsData
 ): FSCheckoutStepsState {
   switch (action.type) {
     case fromAction.SET_IDENTIFICATION_TYPE_SUCCESS: {
@@ -24,6 +35,48 @@ export function reducer(
       return {
         ...state,
         identificationType,
+      };
+    }
+
+    case CheckoutActions.CREATE_PAYMENT_DETAILS_SUCCESS:
+    case CheckoutActions.SET_PAYMENT_DETAILS_SUCCESS: {
+      return {
+        ...state,
+        paymentDetails: action.payload,
+      };
+    }
+
+    case CheckoutActions.CREATE_PAYMENT_DETAILS_FAIL: {
+      const paymentDetails = action.payload;
+      if (paymentDetails['hasError']) {
+        return {
+          ...state,
+          paymentDetails,
+        };
+      }
+
+      return state;
+    }
+
+    case CheckoutActions.PLACE_ORDER_SUCCESS: {
+      const orderDetails: Order = action.payload;
+
+      return {
+        ...state,
+        orderDetails,
+      };
+    }
+
+    case CheckoutActions.LOAD_CHECKOUT_DETAILS_SUCCESS: {
+      return {
+        ...state,
+        address: action.payload.deliveryAddress,
+        deliveryMode: {
+          ...state.deliveryMode,
+          selected:
+            action.payload.deliveryMode && action.payload.deliveryMode.code,
+        },
+        paymentDetails: action.payload.paymentInfo,
       };
     }
   }
