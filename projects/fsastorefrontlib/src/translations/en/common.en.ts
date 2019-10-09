2213108893aae4243a0f3bcb02c4edb5f3ef870e
NC\iilic <i.ilic@msgnetconomy.net>
2019-10-09 19:12:21 +0200

    FSA-4351: Minor css changes to header, changes "Quotes" title on same page to "Applications"

--- projects/fsastorefrontlib/src/translations/en/common.en.ts
+++ projects/fsastorefrontlib/src/translations/en/common.en.ts
@@ -70,6 +70,7 @@ export const fscommon = {
     whatDoesItCostMe: 'What Does It Cost Me?',
     legalInformation: 'Legal Information',
     listView: 'List View',
+    applications: 'Applications',
     mapView: 'Map View',
     register: {
       dateOfBirthMinimumAge: 'Must be over 18 years old',
