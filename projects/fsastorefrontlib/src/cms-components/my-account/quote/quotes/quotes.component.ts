e78f85b7550e3cbe201e7d709ae0488626efd4cb
Nikola Radulovic <n.radulovic@msgnetconomy.net>
2019-11-01 13:07:12 +0100

    NO-TASK: Retrieve quote

--- projects/fsastorefrontlib/src/cms-components/my-account/quote/quotes/quotes.component.ts
+++ projects/fsastorefrontlib/src/cms-components/my-account/quote/quotes/quotes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import * as fromQuoteStore from '../../../../core/my-account/store';
 import { Store, select } from '@ngrx/store';
-import { OccConfig } from '@spartacus/core';
+import { OccConfig, RoutingService } from '@spartacus/core';
 import { QuoteService } from '../../../../core/my-account/services/quote.service';
 
 @Component({
@@ -13,7 +13,8 @@ export class QuotesComponent implements OnInit {
   constructor(
     private store: Store<fromQuoteStore.UserState>,
     private config: OccConfig,
-    protected quoteService: QuoteService
+    protected quoteService: QuoteService,
+    protected routingService: RoutingService
   ) {}
 
   quotes$;
@@ -29,5 +30,12 @@ export class QuotesComponent implements OnInit {
 
   public getBaseUrl() {
     return this.config.backend.occ.baseUrl || '';
+  }
+
+  retrieveQuote(quote: any) {
+    this.quoteService.retrieveQuote(quote);
+    this.routingService.go({
+      cxRoute: 'addOptions',
+    });
   }
 }
