fc656e4333268a10b766c606cc3acf28c6306ce1
Nikola Radulovic <n.radulovic@msgnetconomy.net>
2019-11-01 12:11:25 +0100

    FSA-4361: Renames myaccount to my-account

--- projects/fsastorefrontlib/src/cms-components/cms-lib.module.ts
+++ projects/fsastorefrontlib/src/cms-components/cms-lib.module.ts
@@ -5,12 +5,12 @@ import { ComparisonTableModule } from './comparison-table/comparison-table.modul
 import { CategoryFormsModule } from './form/cms-category-form-component/cms-category-forms.module';
 import { CustomContainerModule } from './container/cms-custom-container.module';
 import { AgentModule } from './agent/agent.module';
-import { MyAccountModule } from './myaccount/myaccount.module';
-import { effects } from '../core/myaccount/store/effects/index';
+import { MyAccountModule } from './my-account/myaccount.module';
+import { effects } from '../core/my-account/store/effects/index';
 import {
   reducerProvider,
   reducerToken,
-} from '../core/myaccount/store/reducers/index';
+} from '../core/my-account/store/reducers/index';
 import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import { UserRequestModule } from './user-request/user-request.module';
