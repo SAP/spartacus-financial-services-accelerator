bb8bfcaae3999144fdb04481f6c3eacb19a50e7a
Nikola Radulovic <n.radulovic@msgnetconomy.net>
2019-11-06 13:55:44 +0100

    FSA-4144: Fixes broken test.

--- projects/dynamicforms/src/core/form-containers/dynamic-form/dynamic-form.component.spec.ts
+++ projects/dynamicforms/src/core/form-containers/dynamic-form/dynamic-form.component.spec.ts
@@ -1,14 +1,53 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DynamicFormComponent } from './dynamic-form.component';
+import { Directive, Input } from '@angular/core';
+import {
+  FieldConfig,
+  FormDefinition,
+} from '../../models/field-config.interface';
+import { FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormService } from '../../services/form.service';
+
+@Directive({
+  // tslint:disable
+  selector: '[cxDynamicField]',
+})
+export class MockDynamicFieldDirective {
+  @Input()
+  config: FieldConfig;
+  @Input()
+  group: FormGroup;
+}
+
+const mockFormGroup = {
+  config: {
+    formGroups: [],
+  },
+};
+
+export class MockFormService {
+  createForm() {
+    return mockFormGroup;
+  }
+}
 
 describe('DynamicFormComponent', () => {
   let component: DynamicFormComponent;
   let fixture: ComponentFixture<DynamicFormComponent>;
+  let mockFormService: MockFormService;
 
   beforeEach(async(() => {
+    mockFormService = new MockFormService();
     TestBed.configureTestingModule({
-      declarations: [DynamicFormComponent],
+      imports: [ReactiveFormsModule],
+      declarations: [DynamicFormComponent, MockDynamicFieldDirective],
+      providers: [
+        {
+          provide: FormService,
+          useValue: mockFormService,
+        },
+      ],
     }).compileComponents();
   }));
 
