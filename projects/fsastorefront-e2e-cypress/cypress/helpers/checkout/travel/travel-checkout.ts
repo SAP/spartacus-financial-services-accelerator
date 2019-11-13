5e627614c6dedb7fe6cfeba7905b7623cb26adce
Nikola Radulovic <n.radulovic@msgnetconomy.net>
2019-11-13 16:36:22 +0100

    FSA-4244: Initial implementation for travel checkout test

---
+++ projects/fsastorefront-e2e-cypress/cypress/helpers/checkout/travel/travel-checkout.ts
@@ -0,0 +1,88 @@
+export function openCategoryPage() {
+  cy.get('cx-category-navigation').should('be.visible');
+  cy.get('cx-category-navigation')
+    .findByText('Travel')
+    .click({ force: true });
+  cy.wait(1000);
+
+  cy.get('fsa-enriched-responsive-banner').should('be.visible');
+  cy.get('fsa-enriched-responsive-banner')
+    .findByText('Get a quote')
+    .click({ force: true });
+}
+
+export function populateInsuranceInfoForm() {
+  cy.get('cx-dynamic-form').within(() => {
+    cy.get('[name=tripDestination]').select('Europe');
+    cy.get('[name="NoOfDays"]').type('9');
+    cy.get('[name="tripStartDate"]').type('2020-01-01');
+    cy.get('[name="tripEndDate"]').type('2020-01-10');
+    cy.get('[name="costOfTrip"]').type('3000');
+    cy.get('[name="Travellers"]').select('1');
+    cy.get('[name="tripDetailsTravellerAges"]').type('20');
+    cy.get('[name="submit"]')
+      .findByText('Find Prices')
+      .click();
+    cy.wait(3000);
+  });
+}
+
+export function addMainProductToCart() {
+  cy.get('fsa-comparison-table-panel-item').should('have.length', 3);
+  cy.get('fsa-comparison-table-panel-item')
+    .eq(0)
+    .within(() => {
+      cy.get('.primary-button').click();
+    });
+}
+
+export function addOptionalProduct() {
+  cy.get('fsa-add-options').should('be.visible');
+  cy.get('fsa-add-options').within(() => {
+    cy.get('h3').should('have.length', 6);
+    cy.get('.secondary-button')
+      .eq(0)
+      .click();
+    cy.wait(1000);
+  });
+  cy.get('.primary-button').should('be.visible');
+  cy.get('.primary-button').click();
+}
+
+export function checkQuoteReview() {
+  cy.get('fsa-mini-cart').within(() => {
+    cy.get('.short-overview-item').should('have.length', 2);
+    cy.get('.short-overview-item')
+      .eq(0)
+      .should('have.text', ' Single - Budget Plan:  €90.00 ');
+    cy.get('.short-overview-item')
+      .eq(1)
+      .should('have.text', ' Winter Sports Cover:  €9.00 ');
+    cy.get('.highlighted').should('have.text', ' Total price:  €99.00 ');
+  });
+  cy.get('.primary-button').click();
+}
+
+export function checkPaymentPage() {
+  cy.get('cx-payment-method').within(() => {
+    cy.get('.btn-primary').click();
+  });
+}
+
+export function placeOrderOnFinalReivew() {
+  cy.get('fsa-final-review').within(() => {
+    cy.get('.form-check-input').click();
+    cy.get('.primary-button').click();
+  });
+}
+
+export function checkOrderConfirmation() {
+  cy.get('fsa-order-confirmation').within(() => {
+    cy.get('h2')
+      .eq(0)
+      .should(
+        'have.text',
+        ' Thank you! Your policy request has been submitted '
+      );
+  });
+}
---
+++ projects/fsastorefront-e2e-cypress/cypress/integration/regression/checkout/travel-checkout.e2e-spec.ts
@@ -0,0 +1,47 @@
+import * as register from '../../../helpers/register';
+import { donnaMooreUser } from '../../../sample-data/users';
+import * as travelCheckout from '../../../helpers/checkout/travel/travel-checkout';
+
+context('Travel Insurance Checkout', () => {
+  before(() => {
+    register.login(donnaMooreUser.email, donnaMooreUser.password);
+    cy.wait(1000);
+    cy.visit('/');
+  });
+
+  describe('Checkout', () => {
+    it('Should open travel category page', () => {
+      travelCheckout.openCategoryPage();
+    });
+    it('Should populate insurance information form', () => {
+      travelCheckout.populateInsuranceInfoForm();
+    });
+
+    it('Add main product to the cart', () => {
+      travelCheckout.addMainProductToCart();
+      cy.wait(2000);
+    });
+
+    it('Add optional product to the cart', () => {
+      travelCheckout.addOptionalProduct();
+      cy.wait(1000);
+    });
+
+    it('Check mini cart on quote review page', () => {
+      travelCheckout.checkQuoteReview();
+    });
+
+    it('Select default payment details', () => {
+      travelCheckout.checkPaymentPage();
+    });
+
+    it('Place order on final review page', () => {
+      travelCheckout.placeOrderOnFinalReivew();
+      cy.wait(2000);
+    });
+
+    it('Check order confirmation', () => {
+      travelCheckout.checkOrderConfirmation();
+    });
+  });
+});
