58bed9d25b4be3d1a11003d009ab418c09cb6a72
Ksenija Zivkovic <k.zivkovic@msgnetconomy.net>
2019-11-19 22:04:23 +0100

    FSA-4435: Prettier fixes

--- projects/fsastorefrontlib/src/cms-components/form/cms-category-form-component/cms-category-form-submit-component.ts
+++ projects/fsastorefrontlib/src/cms-components/form/cms-category-form-component/cms-category-form-submit-component.ts
@@ -1,9 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import {
-  FormDataService,
-  FormDefinition,
-} from '@fsa/dynamicforms';
+import { FormDataService, FormDefinition } from '@fsa/dynamicforms';
 import { CmsComponentConnector, PageContext, PageType } from '@spartacus/core';
 import { CmsComponentData } from '@spartacus/storefront';
 import { Observable, of, Subscription } from 'rxjs';
