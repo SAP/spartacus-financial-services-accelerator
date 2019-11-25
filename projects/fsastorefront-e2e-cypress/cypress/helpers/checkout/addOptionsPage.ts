e1b275b53768a3233ed139f33947c24fcd5c94e1
Nikola Radulovic <n.radulovic@msgnetconomy.net>
2019-11-25 16:18:42 +0100

    FSA-4509: Refactors add options page to be re-usable.

--- projects/fsastorefront-e2e-cypress/cypress/helpers/checkout/addOptionsPage.ts
+++ projects/fsastorefront-e2e-cypress/cypress/helpers/checkout/addOptionsPage.ts
@@ -1,6 +1,42 @@
+export interface AddOptionItem {
+  name: string;
+  available?: boolean;
+  shouldAdd?: boolean;
+}
+
+export interface AddOptions {
+  title: string;
+  items: AddOptionItem[];
+}
+
 export function checkAddOptionsPage() {
   cy.get('div.progress-inner-wrapper').should('have.length', 5);
   cy.get('fsa-add-options').should('be.visible');
+}
+
+export function checkAddOptionsPageContent(addOptions: AddOptions) {
+  cy.get('h2.main-heading').contains(addOptions.title);
+  cy.get('fsa-add-options')
+    .should('be.visible')
+    .within(() => {
+      cy.get('h3').should('have.length', addOptions.items.length);
+      addOptions.items.forEach((item, index) => {
+        cy.get('h3')
+          .eq(index)
+          .should('have.text', item.name);
+        if (!item.available) {
+          cy.get('button.secondary-button')
+            .eq(index)
+            .should('contain', 'Not available');
+        }
+        if (item.shouldAdd) {
+          cy.get('.secondary-button')
+            .eq(index)
+            .click();
+          cy.wait(1000);
+        }
+      });
+    });
 }
 
 export function checkAddOptionsPageCurrentAccount() {
