eb6ec7fb7816d2633d789079207596558efa39eb
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2019-11-29 15:34:59 +0100

    FSA-4371: Fixed test

--- projects/fsastorefrontlib/src/cms-components/my-account/inbox/inbox.component.spec.ts
+++ projects/fsastorefrontlib/src/cms-components/my-account/inbox/inbox.component.spec.ts
@@ -1,16 +1,19 @@
 import { Component, DebugElement, Input } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { NgbTabsetModule } from '@ng-bootstrap/ng-bootstrap';
-import { CmsService, I18nTestingModule } from '@spartacus/core';
+import { AuthService, AuthRedirectService, CmsService, I18nTestingModule, UserToken } from '@spartacus/core';
 import { CmsComponentData } from '@spartacus/storefront';
 import { Observable, of } from 'rxjs';
 import { InboxTab } from '../../../core/my-account/services/inbox-data.service';
 import { InboxService } from './../../../core/my-account/services/inbox.service';
+import { InboxDataService } from '../../../core/my-account/services/inbox-data.service';
 import {
   CmsInboxComponent,
   CmsInboxTabComponent,
 } from './../../../occ/occ-models/cms-component.models';
 import { InboxComponent } from './inbox.component';
+
+import createSpy = jasmine.createSpy;
 
 @Component({
   template: '',
@@ -27,6 +30,7 @@ class InboxTabComponent {
 })
 class InboxMessagesComponent {
   @Input() initialGroup;
+  @Input() checkBoxStatus;
 }
 
 const mockedCMSInboxTab: InboxTab = {
@@ -34,19 +38,33 @@ const mockedCMSInboxTab: InboxTab = {
   messageGroup: 'TestGeneralMessageGroup',
 };
 
+class MockAuthService {
+  getUserToken(): Observable<UserToken> {
+    return of({ access_token: 'test' } as UserToken);
+  }
+}
+
 class MockCmsService {
   getComponentData(): Observable<CmsInboxTabComponent>[] {
     return [of(mockedCMSInboxTab)];
   }
 }
 
+class MockRedirectAfterAuthService {
+  redirect = createSpy('AuthRedirectService.redirect');
+}
+
 class MockInboxService {
   activeMessageGroupAndTitle: Observable<InboxTab> = of(mockedCMSInboxTab);
+}
+class MockInboxDataService {
+  userId = 'current';
 }
 
 describe('InboxComponent', () => {
   let inboxComponent: InboxComponent;
   let mockInboxService: MockInboxService;
+  let mockInboxDataService: MockInboxDataService;
   let mockCmsService: MockCmsService;
   let fixture: ComponentFixture<InboxComponent>;
   let el: DebugElement;
@@ -65,6 +83,8 @@ describe('InboxComponent', () => {
   beforeEach(async(() => {
     mockInboxService = new MockInboxService();
     mockCmsService = new MockCmsService();
+    mockInboxDataService = new MockInboxDataService();
+
     TestBed.configureTestingModule({
       imports: [NgbTabsetModule, I18nTestingModule],
       declarations: [InboxComponent, InboxTabComponent, InboxMessagesComponent],
@@ -78,9 +98,18 @@ describe('InboxComponent', () => {
           useValue: mockInboxService,
         },
         {
+          provide: InboxDataService,
+          useValue: mockInboxDataService,
+        },
+        {
           provide: CmsService,
           useValue: mockCmsService,
         },
+        { provide: AuthService, useClass: MockAuthService },
+        {
+          provide: AuthRedirectService,
+          useClass: MockRedirectAfterAuthService,
+        }
       ],
     }).compileComponents();
   }));
