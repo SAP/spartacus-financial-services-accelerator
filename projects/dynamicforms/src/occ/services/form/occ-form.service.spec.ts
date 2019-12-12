334754697b3ce18f54207e0d671154d31c77f102
Nikola Radulovic <n.radulovic@msgnetconomy.net>
2019-12-12 10:58:03 +0100

    FSA-4467: Fix failing test.

--- projects/dynamicforms/src/occ/services/form/occ-form.service.spec.ts
+++ projects/dynamicforms/src/occ/services/form/occ-form.service.spec.ts
@@ -51,6 +51,7 @@ describe('OccYformService', () => {
 
   describe('persistFormData', () => {
     it('saveFormData', async(() => {
+      console.log(formData);
       service.saveFormData(formData).subscribe();
       httpMock.expectOne((req: HttpRequest<any>) => {
         return (
