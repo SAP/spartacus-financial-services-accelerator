f6dd365b8fe424f638157df62fcd354dd8f12a34
Nikola Radulovic <n.radulovic@msgnetconomy.net>
2019-12-12 13:04:14 +0100

    FSA-4467: Fixes formatting with prettier

--- projects/dynamicforms/src/core/form-containers/dynamic-form/dynamic-form.component.ts
+++ projects/dynamicforms/src/core/form-containers/dynamic-form/dynamic-form.component.ts
@@ -111,7 +111,8 @@ export class DynamicFormComponent implements OnInit, OnDestroy {
             if (
               form &&
               form.content === undefined &&
-              this.form && this.value !== undefined &&
+              this.form &&
+              this.value !== undefined &&
               this.valid
             ) {
               this.submit.emit({
