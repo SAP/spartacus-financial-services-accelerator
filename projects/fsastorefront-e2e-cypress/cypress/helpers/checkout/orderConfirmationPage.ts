c6fba673275c793c80897b2ba314ff7bf9195b7b
Bojan Jevtic <b.jevtic@msgnetconomy.net>
2019-12-26 13:53:14 +0100

    FSA-4490: formatting

--- projects/fsastorefront-e2e-cypress/cypress/helpers/checkout/orderConfirmationPage.ts
+++ projects/fsastorefront-e2e-cypress/cypress/helpers/checkout/orderConfirmationPage.ts
@@ -2,9 +2,6 @@ export function checkOrderConfirmationBanking() {
   cy.get('fsa-order-confirmation-message').within(() => {
     cy.get('h5')
       .eq(0)
-      .should(
-        'have.text',
-        ' Thank you for your order! '
-      );
+      .should('have.text', ' Thank you for your order! ');
   });
 }
