b6069c4f7fc34a80a7f747620a083b5627452b4e
Bojan Jevtic <b.jevtic@msgnetconomy.net>
2020-01-31 12:18:56 +0100

    NO-TASK: Changes code coverage generation

--- src/test.ts
+++ src/test.ts
@@ -15,6 +15,8 @@ getTestBed().initTestEnvironment(
   platformBrowserDynamicTesting()
 );
 // Then we find all the tests.
-const context = require.context('./', true, /\.spec\.ts$/);
+const context = require.context('./app/', true, /\.ts$/);
+
+
 // And load the modules.
 context.keys().map(context);
