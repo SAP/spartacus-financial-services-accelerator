4d369a0f7051867bd246a0732436a1ade1e03c90
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2020-02-05 22:46:23 +0100

    FSA-4761: Prefill fixed after reload

--- projects/fsastorefrontlib/src/cms-components/agent/contact-agent-form/contact-agent-form.component.ts
+++ projects/fsastorefrontlib/src/cms-components/agent/contact-agent-form/contact-agent-form.component.ts
@@ -2,20 +2,17 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params } from '@angular/router';
 import {
   FormBuilder,
-  FormControl,
   FormGroup,
   Validators,
 } from '@angular/forms';
 import { Observable, Subscription } from 'rxjs';
-import { AgentSearchService } from '../../../core/agent/services/agent-search.service';
-import { CustomFormValidators } from '@spartacus/storefront';
 import {
-  UserState,
-  UserDetailsState,
   StateWithUser,
   UsersSelectors,
 } from '@spartacus/core';
 import { Store, select } from '@ngrx/store';
+import { DefaultFormValidators } from '@fsa/dynamicforms';
+import { AgentSearchService } from '../../../core/agent/services/agent-search.service';
 
 @Component({
   selector: 'fsa-contact-agent-form',
@@ -27,37 +24,41 @@ export class ContactAgentFormComponent implements OnInit, OnDestroy {
     private route: ActivatedRoute,
     protected fb: FormBuilder,
     protected userStore: Store<StateWithUser>
-  ) {}
+  ) { }
 
-  subscription = new Subscription();
+  private subscription = new Subscription();
+
   agent$: Observable<any>;
-  contactAgentForm: FormGroup;
-  prefilledFirstName;
-  prefilledLastName;
-  prefilledEmail;
+
+  contactAgentForm: FormGroup = this.fb.group({
+    firstName: ['', Validators.required],
+    lastName: ['', Validators.required],
+    email: ['', [Validators.required, DefaultFormValidators.regexValidator(
+      DefaultFormValidators.emailRegex
+    )]],
+    phoneNumber: ['', [Validators.required]],
+    interest: ['', [Validators.required]],
+    contactType: ['', [Validators.required]],
+    message: ['', [Validators.required]],
+  });
 
   ngOnInit() {
-    this.subscription.add(
-      this.userStore
-        .pipe(select(UsersSelectors.getUserState))
-        .subscribe(user => {
-          this.prefilledFirstName = user.account.details.firstName;
-          this.prefilledLastName = user.account.details.lastName;
-          this.prefilledEmail = user.account.details.uid;
-        })
-    );
-    this.contactAgentForm = this.fb.group({
-      firstName: [this.prefilledFirstName, Validators.required],
-      lastName: [this.prefilledLastName, Validators.required],
-      email: [this.prefilledEmail, [Validators.required]],
-      phoneNumber: ['', [Validators.required]],
-      interest: ['', [Validators.required]],
-      contactType: ['', [Validators.required]],
-      message: ['', [Validators.required]],
-    });
-    this.subscription.add(
-      this.route.params.subscribe(params => this.initialize(params))
-    );
+    this.subscription
+      .add(
+        this.route.params.subscribe(params => this.initialize(params))
+      )
+      .add(
+        this.userStore.pipe(select(UsersSelectors.getUserState))
+          .subscribe(user => {
+            if (user && user.account) {
+              this.contactAgentForm.patchValue({
+                firstName: user.account.details.firstName,
+                lastName: user.account.details.lastName,
+                email: user.account.details.uid
+              });
+            }
+          })
+      );
   }
 
   private initialize(params: Params) {
