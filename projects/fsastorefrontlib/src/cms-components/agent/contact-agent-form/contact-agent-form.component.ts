5d10d03bb5cfdb9eb1b6df28b9cf582e7040a4c7
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2020-02-05 16:44:40 +0100

    FSA-4761: Prefill implemented

--- projects/fsastorefrontlib/src/cms-components/agent/contact-agent-form/contact-agent-form.component.ts
+++ projects/fsastorefrontlib/src/cms-components/agent/contact-agent-form/contact-agent-form.component.ts
@@ -9,6 +9,13 @@ import {
 import { Observable, Subscription } from 'rxjs';
 import { AgentSearchService } from '../../../core/agent/services/agent-search.service';
 import { CustomFormValidators } from '@spartacus/storefront';
+import {
+  UserState,
+  UserDetailsState,
+  StateWithUser,
+  UsersSelectors,
+} from '@spartacus/core';
+import { Store, select } from '@ngrx/store';
 
 @Component({
   selector: 'fsa-contact-agent-form',
@@ -18,23 +25,36 @@ export class ContactAgentFormComponent implements OnInit, OnDestroy {
   constructor(
     protected agentSearchService: AgentSearchService,
     private route: ActivatedRoute,
-    protected fb: FormBuilder
+    protected fb: FormBuilder,
+    protected userStore: Store<StateWithUser>
   ) {}
 
   subscription = new Subscription();
   agent$: Observable<any>;
-
-  contactAgentForm: FormGroup = this.fb.group({
-    firstName: ['', Validators.required],
-    lastName: ['', Validators.required],
-    email: ['', [Validators.required, CustomFormValidators.emailValidator]],
-    phoneNumber: ['', [Validators.required]],
-    interest: ['', [Validators.required]],
-    contactType: ['', [Validators.required]],
-    message: ['', [Validators.required]],
-  });
+  contactAgentForm: FormGroup;
+  prefilledFirstName;
+  prefilledLastName;
+  prefilledEmail;
 
   ngOnInit() {
+    this.subscription.add(
+      this.userStore
+        .pipe(select(UsersSelectors.getUserState))
+        .subscribe(user => {
+          this.prefilledFirstName = user.account.details.firstName;
+          this.prefilledLastName = user.account.details.lastName;
+          this.prefilledEmail = user.account.details.uid;
+        })
+    );
+    this.contactAgentForm = this.fb.group({
+      firstName: [this.prefilledFirstName, Validators.required],
+      lastName: [this.prefilledLastName, Validators.required],
+      email: [this.prefilledEmail, [Validators.required]],
+      phoneNumber: ['', [Validators.required]],
+      interest: ['', [Validators.required]],
+      contactType: ['', [Validators.required]],
+      message: ['', [Validators.required]],
+    });
     this.subscription.add(
       this.route.params.subscribe(params => this.initialize(params))
     );
