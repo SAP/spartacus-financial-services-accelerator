acab947fdf378d5722546e8a817d3b0ace56da8b
Nikola Radulovic <n.radulovic@msgnetconomy.net>
2020-02-10 16:00:25 +0100

    FSA-4710: Adds missing tests for inbox service

--- projects/fsastorefrontlib/src/cms-components/my-account/inbox/inbox-tab/inbox-messages/inbox-messages.component.spec.ts
+++ projects/fsastorefrontlib/src/cms-components/my-account/inbox/inbox-tab/inbox-messages/inbox-messages.component.spec.ts
@@ -1,10 +1,51 @@
-import { DebugElement, Component, Input } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { InboxService } from '../../../../../core/my-account/services/inbox.service';
 import { InboxMessagesComponent } from './inbox-messages.component';
 import { I18nTestingModule } from '@spartacus/core';
+import { of, BehaviorSubject } from 'rxjs';
+import { InboxTab } from './../../../../../core/my-account/services/inbox-data.service';
 
-class MockInboxService {}
+const mockInboxTab: InboxTab = {
+  title: 'title',
+  messageGroup: 'group',
+};
+
+//move to method level
+const mockMessages = {
+  sorts: [
+    {
+      code: 'asc',
+    },
+  ],
+  pagination: {
+    page: 1,
+  },
+  messages: [
+    {
+      uid: 'testMsg1',
+      subject: 'testSubject1',
+      readDate: '21-01-2019',
+    },
+    {
+      uid: 'testMsg2',
+      subject: 'testSubject2',
+    },
+  ],
+};
+
+class MockInboxService {
+  activeMessageGroupAndTitle = of(mockInboxTab);
+  messagesSource = new BehaviorSubject<any>(null);
+
+  getMessages(messageGroup, searchConfig) {
+    return of(mockMessages);
+  }
+
+  setMessagesState() {
+    return of(true);
+  }
+}
 
 @Component({
   template: '',
@@ -24,11 +65,23 @@ class MockPaginationComponent {
   @Input() pagination;
 }
 
+const loadedMessages = [
+  {
+    uid: 'firstMessage',
+    read: false,
+    checked: false
+  },
+  {
+    uid: 'secondMessage',
+    read: false,
+    checked: true
+  },
+];
+
 describe('InboxMessagesComponent', () => {
   let inboxMessagesComponent: InboxMessagesComponent;
   let mockInboxService: MockInboxService;
   let fixture: ComponentFixture<InboxMessagesComponent>;
-  let el: DebugElement;
 
   beforeEach(async(() => {
     mockInboxService = new MockInboxService();
@@ -52,10 +105,72 @@ describe('InboxMessagesComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(InboxMessagesComponent);
     inboxMessagesComponent = fixture.componentInstance;
-    el = fixture.debugElement;
+    fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(inboxMessagesComponent).toBeTruthy();
   });
+
+  it('should clear search data', () => {
+    inboxMessagesComponent.initialGroup = 'initial-group';
+    expect(inboxMessagesComponent.searchConfig.currentPage).toEqual(1);
+  });
+
+  it('should sort messages', () => {
+    inboxMessagesComponent.sortMessages('desc', 'date');
+  });
+
+  it('should change page and reset main checkbox', () => {
+    inboxMessagesComponent.mainCheckboxChecked = true;
+    inboxMessagesComponent.pageChange(2);
+    expect(inboxMessagesComponent.mainCheckboxChecked).toEqual(false);
+    //check pagination too
+  });
+
+  it('should read message', () => {
+    inboxMessagesComponent.loadedMessages = loadedMessages;
+    inboxMessagesComponent.readMessage({
+      uid: 'firstMessage',
+    });
+    expect(inboxMessagesComponent.loadedMessages[0].read).toEqual(true);
+  });
+
+  it('should check message', () => {
+    inboxMessagesComponent.loadedMessages = loadedMessages;
+    inboxMessagesComponent.checkMessage('firstMessage', true);
+    expect(inboxMessagesComponent.loadedMessages[0].checked).toEqual(true);
+  });
+
+  it('should uncheck message with main checkbox', () => {
+    inboxMessagesComponent.loadedMessages = loadedMessages;
+    inboxMessagesComponent.mainCheckboxChecked = true;
+    inboxMessagesComponent.checkMessage('secondMessage', false);
+    expect(inboxMessagesComponent.loadedMessages[1].checked).toEqual(false);
+    expect(inboxMessagesComponent.mainCheckboxChecked).toEqual(false);
+  });
+
+  it('should check all messages with main checkbox', () => {
+    inboxMessagesComponent.loadedMessages = loadedMessages;
+    inboxMessagesComponent.checkAllCheckboxes(true);
+    expect(inboxMessagesComponent.loadedMessages[0].checked).toEqual(true);
+    expect(inboxMessagesComponent.loadedMessages[1].checked).toEqual(true);
+    expect(inboxMessagesComponent.mainCheckboxChecked).toEqual(true);
+  });
+
+  it('should uncheck all messages with main checkbox', () => {
+    inboxMessagesComponent.loadedMessages = loadedMessages;
+    inboxMessagesComponent.mainCheckboxChecked = true;
+    inboxMessagesComponent.checkAllCheckboxes(false);
+    expect(inboxMessagesComponent.loadedMessages[0].checked).toEqual(false);
+    expect(inboxMessagesComponent.loadedMessages[1].checked).toEqual(false);
+    expect(inboxMessagesComponent.mainCheckboxChecked).toEqual(false);
+  });
+
+  it('should change selected messages', () => {
+    inboxMessagesComponent.loadedMessages = loadedMessages;
+    inboxMessagesComponent.changeSelectedMessages(true);
+    expect(inboxMessagesComponent.loadedMessages[1].read).toEqual(true);
+  });
+
 });
