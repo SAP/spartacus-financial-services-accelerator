3a82b2f5f3663a8c0977c115915b977d18af8cf8
Ksenija Zivkovic <k.zivkovic@msgnetconomy.net>
2020-02-11 15:33:25 +0100

    FSA-4747: Added unsubscribing

--- projects/fsastorefrontlib/src/cms-components/category/category-feature-carousel/category-feature-carousel.component.ts
+++ projects/fsastorefrontlib/src/cms-components/category/category-feature-carousel/category-feature-carousel.component.ts
@@ -1,17 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import {
   CmsCategoryFeatureComponent,
   CmsCategoryFeatureCarouselComponent,
 } from '../../../occ/occ-models';
 import { CmsComponentData } from '@spartacus/storefront';
 import { CmsService } from '@spartacus/core';
-import { Observable, of } from 'rxjs';
+import { Observable, of, Subscription } from 'rxjs';
 
 @Component({
   selector: 'fsa-category-carousel',
   templateUrl: './category-feature-carousel.component.html',
 })
-export class CategoryFeatureCarouselComponent implements OnInit {
+export class CategoryFeatureCarouselComponent implements OnInit, OnDestroy {
   constructor(
     protected componentData: CmsComponentData<
       CmsCategoryFeatureCarouselComponent
@@ -19,19 +19,29 @@ export class CategoryFeatureCarouselComponent implements OnInit {
     protected cmsService: CmsService
   ) {}
 
+  subscription = new Subscription();
+
   component$: Observable<CmsCategoryFeatureCarouselComponent>;
   items$: Observable<Observable<CmsCategoryFeatureComponent>[]>;
 
   ngOnInit() {
     this.component$ = this.componentData.data$;
-    this.component$.subscribe(data => {
-      const categoryFeatures = [];
-      if (data.categoryFeatures) {
-        data.categoryFeatures.split(' ').forEach(feature => {
-          categoryFeatures.push(this.cmsService.getComponentData(feature));
-        });
-      }
-      this.items$ = of(categoryFeatures);
-    });
+    this.subscription.add(
+      this.component$.subscribe(data => {
+        const categoryFeatures = [];
+        if (data.categoryFeatures) {
+          data.categoryFeatures.split(' ').forEach(feature => {
+            categoryFeatures.push(this.cmsService.getComponentData(feature));
+          });
+        }
+        this.items$ = of(categoryFeatures);
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 }
