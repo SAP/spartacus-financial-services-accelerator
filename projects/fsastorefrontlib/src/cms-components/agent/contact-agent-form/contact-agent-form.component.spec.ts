c218459714db24483beac4f1ee5f0cb041c03b08
Bojan Jevtic <b.jevtic@msgnetconomy.net>
2020-02-13 10:57:40 +0100

    FSA-4746: Contact agent form test

--- projects/fsastorefrontlib/src/cms-components/agent/contact-agent-form/contact-agent-form.component.spec.ts
+++ projects/fsastorefrontlib/src/cms-components/agent/contact-agent-form/contact-agent-form.component.spec.ts
@@ -3,19 +3,36 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
-import { I18nTestingModule, UserService } from '@spartacus/core';
+import { I18nTestingModule, UserService, GlobalMessageService, RoutingService } from '@spartacus/core';
+import { FSCsTicketService } from './../../../core/cs-ticket/facade/cs-ticket.service';
 import { of } from 'rxjs';
 
 import { ContactAgentFormComponent } from './contact-agent-form.component';
 import { AgentSearchService } from '../../../core/agent/facade/agent-search.service';
+import createSpy = jasmine.createSpy;
 
 const mockedUserDetails = {
   firstName: 'Test',
   lastName: 'Testera',
   uid: 'test@testera.com',
 };
-
 const agentParams = 'agent@test.com';
+const ticketData = {
+  interest: 'INCIDENT',
+  contactType: 'EMAIL',
+  subject: 'Ticket subject'
+};
+const mockAgentSearchService = {
+  getAgentByID: jasmine.createSpy().and.returnValue(of({})),
+};
+const mockContactAgentForm: any = {
+  email: 'test@email.com',
+  interest: 'PROBLEM',
+  contactType: 'CALL',
+  subject: 'The Subject',
+  message: 'Test message'
+};
+
 
 let mockParams = {
   agentParams: agentParams,
@@ -24,32 +41,42 @@ let mockParams = {
 class ActivatedRouteMock {
   params = of(mockParams);
 }
-
-const mockAgentSearchService = {
-  getAgentByID: jasmine.createSpy().and.returnValue(of({})),
-};
-
 @Pipe({
   name: 'cxUrl',
 })
 class MockUrlPipe implements PipeTransform {
   transform() {}
 }
-
 class MockedUserService {
   get() {
     return of(mockedUserDetails);
   }
 }
 
+class MockCsTicketService {
+  createCsTicketForAgent() {
+    return of(ticketData);
+  }
+}
+class MockRoutingService {
+  go = createSpy();
+}
+
+class MockGlobalMessageService {
+  add(): void {}
+}
+
 describe('ContactAgentFormComponent', () => {
   let component: ContactAgentFormComponent;
   let fixture: ComponentFixture<ContactAgentFormComponent>;
   let mockedUserService: MockedUserService;
+  let mockedCsTicketService: MockCsTicketService;
   let mockSearchService: AgentSearchService;
+  let globalMessageService: GlobalMessageService;
 
   beforeEach(async(() => {
     mockedUserService = new MockedUserService();
+    mockedCsTicketService = new MockCsTicketService();
     TestBed.configureTestingModule({
       declarations: [ContactAgentFormComponent, MockUrlPipe],
       imports: [I18nTestingModule, RouterTestingModule, ReactiveFormsModule],
@@ -66,6 +93,18 @@ describe('ContactAgentFormComponent', () => {
           provide: ActivatedRoute,
           useClass: ActivatedRouteMock,
         },
+        {
+          provide: GlobalMessageService,
+          useClass: MockGlobalMessageService,
+        },
+        {
+          provide: RoutingService,
+          useClass: MockRoutingService,
+        },
+        {
+          provide: FSCsTicketService,
+          useValue: mockedCsTicketService,
+        },
       ],
     }).compileComponents();
   }));
@@ -73,9 +112,13 @@ describe('ContactAgentFormComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(ContactAgentFormComponent);
     component = fixture.componentInstance;
+    globalMessageService = TestBed.get(GlobalMessageService);
     mockedUserService = TestBed.get(UserService as Type<UserService>);
     mockSearchService = TestBed.get(AgentSearchService as Type<
       AgentSearchService
+    >);
+    mockedCsTicketService = TestBed.get(FSCsTicketService as Type<
+      FSCsTicketService
     >);
   });
 
@@ -108,5 +151,25 @@ describe('ContactAgentFormComponent', () => {
     component.ngOnInit();
     component.ngOnDestroy();
     expect(subscriptions.unsubscribe).toHaveBeenCalled();
+  });
+
+  describe('collectDataFromContactAgentForm()', () => {
+    it('should return contact agent data', () => {
+      const form = mockContactAgentForm;
+
+      expect(component.collectDataFromContactAgentForm(mockContactAgentForm)).toEqual({
+        email: form.email,
+        interest: form.interest,
+        contactType: form.contactType,
+        subject: form.subject,
+        message: form.message
+      });
+    });
+  });
+
+  it('should submit form', () => {
+    spyOn(mockedCsTicketService, 'createCsTicketForAgent').and.returnValue(of(ticketData));
+    component.ngOnInit();
+    component.submit();
   });
 });
--- projects/fsastorefrontlib/src/occ/adapters/cs-ticket/occ-cs-ticket.adapter.spec.ts
+++ projects/fsastorefrontlib/src/occ/adapters/cs-ticket/occ-cs-ticket.adapter.spec.ts
@@ -1,11 +1,11 @@
-import { OccFsCsTicketAdapter } from './occ-cs-ticket.adapter';
-import { HttpClientModule, HttpRequest } from '@angular/common/http';
+import { HttpClientModule, HttpRequest, HttpErrorResponse } from '@angular/common/http';
 import {
   HttpClientTestingModule,
   HttpTestingController,
 } from '@angular/common/http/testing';
 import { async, TestBed } from '@angular/core/testing';
 import { OccConfig, OCC_USER_ID_CURRENT } from '@spartacus/core';
+import { OccFsCsTicketAdapter } from './occ-cs-ticket.adapter';
 
 const MockOccModuleConfig: OccConfig = {
   context: {
@@ -18,6 +18,7 @@ const MockOccModuleConfig: OccConfig = {
     },
   },
 };
+const userId = OCC_USER_ID_CURRENT;
 const agentId = 'testAgent';
 const ticketData = {
   interest: 'INCIDENT',
@@ -25,6 +26,7 @@ const ticketData = {
   subject: 'Ticket subject'
 };
 describe('OccFsCsTicketAdapter', () => {
+  let httpClientSpy: { get: jasmine.Spy };
   let adapter: OccFsCsTicketAdapter;
   let httpMock: HttpTestingController;
 
@@ -36,12 +38,29 @@ describe('OccFsCsTicketAdapter', () => {
         { provide: OccConfig, useValue: MockOccModuleConfig },
       ],
     });
+    httpClientSpy = jasmine.createSpyObj('HttpClient', ['get']);
+
     adapter = TestBed.get(OccFsCsTicketAdapter);
     httpMock = TestBed.get(HttpTestingController);
   });
 
   describe('createCsTicketForAgent', () => {
-    it('create customer support ticket for agent', async(() => {
+    it('create customer support ticket', async(() => {
+      adapter.createCsTicketForAgent(undefined, OCC_USER_ID_CURRENT, ticketData).subscribe();
+      const mockReq = httpMock.expectOne((req: HttpRequest<any>) => {
+        return (
+          req.url ===
+          '/users' +
+          `/${userId}` +
+          '/csTickets' &&
+          req.method === 'POST'
+        );
+      });
+      expect(mockReq.cancelled).toBeFalsy();
+      expect(mockReq.request.responseType).toEqual('json');
+    }));
+
+    it('create customer support ticket for specific agent', async(() => {
       adapter.createCsTicketForAgent(agentId, OCC_USER_ID_CURRENT, ticketData).subscribe();
       const mockReq = httpMock.expectOne((req: HttpRequest<any>) => {
         return (
@@ -55,5 +74,27 @@ describe('OccFsCsTicketAdapter', () => {
       expect(mockReq.cancelled).toBeFalsy();
       expect(mockReq.request.responseType).toEqual('json');
     }));
+
+    it('should throw an error', () => {
+      let response: any;
+      let errResponse: any;
+      const errorResponse = new HttpErrorResponse({
+        error: '400 error',
+        status: 400,
+        statusText: 'Bad Request'
+      });
+      adapter.createCsTicketForAgent(agentId, OCC_USER_ID_CURRENT, undefined).subscribe(res => response = res, err => errResponse = err);
+      httpMock.expectOne((req: HttpRequest<any>) => {
+        return (
+          req.url ===
+          '/users' +
+          `/${userId}` +
+          '/csTickets' &&
+          req.method === 'POST'
+        );
+      }).flush(errorResponse);
+      expect(errorResponse.status).toEqual(400);
+      expect(errorResponse.name).toEqual('HttpErrorResponse');
+  });
   });
 });
