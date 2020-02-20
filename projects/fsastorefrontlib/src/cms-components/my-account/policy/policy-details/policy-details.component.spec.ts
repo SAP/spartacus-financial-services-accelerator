ad6ec37724e7d748555b9117de319f8a298c8cfd
Ivan Ilic <i.ilic@msgnetconomy.net>
2020-02-20 13:17:20 +0100

    FSA-4764: Prettier run

--- projects/fsastorefrontlib/src/cms-components/my-account/policy/policy-details/policy-details.component.spec.ts
+++ projects/fsastorefrontlib/src/cms-components/my-account/policy/policy-details/policy-details.component.spec.ts
@@ -46,7 +46,7 @@ const mockAllowedFSRequestTypes = [
 const mockFaultyFSRequestTypes = [
   {
     code: 'fscoverage_change_request_type',
-  }
+  },
 ];
 
 const MockOccModuleConfig: OccConfig = {
@@ -89,15 +89,20 @@ describe('PolicyDetailsComponent', () => {
   });
 
   it('should checkk if request type is allowed', () => {
-    expect(component.isChangeAllowed(mockAllowedFSRequestTypes, 'FSCOVERAGE_CHANGE')).toEqual(true);
+    expect(
+      component.isChangeAllowed(mockAllowedFSRequestTypes, 'FSCOVERAGE_CHANGE')
+    ).toEqual(true);
   });
 
   it('should check if request type is not allowed', () => {
-    expect(component.isChangeAllowed(mockAllowedFSRequestTypes, 'NOT_EXISTING_TYPE')).toEqual(false);
+    expect(
+      component.isChangeAllowed(mockAllowedFSRequestTypes, 'NOT_EXISTING_TYPE')
+    ).toEqual(false);
   });
 
   it('should check if isChangeAllowed returns false with missing requestType', () => {
-    expect(component.isChangeAllowed(mockFaultyFSRequestTypes, 'NOT_EXISTING_TYPE')).toEqual(false);
+    expect(
+      component.isChangeAllowed(mockFaultyFSRequestTypes, 'NOT_EXISTING_TYPE')
+    ).toEqual(false);
   });
-
 });
