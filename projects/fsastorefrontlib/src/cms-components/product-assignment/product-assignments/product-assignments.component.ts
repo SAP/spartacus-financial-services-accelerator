ef356d0fa2ac63003ab110b17f62ae08bdf3eaff
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2020-02-21 13:00:05 +0100

    FSA-4828: user and unit ID fetched

--- projects/fsastorefrontlib/src/cms-components/product-assignment/product-assignments/product-assignments.component.ts
+++ projects/fsastorefrontlib/src/cms-components/product-assignment/product-assignments/product-assignments.component.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, OnDestroy } from '@angular/core';
+import { Subscription, pipe } from 'rxjs';
+import { ActivatedRoute, Params } from '@angular/router';
+import { UserService, AuthService, OCC_USER_ID_ANONYMOUS } from '@spartacus/core';
+import { take } from 'rxjs/operators';
 
 @Component({
   selector: 'fsa-product-assignments',
   templateUrl: './product-assignments.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProductAssignmentsComponent implements OnInit {
+export class ProductAssignmentsComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  constructor(
+    protected route: ActivatedRoute,
+    protected authService: AuthService,
+  ) { }
+
+  private subscription = new Subscription();
+  userId: string;
+  orgUnitId: string;
 
   ngOnInit() {
+    this.subscription
+      .add(this.route.params.subscribe(params => this.initialize(params)))
+      .add(
+        this.authService.getOccUserId().pipe(take(1)).subscribe(user => {
+          if (user && user !== OCC_USER_ID_ANONYMOUS) {
+            this.userId = user;
+          }
+        })
+      );
+  }
+
+  private initialize(params: Params) {
+    if (params) {
+      this.orgUnitId = params.orgUnitId;
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
   }
 
 }
