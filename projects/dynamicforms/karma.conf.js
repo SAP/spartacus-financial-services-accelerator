6cc38361fbcde04a72123dcd044a96d052d0b202
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2020-04-07 15:51:32 +0200

    FSA-4507: testGroup renamed to testButton

--- projects/dynamicforms/karma.conf.js
+++ projects/dynamicforms/karma.conf.js
@@ -1,6 +1,5 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
-
 module.exports = function(config) {
   config.set({
     basePath: '',
@@ -12,21 +11,34 @@ module.exports = function(config) {
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
+    browserConsoleLogOptions: {
+      level: 'log',
+      terminal: true,
+    },
     client: {
+      captureConsole: true,
+      mocha: {
+        bail: true,
+      },
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage/dynamicforms'),
-      reports: ['html', 'lcovonly'],
+      dir: require('path').join(__dirname, '../../coverage'),
+      reports: ['lcov', 'cobertura', 'text-summary'],
+      thresholds: {
+        statements: 80,
+        lines: 80,
+        branches: 60,
+        functions: 80,
+      },
       fixWebpackSourcePaths: true,
     },
     reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_INFO,
+    logLevel: config.LOG_LOG,
     autoWatch: true,
-    browsers: ['ChromeHeadless'],
+    browsers: ['Chrome'],
     singleRun: false,
-    restartOnFileChange: true,
   });
 };
