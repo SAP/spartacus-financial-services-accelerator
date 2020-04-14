aa938a5dd560180da1a66fa354fd0add311d1da8
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2020-04-14 21:25:31 +0200

    FSA-4934: removed unecessary interface

--- projects/dynamicforms/src/core/config/form-config.ts
+++ projects/dynamicforms/src/core/config/form-config.ts
@@ -14,22 +14,6 @@ export abstract class FormComponentConfig {
   [_: string]: FormComponentMapping;
 }
 
-export interface CssClass {
-  form?: string;
-  controlElement?: string;
-  formTitle?: string;
-  inputWrapper?: string;
-  label?: string;
-  input?: string;
-  radioInputWrapper?: string;
-  radioInput?: string;
-  radioLabel?: string;
-  radioOptionLabel?: string;
-  validatorMessageWrapper?: string;
-  validatorMessage?: string;
-  submitButton?: string;
-}
-
 export interface ValidatorMapping {
   validator: any;
 }
@@ -44,7 +28,6 @@ export abstract class DynamicFormsConfig {
     errorWrapperClass?: string;
     errorClass?: string;
     errorMessageClass?: string;
-    cssClass?: CssClass;
     components?: FormComponentConfig;
     validators?: ValidatorConfig;
   };
