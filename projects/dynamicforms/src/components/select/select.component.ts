ccd400c6f95f3511388f81c38fd00ac9915c0896
Nikola Radulovic <n.radulovic@msgnetconomy.net>
2020-04-15 13:30:39 +0200

    FSA-4956: Reduce logic in abstract component

--- projects/dynamicforms/src/components/select/select.component.ts
+++ projects/dynamicforms/src/components/select/select.component.ts
@@ -1,43 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AbstractFormComponent } from '../abstract-form.component';
+import { Subscription } from 'rxjs';
 import { map } from 'rxjs/operators';
-import { FieldOption } from 'dynamicforms/src/core';
 
 @Component({
   selector: 'cx-select',
   templateUrl: './select.component.html',
 })
-export class SelectComponent extends AbstractFormComponent implements OnInit {
+export class SelectComponent extends AbstractFormComponent
+  implements OnInit, OnDestroy {
+  private sub = new Subscription();
+
   ngOnInit() {
-    if (this.config.depends) {
-      this.config.depends.forEach(dependField => {
-        this.group.get(dependField).valueChanges.subscribe(val => {
-          this.setFormControlValues(val);
-        });
-      });
+    if (this.config.apiUrl) {
+      this.setFormControlValuesFromAPI();
     }
-    this.setFormControlValues(null);
   }
 
-  setFormControlValues(val: string) {
-    if (this.config.apiUrl) {
-      this.config.options = [];
-      this.getValuesFromAPI(this.config.apiUrl)
+  setFormControlValuesFromAPI() {
+    this.config.options = [];
+    this.sub.add(
+      this.formService
+        .getValuesFromAPI(this.config.apiUrl)
         .pipe(
           map(result => {
             if (result.values) {
-              const apiValues: FieldOption[] = [];
               result.values.forEach(item => {
-                apiValues.push({
+                this.config.options.push({
                   name: item.key,
                   label: item.value,
                 });
               });
-              this.config.options = apiValues;
             }
           })
         )
-        .subscribe();
+        .subscribe()
+    );
+  }
+
+  ngOnDestroy() {
+    if (this.sub) {
+      this.sub.unsubscribe();
     }
   }
 }
