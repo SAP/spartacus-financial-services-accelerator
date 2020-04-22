bfe5a2ab5dfa2d1fb81c975097c5ba55f9727d65
Bojan Jevtic <b.jevtic@msgnetconomy.net>
2020-04-22 10:37:59 +0200

    FSA-4964: Adds initial test

---
+++ projects/dynamicforms/src/core/services/form-dependencies/form-dependency-resolver.service.spec.ts
@@ -0,0 +1,100 @@
+import { Type } from '@angular/core';
+import { TestBed } from '@angular/core/testing';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { of } from 'rxjs';
+import { DynamicFormsConfig } from '../../config/form-config';
+import { FieldConfig } from '../../models/form-config.interface';
+import { defaultFormConfig } from './../../config/default-form-config';
+import { DependencyFn } from './../../models/form-config.interface';
+import { FormValidationService } from './../form-validation/form-validation.service';
+import { FormDependencyResolverService } from './form-dependency-resolver.service';
+
+const fieldType = 'input';
+const minValue = 'minValue';
+
+const dependencyConditions: DependencyFn[] = [
+  {
+    name: 'testField2',
+    conditions: [
+      {
+        name: minValue,
+        arguments: [
+          {
+            value: '5',
+          },
+        ],
+      },
+    ],
+  },
+];
+
+const testField1: FieldConfig = {
+  name: 'testField1',
+  type: fieldType,
+  dependsOn: dependencyConditions,
+};
+
+const testField2: FieldConfig = {
+  name: 'testField2',
+  type: fieldType,
+};
+
+const mockFormGroup = new FormGroup({
+  testField1: new FormControl('6'),
+  testField2: new FormControl('8'),
+});
+
+class MockFormValidationService {
+  getValidatorForFunction() {
+    return Validators.min(1);
+  }
+}
+
+describe('FormDependencyResolverService', () => {
+  let service: FormDependencyResolverService;
+  let mockFormValidationService: FormValidationService;
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
+      providers: [
+        FormDependencyResolverService,
+        { provide: DynamicFormsConfig, useValue: defaultFormConfig },
+        {
+          provide: FormValidationService,
+          useClass: MockFormValidationService,
+        },
+      ],
+    });
+    mockFormValidationService = TestBed.get(FormValidationService as Type<
+      FormValidationService
+    >);
+    service = new FormDependencyResolverService(
+      mockFormValidationService as FormValidationService,
+      new FormBuilder()
+    );
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+
+  it('should resolve form control dependency', () => {
+    Object.defineProperty(mockFormGroup.get('testField2'), 'valueChanges', {
+      value: of('6'),
+    });
+    const dependentFormControl = mockFormGroup.controls['testField1'];
+
+    service.resolveFormControlDependencies(
+      dependencyConditions,
+      dependentFormControl,
+      mockFormGroup
+    );
+    expect(dependentFormControl.disabled).toBe(false);
+  });
+});
