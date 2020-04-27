ec54617e535a10c15c257e13dd5aff3e3ee03702
Nikola Radulovic <n.radulovic@msgnetconomy.net>
2020-04-27 10:12:02 +0200

    FSA-4963: Fixes broken test

--- projects/fsastorefrontlib/src/cms-components/change-process/change-coverage/change-coverage.component.spec.ts
+++ projects/fsastorefrontlib/src/cms-components/change-process/change-coverage/change-coverage.component.spec.ts
@@ -184,7 +184,7 @@ describe('ChangeCoverageComponent', () => {
     expect(mockUserRequestNavigationService.continue).toHaveBeenCalled();
   });
 
-  it('should add coverage', () => {
+  it('should add coverage and execute simulation', () => {
     component.potentialCoverages =
       mockChangeRequest.insurancePolicy.optionalProducts;
     const coverage = {
@@ -197,7 +197,9 @@ describe('ChangeCoverageComponent', () => {
       of(mockChangeRequest)
     );
     component.addCoverage(coverage);
+    component.simulateChanges(mockChangeRequest);
     expect(component.potentialCoverages[0].coverageIsIncluded).toEqual(true);
+    expect(mockChangeRequestService.simulateChangeRequest).toHaveBeenCalled();
   });
 
   it('should remove coverage', () => {
@@ -227,18 +229,5 @@ describe('ChangeCoverageComponent', () => {
     expect(
       mockChangeRequestService.simulateChangeRequest
     ).not.toHaveBeenCalled();
-  });
-
-  it('should execute simulation if policy has been changed', () => {
-    spyOn(mockChangeRequestService, 'getChangeRequest').and.returnValue(
-      of(mockChangeRequest)
-    );
-
-    component.potentialCoverages =
-      mockChangeRequest.insurancePolicy.optionalProducts;
-
-    component.ngOnInit();
-    component.simulateChanges(mockChangeRequest);
-    expect(mockChangeRequestService.simulateChangeRequest).toHaveBeenCalled();
   });
 });
