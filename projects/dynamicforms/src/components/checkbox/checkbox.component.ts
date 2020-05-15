883e791cbc71d09849510f079dcbb75a9ed723c0
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2020-05-15 13:16:57 +0200

    FSA-5008: Checkbox refactor

--- projects/dynamicforms/src/components/checkbox/checkbox.component.ts
+++ projects/dynamicforms/src/components/checkbox/checkbox.component.ts
@@ -5,4 +5,28 @@ import { AbstractOptionsComponent } from '../abstract-options/abstract-options.c
   selector: 'cx-checkbox',
   templateUrl: './checkbox.component.html',
 })
-export class CheckboxComponent extends AbstractOptionsComponent {}
+export class CheckboxComponent extends AbstractOptionsComponent {
+  ngOnInit() {
+    super.ngOnInit();
+    // console.log(
+    //   this.group.controls[this.config.name].valueChanges.subscribe(data => data)
+    // );
+    // if (this.config.options) {
+    //   const selectedOption = this.config.options.find(
+    //     option => option.selected
+    //   );
+    //   if (selectedOption) {
+    //     this.group.get(this.config.name).setValue(selectedOption.name);
+    //   }
+    // }
+  }
+  checkCheckbox(control, optionName) {
+    if (control.value === true) {
+      const selectedOption = this.config.options.filter(option => {
+        return option.name === optionName;
+      });
+      // console.log(selectedOption[0].name);
+      console.log(this.group.get(this.config.name).value);
+    }
+  }
+}
