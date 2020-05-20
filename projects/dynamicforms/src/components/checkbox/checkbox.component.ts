7272adf9a4a8681547d79892c2d52b5e598c0de8
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2020-05-20 19:12:48 +0200

    FSA-5008: individual controls implemented

--- projects/dynamicforms/src/components/checkbox/checkbox.component.ts
+++ projects/dynamicforms/src/components/checkbox/checkbox.component.ts
@@ -7,35 +7,29 @@ import { AbstractOptionsComponent } from '../abstract-options/abstract-options.c
 })
 export class CheckboxComponent extends AbstractOptionsComponent {
   selectedControlName: string;
-  selectedOptions: string[] = [];
+  selectedOptions: string;
 
   checkCheckbox(optionName) {
-    let selectedControlName;
     const selectedOption = this.config.options.filter(
       option => option.name === optionName
     );
     if (
       this.group.controls[this.config.name].value === true &&
-      !this.selectedOptions.includes(selectedOption[0].name)
+      this.selectedOptions !== selectedOption[0].name
     ) {
-      this.selectedOptions.push(selectedOption[0].name);
-      selectedControlName = `added_${this.config.name}`;
-      if (this.group.get(selectedControlName)) {
-        this.group.get(selectedControlName).patchValue(this.selectedOptions);
+      this.selectedOptions = selectedOption[0].name;
+      if (this.group.get(this.selectedOptions)) {
+        this.group.get(this.selectedOptions).setValue(false);
       } else {
         this.group.addControl(
-          selectedControlName,
-          this.fb.control(this.selectedOptions)
+          this.selectedOptions,
+          this.fb.control(this.selectedOptions, this.fb.control(''))
         );
+        this.group.get(this.selectedOptions).setValue(true);
       }
     } else {
-      this.selectedOptions = this.selectedOptions.filter(
-        ctrl => ctrl !== selectedOption[0].name
-      );
-      if (this.group.get(`added_${this.config.name}`)) {
-        this.group
-          .get(`added_${this.config.name}`)
-          .patchValue(this.selectedOptions);
+      if (this.group.get(this.selectedOptions)) {
+        this.group.get(this.selectedOptions).setValue(false);
       }
     }
     console.log(this.group.value);
