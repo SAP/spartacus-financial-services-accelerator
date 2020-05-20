1f0383eb9aa1ec1d1fdcd9c17fd68ece765b8869
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2020-05-20 17:57:25 +0200

    FSA-5008: added prefilled check

--- projects/dynamicforms/src/components/checkbox/checkbox.component.ts
+++ projects/dynamicforms/src/components/checkbox/checkbox.component.ts
@@ -14,7 +14,10 @@ export class CheckboxComponent extends AbstractOptionsComponent {
     const selectedOption = this.config.options.filter(
       option => option.name === optionName
     );
-    if (!this.selectedOptions.includes(selectedOption[0].name)) {
+    if (
+      this.group.controls[this.config.name].value === true &&
+      !this.selectedOptions.includes(selectedOption[0].name)
+    ) {
       this.selectedOptions.push(selectedOption[0].name);
       selectedControlName = `added_${this.config.name}`;
       if (this.group.get(selectedControlName)) {
@@ -29,9 +32,11 @@ export class CheckboxComponent extends AbstractOptionsComponent {
       this.selectedOptions = this.selectedOptions.filter(
         ctrl => ctrl !== selectedOption[0].name
       );
-      this.group
-        .get(`added_${this.config.name}`)
-        .patchValue(this.selectedOptions);
+      if (this.group.get(`added_${this.config.name}`)) {
+        this.group
+          .get(`added_${this.config.name}`)
+          .patchValue(this.selectedOptions);
+      }
     }
     console.log(this.group.value);
   }
