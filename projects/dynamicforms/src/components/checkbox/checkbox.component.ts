e12bae2d626553f433bd803b28d8f4613af0b1c4
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2020-05-22 16:28:20 +0200

    FSA-5008: Working version with nested formcontrols

--- projects/dynamicforms/src/components/checkbox/checkbox.component.ts
+++ projects/dynamicforms/src/components/checkbox/checkbox.component.ts
@@ -17,29 +17,26 @@ export class CheckboxComponent extends AbstractOptionsComponent {
     this.config.options.forEach(() => {
       this.addedControls(this.config.name).push(this.fb.control(''));
     });
-    console.log(this.group.value);
   }
 
-  checkCheckbox(event, optionName, i) {
+  checkCheckbox(event, optionName) {
+    if (this.checkBoxArray.value.includes('')) {
+      this.addedControls(this.config.name).controls = []; // needed to clear the initial empty string values from fromArray
+    }
+    // if checkbox is checked and array doesn't contain checked value, insert it into array
     if (
       event.target.checked &&
       !this.checkBoxArray.value.includes(optionName)
     ) {
-      if (this.checkBoxArray.value.includes('')) {
-        this.addedControls(this.config.name).controls.splice(
-          0,
-          this.addedControls(this.config.name).controls.length
-        );
-      }
       this.addedControls(this.config.name).push(this.fb.control(optionName));
+      // if checkbox is unchecked and array contains value that has been unchecked, remove it from array
     } else {
       this.addedControls(this.config.name).removeAt(
         this.group
           .get(this.config.name)
-          .value.findIndex(elem => elem === this.selectedOptions)
+          .value.findIndex(elem => elem.name !== optionName)
       );
     }
-    console.log(this.group.value);
   }
   addedControls(addedControl): FormArray {
     return this.group.get(addedControl) as FormArray;
