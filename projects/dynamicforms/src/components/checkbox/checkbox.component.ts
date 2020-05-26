02843e5d260357c4b5c2473791ef886695e4c8bc
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2020-05-26 14:15:25 +0200

    FSA-5008: added one check on init

--- projects/dynamicforms/src/components/checkbox/checkbox.component.ts
+++ projects/dynamicforms/src/components/checkbox/checkbox.component.ts
@@ -14,22 +14,22 @@ export class CheckboxComponent extends AbstractOptionsComponent {
   ngOnInit() {
     super.ngOnInit();
     this.checkBoxArray = this.addedControls(this.config.name);
-    this.config.options.forEach(() => {
-      this.addedControls(this.config.name).push(this.fb.control(''));
-    });
+    if (this.checkBoxArray.value.length === 0) {
+      this.config.options.forEach(() => {
+        this.checkBoxArray.push(this.fb.control(''));
+      });
+    }
   }
 
   checkCheckbox(event, optionName, index) {
     if (this.checkBoxArray.value.includes('')) {
-      this.checkBoxArray.controls = []; // needed to clear the initial empty string values from fromArray
+      this.checkBoxArray.controls = [];
     }
-    // if checkbox is checked and array doesn't contain checked value, insert it into array
     if (
       event.target.checked &&
       !this.checkBoxArray.value.includes(optionName)
     ) {
       this.checkBoxArray.insert(index, this.fb.control(optionName));
-      // if checkbox is unchecked and array contains value that has been unchecked, remove it from array
     } else {
       this.checkBoxArray.removeAt(index);
     }
