8eb8ed12745157d1d6b690da85df1677620bbd94
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2020-05-26 13:28:50 +0200

    FSA-5008: Fully tested example without prepopulation

--- projects/dynamicforms/src/components/checkbox/checkbox.component.ts
+++ projects/dynamicforms/src/components/checkbox/checkbox.component.ts
@@ -19,23 +19,19 @@ export class CheckboxComponent extends AbstractOptionsComponent {
     });
   }
 
-  checkCheckbox(event, optionName) {
+  checkCheckbox(event, optionName, index) {
     if (this.checkBoxArray.value.includes('')) {
-      this.addedControls(this.config.name).controls = []; // needed to clear the initial empty string values from fromArray
+      this.checkBoxArray.controls = []; // needed to clear the initial empty string values from fromArray
     }
     // if checkbox is checked and array doesn't contain checked value, insert it into array
     if (
       event.target.checked &&
       !this.checkBoxArray.value.includes(optionName)
     ) {
-      this.addedControls(this.config.name).push(this.fb.control(optionName));
+      this.checkBoxArray.insert(index, this.fb.control(optionName));
       // if checkbox is unchecked and array contains value that has been unchecked, remove it from array
     } else {
-      this.addedControls(this.config.name).removeAt(
-        this.group
-          .get(this.config.name)
-          .value.findIndex(elem => elem.name !== optionName)
-      );
+      this.checkBoxArray.removeAt(index);
     }
   }
   addedControls(addedControl): FormArray {
