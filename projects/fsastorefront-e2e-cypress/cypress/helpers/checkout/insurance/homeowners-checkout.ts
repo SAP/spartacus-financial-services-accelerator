7be6cbdcf6b14086e09ddf50473693c66104ea3d
NC\strajkovic <s.trajkovic@netconomy.net>
2020-05-28 18:55:13 +0200

    FSA-5018: Adds homeowners and renters test

---
+++ projects/fsastorefront-e2e-cypress/cypress/helpers/checkout/insurance/homeowners-checkout.ts
@@ -0,0 +1,95 @@
+import * as shared from '../shared-checkout';
+
+export function checkHomeownersComparisonTable() {
+  const comparisonTableContent: addOptionsPage.ComparisonTable = {
+    mainProducts: [
+      {
+        name: 'Homeowners Monthly',
+        price: '€34.50',
+      },
+      {
+        name: 'Homeowners Annually',
+        price: '€338.10',
+      },
+    ],
+  };
+  shared.checkComparisonTable(comparisonTableContent);
+}
+
+export function selectHomeownersAnnually() {
+  cy.get('cx-fs-comparison-table-panel-item')
+    .eq(1)
+    .within(() => {
+      cy.get('.table-header-title').should('have.text', 'Homeowners Annually');
+      cy.get('.primary-button').click();
+    });
+}
+
+export function checkOptionalProducts() {
+  const addOptionsContent: addOptionsPage.AddOptions = {
+    title: 'Your Homeowners Insurance',
+    items: [
+      {
+        name: 'Flood Cover',
+        available: true,
+      },
+      {
+        name: 'Temporary Accomodation Cover',
+        available: true,
+      },
+      {
+        name: 'Jewelry and Watches Cover',
+        available: true,
+        shouldAdd: true,
+      },
+      {
+        name: 'Bicycles Cover',
+        available: true,
+      },
+    ],
+  };
+  shared.checkAddOptionsPageContent(addOptionsContent);
+}
+
+export function checkMiniCartHomeowners() {
+  const miniCartContent: addOptionsPage.MiniCart = {
+    price: ' €491.25 ',
+    products: [
+      {
+        title: ' Homeowners Annually: ',
+        value: ' €338.10 ',
+      },
+      {
+        title: ' Jewelry and Watches Cover: ',
+        value: ' €153.15 ',
+      },
+    ],
+  };
+  shared.checkMiniCart(miniCartContent);
+}
+
+export function populateHomeownersSpecific() {
+  cy.get('[name=propertyDetailsCoverRequired]')
+    .eq(0)
+    .click();
+  cy.get('[name=propertyValue]').type('200000');
+  cy.get('[name=propertyRebuildCost]').type('25000');
+}
+
+export function populateBuildingCover() {
+  cy.get('[name=numberOfYearsHoldingInsurance]').type('12');
+
+  cy.get('h4').contains('Your Building Cover');
+  cy.get('[name=alreadyHeldInsurance]').select('3');
+  cy.get('[name="accidentalDamageCoverBuilding"]')
+    .eq(0)
+    .click();
+}
+
+export function checkHomeownersPolicy() {
+  cy.get('.title').contains('Homeowners Insurance');
+  cy.get('.label').contains('Property type');
+  cy.get('.value').contains('House');
+  cy.get('.label').contains('Premium');
+  cy.get('.value').contains('€491.25');
+}
---
+++ projects/fsastorefront-e2e-cypress/cypress/integration/checkout/insurance/renters-checkout.e2e-spec.ts
@@ -0,0 +1,92 @@
+import { registrationUserWithoutPhone } from '../../../sample-data/users';
+import * as register from '../../../helpers/register';
+import * as renters from '../../../helpers/checkout/insurance/renters-checkout';
+import * as checkout from '../../../helpers/checkout/checkoutSteps';
+import {
+  addPaymentMethod,
+  selectPaymentMethod,
+} from '../../../helpers/checkout/insurance/payment';
+import { checkMyPoliciesPage } from '../../../helpers/my-account/policies';
+
+context('Renters Checkout', () => {
+  before(() => {
+    cy.visit('/');
+  });
+
+  it('Should open renters category page', () => {
+    checkout.startInsuranceCheckout('Renters');
+  });
+
+  it('Should populate first page in checkout', () => {
+    checkout.checkProgressBarInsurance('Your Renters Insurance');
+    checkout.checkFirstCheckoutStep('Renters');
+    checkout.populatePropertyDetails();
+    checkout.populateContentsCover();
+    checkout.populatePropertyAddress();
+    checkout.clickContinueButton();
+  });
+
+  it('Should check comparison table', () => {
+    checkout.checkInsuranceComparisonPage('Your Renters Insurance', '2');
+    renters.checkRentersComparisonTable();
+    renters.selectRentersMonthly();
+  });
+
+  it('Should check add options page', () => {
+    renters.checkOptionalProducts();
+    renters.checkMiniCartRenters();
+    checkout.removeOptionalProduct('Bicycles Cover');
+    renters.checkMiniCartRentersRemovedProduct();
+    checkout.clickContinueButton();
+  });
+
+  it('Should be able to register user without phone number', () => {
+    register.populateRegistrationForm(registrationUserWithoutPhone);
+    register.loginInUser(
+      registrationUserWithoutPhone.email,
+      registrationUserWithoutPhone.password
+    );
+  });
+
+  it('Should populate personal details page', () => {
+    checkout.checkPersonalDetailsPageInsurance();
+    checkout.populatePersonalDetailsPage();
+  });
+
+  it('Should check quote review page', () => {
+    checkout.checkProgressBarInsurance('Renters');
+    renters.checkMiniCartRentersRemovedProduct();
+    checkout.clickContinueButton();
+    checkout.checkAccordions('propertyQuoteReview');
+    addPaymentMethod(registrationUserWithoutPhone.email);
+    checkout.clickContinueButton();
+    checkout.ConfirmBindQuote();
+  });
+
+  it('Select default payment details', () => {
+    selectPaymentMethod();
+  });
+
+  it('Place order on final review page', () => {
+    checkout.placeOrderOnFinalReview();
+  });
+
+  it('Check order confirmation', () => {
+    checkout.checkAccordions('rentersFinalReview');
+    checkout.checkOrderConfirmation();
+  });
+
+  it('Check my policies page', () => {
+    checkMyPoliciesPage();
+    renters.checkRentersPolicy();
+  });
+
+  it('Should validate phone number', () => {
+    register.validatePhoneNumber('');
+    checkout.checkMyAccountEmptyPages('Claims', 'You have no Claims!');
+    checkout.checkMyAccountEmptyPages(
+      'Quotes & Applications',
+      'You have no Quotes and Applications!'
+    );
+  });
+});
