2d26917b1be0dd22d1ad2ffb2a9a6515d4fb834e
Ivan Ilic <i.ilic@msgnetconomy.net>
2020-06-01 15:21:56 +0200

    FSA-4479: Fix interface description

--- projects/dynamicforms/src/core/resolver/prefill-resolver.interface.ts
+++ projects/dynamicforms/src/core/resolver/prefill-resolver.interface.ts
@@ -1,12 +1,11 @@
 /**
- * @interface
  * An interface representing prefill resolver used for getting data from application state.
  */
 export interface PrefillResolver {
   /**
    * Method used to get state object property
    *
-   * @param fieldPath Path to the property
+   * @param fieldPath  Path to the property
    */
   getFieldValue(fieldPath: string);
 }
