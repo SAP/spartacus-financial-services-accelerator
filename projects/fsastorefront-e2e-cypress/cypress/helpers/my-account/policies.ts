9abc30a6ea4535bae6ca58def966fec6e4fdd56b
NC\strajkovic <s.trajkovic@netconomy.net>
2020-06-03 17:11:08 +0200

    FSA-5096: Fix PR comments

--- projects/fsastorefront-e2e-cypress/cypress/helpers/my-account/policies.ts
+++ projects/fsastorefront-e2e-cypress/cypress/helpers/my-account/policies.ts
@@ -1,9 +1,3 @@
-/*export const POLICIES_PAGE = '/my-account/my-policies/';
-
-export function accessPoliciesPage() {
-  cy.visit(POLICIES_PAGE);
-}*/
-
 export function checkPoliciesTitle() {
   cy.get('.heading-headline').should('contain', 'Policies');
 }
