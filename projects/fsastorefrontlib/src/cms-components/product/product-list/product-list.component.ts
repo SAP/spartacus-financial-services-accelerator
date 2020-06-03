f353a05e669e8cae1c7a74f7b9a3dc7e332ef8ef
Ivan Ilic <i.ilic@msgnetconomy.net>
2020-06-03 13:46:19 +0200

    FSA-4944: Remove local pageChange metod

--- projects/fsastorefrontlib/src/cms-components/product/product-list/product-list.component.ts
+++ projects/fsastorefrontlib/src/cms-components/product/product-list/product-list.component.ts
@@ -1,24 +1,8 @@
 import { Component } from '@angular/core';
-import {
-  ProductListComponent,
-  PageLayoutService,
-  ProductListComponentService,
-  ViewConfig,
-} from '@spartacus/storefront';
+import { ProductListComponent } from '@spartacus/storefront';
 
 @Component({
   selector: 'cx-fs-product-list',
   templateUrl: './product-list.component.html',
 })
-export class FSProductListComponent extends ProductListComponent {
-  constructor(
-    protected fsPageLayoutService: PageLayoutService,
-    protected fsProductListComponentService: ProductListComponentService,
-    public fsScrollConfig?: ViewConfig
-  ) {
-    super(fsPageLayoutService, fsProductListComponentService, fsScrollConfig);
-  }
-  viewPage(pageNumber: number) {
-    this.fsProductListComponentService.viewPage(pageNumber);
-  }
-}
+export class FSProductListComponent extends ProductListComponent {}
