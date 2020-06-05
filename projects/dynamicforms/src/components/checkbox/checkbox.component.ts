591173524a7722d7b4cdb705557edd8e5260e6bb
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2020-06-05 11:35:32 +0200

    FSA-5008: Version with array being populated all the time

--- projects/dynamicforms/src/components/checkbox/checkbox.component.ts
+++ projects/dynamicforms/src/components/checkbox/checkbox.component.ts
@@ -14,39 +14,37 @@ export class CheckboxComponent extends AbstractOptionsComponent {
   ngOnInit() {
     super.ngOnInit();
     this.checkBoxArray = this.addedControls(this.config.name);
-    this.checkedItems$ = this.checkBoxArray.valueChanges; // this always gives us the state of formArray, might be used in HTML?
-
-    // this checks if the formArray is comming from formBuilder (it's only instantiated there)
-    // and then fills it with formControls of default value - empty string
-    // If we were to fill the controls with config.options.name, they would be all preselected when the form is loaded for the first time
-    if (this.checkBoxArray.value.length === 0) {
-      this.config.options.forEach(() => {
-        this.checkBoxArray.push(this.fb.control(''));
-      });
-    }
+    this.checkedItems$ = this.checkBoxArray.valueChanges;
+    this.initializeCheckboxes();
     this.checkedItems$.subscribe(data => console.log(data));
   }
 
   checkCheckbox(e, optionName, index) {
-    // when checkbox is clicked the default empty strings are cleared and the formArray is populated with the clicked values
-    // this has to be done because if 1 checkbox is checked and then unchecked,
-    // if array hasn't been cleared the validation won't fire, because array would still hold something like this [true, "", "", ""]
     if (e.target.checked) {
-      if (this.checkBoxArray.value.includes('')) {
+      this.initializeCheckboxes();
+      this.populateCheckboxes(index, optionName);
+    } else {
+      this.populateCheckboxes(index, false);
+      const isEmpty = this.checkBoxArray.value.every(
+        element => element === false
+      );
+      if (isEmpty) {
         this.checkBoxArray.clear();
       }
-      this.checkBoxArray.insert(index, this.fb.control(optionName));
-    } else {
-      // cannot use index from HTML template, because it doesn't return the items in their previous index place
-      for (let i = 0; i < this.checkBoxArray.value.length; ) {
-        this.checkBoxArray.controls.forEach(item => {
-          if (item.value === optionName) {
-            this.checkBoxArray.removeAt(i);
-          }
-          i++;
-        });
-      }
     }
+  }
+
+  initializeCheckboxes() {
+    if (this.checkBoxArray.value.length === 0) {
+      this.config.options.forEach(() => {
+        this.checkBoxArray.push(this.fb.control(false));
+      });
+    }
+  }
+
+  populateCheckboxes(index, optionName) {
+    this.checkBoxArray.removeAt(index);
+    this.checkBoxArray.insert(index, this.fb.control(optionName));
   }
 
   addedControls(addedControl): FormArray {
