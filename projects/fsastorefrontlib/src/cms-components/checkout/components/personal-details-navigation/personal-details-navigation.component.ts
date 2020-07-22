f87eb8b2f9ce4c93acc93af4a8e08146af4ea5ce
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2020-07-22 11:03:04 +0200

    FSA-5201: Bug fixed

--- projects/fsastorefrontlib/src/cms-components/checkout/components/personal-details-navigation/personal-details-navigation.component.ts
+++ projects/fsastorefrontlib/src/cms-components/checkout/components/personal-details-navigation/personal-details-navigation.component.ts
@@ -4,7 +4,7 @@ import { ActivatedRoute } from '@angular/router';
 import { FormDataService, YFormData } from '@fsa/dynamicforms';
 import { Cart, RoutingService } from '@spartacus/core';
 import { Subscription } from 'rxjs';
-import { map, take } from 'rxjs/operators';
+import { map, take, switchMap } from 'rxjs/operators';
 import { FSOrderEntry } from '../../../../occ/occ-models';
 import { FSCartService } from './../../../../core/cart/facade/cart.service';
 import { FSCheckoutConfigService } from './../../../../core/checkout/services/checkout-config.service';
@@ -36,52 +36,41 @@ export class PersonalDetailsNavigationComponent implements OnInit, OnDestroy {
   }
 
   navigateNext() {
-    this.subscription
-      .add(
-        this.cartService
-          .getActive()
-          .pipe(
-            take(1),
-            map((cart: Cart) => {
-              if (
-                cart &&
-                cart.code &&
-                cart.entries &&
-                cart.entries.length > 0
-              ) {
-                this.cartId = cart.code;
-                const entry: FSOrderEntry = cart.entries[0];
-                const yFormData: YFormData = {
-                  refId: cart.code + '_' + cart.entries[0].entryNumber,
-                };
-                if (entry.formData && entry.formData.length > 0) {
-                  yFormData.id = entry.formData[0].id;
+    this.subscription.add(
+      this.cartService
+        .getActive()
+        .pipe(
+          take(1),
+          switchMap((cart: Cart) => {
+            if (cart && cart.code && cart.entries && cart.entries.length > 0) {
+              this.cartId = cart.code;
+              const entry: FSOrderEntry = cart.entries[0];
+              const yFormData: YFormData = {
+                refId: cart.code + '_' + cart.entries[0].entryNumber,
+              };
+              if (entry.formData && entry.formData.length > 0) {
+                yFormData.id = entry.formData[0].id;
+              }
+              this.formService.submit(yFormData);
+            }
+            return this.formService.getSubmittedForm().pipe(
+              map(formData => {
+                if (formData && formData.content) {
+                  this.quoteService.underwriteQuote(cart.code);
+                  this.quoteService.updateQuote(
+                    this.cartId,
+                    this.pricingService.buildPricingData(
+                      JSON.parse(formData.content)
+                    )
+                  );
+                  this.routingService.go(this.checkoutStepUrlNext);
                 }
-                this.quoteService.underwriteQuote(cart.code);
-                this.formService.submit(yFormData);
-              }
-            })
-          )
-          .subscribe()
-      )
-      .add(
-        this.formService
-          .getSubmittedForm()
-          .pipe(
-            map(formData => {
-              if (formData && formData.content) {
-                this.quoteService.updateQuote(
-                  this.cartId,
-                  this.pricingService.buildPricingData(
-                    JSON.parse(formData.content)
-                  )
-                );
-                this.routingService.go(this.checkoutStepUrlNext);
-              }
-            })
-          )
-          .subscribe()
-      );
+              })
+            );
+          })
+        )
+        .subscribe()
+    );
   }
 
   ngOnDestroy() {
