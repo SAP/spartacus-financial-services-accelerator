a75fce96984c146dad570e4103e2155341b05d9f
Bojan Jevtic <b.jevtic@msgnetconomy.net>
2020-08-19 14:01:52 +0200

    FSA-5189: Reverts karma configuration

--- projects/fsastorefrontlib/karma.conf.js
+++ projects/fsastorefrontlib/karma.conf.js
@@ -1,27 +1,22 @@
 // Karma configuration file, see link for more information
 // https://karma-runner.github.io/1.0/config/configuration-file.html
+
 module.exports = function (config) {
   config.set({
     basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
+      require('karma-coverage'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
       require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
-    browserConsoleLogOptions: {
-      level: 'log',
-      terminal: true,
-    },
     client: {
-      captureConsole: true,
-      mocha: {
-        bail: true,
-      },
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
     },
+    reporters: ['progress', 'kjhtml', 'coverage-istanbul', 'dots'],
     coverageIstanbulReporter: {
       dir: require('path').join(__dirname, '../../coverage'),
       reports: ['lcov', 'cobertura', 'text-summary'],
@@ -33,12 +28,11 @@ module.exports = function (config) {
       },
       fixWebpackSourcePaths: true,
     },
-    reporters: ['progress', 'kjhtml'],
     port: 9876,
     colors: true,
-    logLevel: config.LOG_LOG,
+    logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['ChromeHeadless'],
     singleRun: false,
   });
 };
