deba08e8287733d46cccc58755421bf6be5e4553
Srdjan Damnjanovic <s.damnjanovic@msgnetconomy.net>
2020-09-03 14:48:06 +0200

    FSA-5281: removed tag, not needed

--- projects/dynamicforms/src/components/data-holder/data-holder.component.ts
+++ projects/dynamicforms/src/components/data-holder/data-holder.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 import { AbstractFormComponent } from '../abstract-form/abstract-form.component';
 
 @Component({
-  selector: 'cx-data-holder',
   template: '',
 })
 export class DataHolderComponent extends AbstractFormComponent {}
