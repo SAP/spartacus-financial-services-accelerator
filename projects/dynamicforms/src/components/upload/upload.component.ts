039ccb9bf2b2ad29a488d838a82717c3a1021ce3
Srdjan Damnjanovic <Srdjan.damnjanovic@msg-global.com>
2020-09-22 14:02:25 +0200

    FSA-5326: Small adjustments for styling, removed unecessary code

--- projects/dynamicforms/src/components/upload/upload.component.ts
+++ projects/dynamicforms/src/components/upload/upload.component.ts
@@ -18,14 +18,14 @@ import { FormService } from './../../core/services/form/form.service';
   templateUrl: './upload.component.html',
 })
 export class UploadComponent extends AbstractFormComponent implements OnInit {
-  fileList: string[];
-  fileSize: number[];
+  fileList: string[] = [];
+  fileSize: number[] = [];
   control$: Observable<any>;
 
   @HostListener('change', ['$event.target.files', '$event.target.accept'])
   handleFiles(files: FileList, fileType: string) {
-    this.fileList = [];
-    this.fileSize = [];
+    this.fileList = []; // reset when user is choosing files again
+    this.fileSize = []; // reset when user is choosing files again
     const fileList = Array.from(files);
     if (this.config.accept === fileType.toString()) {
       fileList.map(data => {
@@ -34,7 +34,7 @@ export class UploadComponent extends AbstractFormComponent implements OnInit {
       });
       this.group.get(this.config.name).setValue(this.fileList);
     } else {
-      this.group.get(this.config.name).reset();
+      this.group.get(this.config.name).setValue(null); // triggering validation if nothing is selected
     }
   }
 
@@ -52,10 +52,8 @@ export class UploadComponent extends AbstractFormComponent implements OnInit {
   ngOnInit() {
     super.ngOnInit();
     this.control$ = this.group.get(this.config.name).valueChanges;
-    this.fileList = [];
     this.control$.subscribe(data => {
       this.fileList = [...data];
-      console.log(this.fileList);
     });
   }
 }
