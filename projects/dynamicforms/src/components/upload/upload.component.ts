3834ebffaa208f9aae815d8b558150ebd5016dde
Srdjan Damnjanovic <Srdjan.damnjanovic@msg-global.com>
2020-09-23 15:15:03 +0200

    ngx-upload-poc: initial commit

--- projects/dynamicforms/src/components/upload/upload.component.ts
+++ projects/dynamicforms/src/components/upload/upload.component.ts
@@ -1,4 +1,14 @@
-import { Component, HostListener } from '@angular/core';
+import { Component, EventEmitter, HostListener, Injector } from '@angular/core';
+import { LanguageService } from '@spartacus/core';
+import {
+  UploadOutput,
+  UploadInput,
+  UploadFile,
+  humanizeBytes,
+  UploaderOptions,
+} from 'ngx-uploader';
+import { DynamicFormsConfig } from '../../core/config/form-config';
+import { FormService } from '../../core/services/form/form.service';
 import { AbstractFormComponent } from '../abstract-form/abstract-form.component';
 
 @Component({
@@ -6,40 +16,93 @@ import { AbstractFormComponent } from '../abstract-form/abstract-form.component'
   templateUrl: './upload.component.html',
 })
 export class UploadComponent extends AbstractFormComponent {
-  fileList: File[] = [];
+  options: UploaderOptions;
+  formData: FormData;
+  files: UploadFile[];
+  uploadInput: EventEmitter<UploadInput>;
+  humanizeBytes: Function;
+  dragOver: boolean;
 
-  @HostListener('change', ['$event'])
-  handleFiles(event) {
-    this.fileList = []; // reset when user is choosing files again
-    const uploadControl = this.group.get(this.config.name);
-    if (
-      this.config.accept === event.target.accept.toString() &&
-      this.config.multiple === event.target.multiple &&
-      this.checkFileSize(event)
-    ) {
-      this.fileList = Array.from(event.target.files);
-      uploadControl.setValue(this.fileList);
-    } else {
-      // triggering validation if nothing is selected
-      uploadControl.markAsTouched({ onlySelf: true });
-      uploadControl.setValue(null);
+  constructor(
+    protected appConfig: DynamicFormsConfig,
+    protected languageService: LanguageService,
+    protected injector: Injector,
+    protected formService: FormService
+  ) {
+    super(appConfig, languageService, injector, formService);
+
+    this.options = { concurrency: 1, maxUploads: 3, maxFileSize: 100 };
+    this.files = []; // local uploading files array
+    this.uploadInput = new EventEmitter<UploadInput>(); // input events, we use this to emit data to ngx-uploader
+    this.humanizeBytes = humanizeBytes;
+  }
+
+  onUploadOutput(output: UploadOutput): void {
+    switch (output.type) {
+      case 'allAddedToQueue':
+        // uncomment this if you want to auto upload files when added
+        // const event: UploadInput = {
+        //   type: 'uploadAll',
+        //   url: '/upload',
+        //   method: 'POST',
+        //   data: { foo: 'bar' }
+        // };
+        // this.uploadInput.emit(event);
+        break;
+      case 'addedToQueue':
+        if (typeof output.file !== 'undefined') {
+          this.files.push(output.file);
+        }
+        break;
+      case 'uploading':
+        if (typeof output.file !== 'undefined') {
+          // update current data in files array for uploading file
+          const index = this.files.findIndex(
+            file =>
+              typeof output.file !== 'undefined' && file.id === output.file.id
+          );
+          this.files[index] = output.file;
+        }
+        break;
+      case 'removed':
+        // remove file from array when removed
+        this.files = this.files.filter(
+          (file: UploadFile) => file !== output.file
+        );
+        break;
+      case 'dragOver':
+        this.dragOver = true;
+        break;
+      case 'dragOut':
+      case 'drop':
+        this.dragOver = false;
+        break;
+      case 'done':
+        // The file is downloaded
+        break;
     }
   }
 
-  bytesToSize(bytes: number) {
-    const sizes = ['Bytes', 'KB', 'MB'];
-    const i = Number(Math.floor(Math.log(bytes) / Math.log(1024)));
-    if (i === 0) {
-      return `${bytes} ${sizes[i]})`;
-    }
-    return `${(bytes / 1024 ** i).toFixed(1)} ${sizes[i]}`;
+  startUpload(): void {
+    const event: UploadInput = {
+      type: 'uploadAll',
+      url: 'http://ngx-uploader.com/upload',
+      method: 'POST',
+      data: { foo: 'bar' },
+    };
+
+    this.uploadInput.emit(event);
   }
 
-  checkFileSize(event): Boolean {
-    const files: File[] = Array.from(event.target.files);
-    const maxExceeded = files.filter(
-      file => file.size / 1024 / 1024 > this.config.maxFileSize
-    );
-    return !(maxExceeded.length > 0);
+  cancelUpload(id: string): void {
+    this.uploadInput.emit({ type: 'cancel', id: id });
+  }
+
+  removeFile(id: string): void {
+    this.uploadInput.emit({ type: 'remove', id: id });
+  }
+
+  removeAllFiles(): void {
+    this.uploadInput.emit({ type: 'removeAll' });
   }
 }
