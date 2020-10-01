2c5ca8849802180e35f13c2d6cde29637a32aa93
Srdjan Damnjanovic <Srdjan.damnjanovic@msg-global.com>
2020-10-01 10:24:24 +0200

    FSA-5326: Reverted code to custom implementation

--- projects/dynamicforms/src/components/upload/upload.component.ts
+++ projects/dynamicforms/src/components/upload/upload.component.ts
@@ -1,60 +1,36 @@
-import { Component, EventEmitter, Injector, OnInit } from '@angular/core';
-import { AbstractControl } from '@angular/forms';
-import { LanguageService } from '@spartacus/core';
-import {
-  UploadOutput,
-  UploadInput,
-  UploadFile,
-  humanizeBytes,
-  UploaderOptions,
-} from 'ngx-uploader';
-import { DynamicFormsConfig } from '../../core/config/form-config';
-import { FormService } from '../../core/services/form/form.service';
+import { Component, HostListener } from '@angular/core';
 import { AbstractFormComponent } from '../abstract-form/abstract-form.component';
 
 @Component({
   selector: 'cx-upload',
   templateUrl: './upload.component.html',
 })
-export class UploadComponent extends AbstractFormComponent implements OnInit {
-  options: UploaderOptions;
-  formData: FormData;
-  files: UploadFile[];
-  uploadInput: EventEmitter<UploadInput>;
-  humanizeBytes: Function;
-  dragOver: boolean;
+export class UploadComponent extends AbstractFormComponent {
+  fileList: File[] = [];
 
-  uploadControl: AbstractControl;
-  filteredFiles: boolean;
-
-  constructor(
-    protected appConfig: DynamicFormsConfig,
-    protected languageService: LanguageService,
-    protected injector: Injector,
-    protected formService: FormService
-  ) {
-    super(appConfig, languageService, injector, formService);
+  @HostListener('change', ['$event'])
+  handleFiles(event) {
+    this.fileList = []; // reset when user is choosing files again
+    const uploadControl = this.group.get(this.config.name);
+    if (
+      this.config.accept === event.target.accept.toString() &&
+      this.config.multiple === event.target.multiple &&
+      this.checkFileSize(event)
+    ) {
+      this.fileList = Array.from(event.target.files);
+      uploadControl.setValue(this.fileList);
+    } else {
+      // triggering validation if nothing is selected
+      uploadControl.markAsTouched({ onlySelf: true });
+      uploadControl.setValue(null);
+    }
   }
 
-  ngOnInit() {
-    super.ngOnInit();
-    this.files = []; // local uploading files array
-    this.uploadInput = new EventEmitter<UploadInput>(); // input events, we use this to emit data to ngx-uploader
-    this.humanizeBytes = humanizeBytes;
-    this.options = {
-      concurrency: 1,
-      maxUploads: this.config?.maxUploads,
-      maxFileSize: this.config?.maxFileSize,
-      allowedContentTypes: this.config?.accept,
-    };
-    this.uploadControl = this.group.get(this.config.name);
-  }
-
-  convertFileSize(bytes: number) {
+  bytesToSize(bytes: number) {
     const sizes = ['Bytes', 'KB', 'MB'];
     const i = Number(Math.floor(Math.log(bytes) / Math.log(1024)));
     if (i === 0) {
-      return `${bytes} ${sizes[i]}`;
+      return `${bytes} ${sizes[i]})`;
     }
     return `${(bytes / 1024 ** i).toFixed(1)} ${sizes[i]}`;
   }
@@ -65,66 +41,5 @@ export class UploadComponent extends AbstractFormComponent implements OnInit {
       file => file.size / 1024 / 1024 > this.config.maxFileSize
     );
     return !(maxExceeded.length > 0);
-  }
-
-  onUploadOutput(output: UploadOutput): void {
-    switch (output.type) {
-      case 'addedToQueue':
-        if (typeof output.file !== 'undefined') {
-          this.files.push(output.file);
-          this.uploadControl.setValue(this.files);
-        }
-        break;
-      case 'uploading':
-        if (typeof output.file !== 'undefined') {
-          // update current data in files array for uploading file
-          const index = this.files.findIndex(
-            file =>
-              typeof output.file !== 'undefined' && file.id === output.file.id
-          );
-          this.files[index] = output.file;
-        }
-        break;
-      case 'removed':
-        // remove file from array when removed
-        this.files = this.files.filter(
-          (file: UploadFile) => file !== output.file
-        );
-        this.uploadControl.setValue(this.files);
-        break;
-      // case 'dragOver':
-      //   this.dragOver = true;
-      //   break;
-      // case 'dragOut':
-      // case 'drop':
-      //   this.dragOver = false;
-      //   break;
-      case 'done':
-        // The file is downloaded
-        break;
-    }
-  }
-
-  startUpload(): void {
-    const event: UploadInput = {
-      type: 'uploadAll',
-      url: 'http://ngx-uploader.com/upload',
-      method: 'POST',
-      data: { foo: 'bar' },
-    };
-
-    this.uploadInput.emit(event);
-  }
-
-  cancelUpload(id: string): void {
-    this.uploadInput.emit({ type: 'cancel', id: id });
-  }
-
-  removeFile(id: string): void {
-    this.uploadInput.emit({ type: 'remove', id: id });
-  }
-
-  removeAllFiles(): void {
-    this.uploadInput.emit({ type: 'removeAll' });
   }
 }
