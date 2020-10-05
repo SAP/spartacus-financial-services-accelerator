417bc96897d8ef4c1433a8790dc9558e1d9fb360
Bojan Jevtic <bojan.jevtic@msg-global.com>
2020-10-05 09:48:52 +0200

    FSA-5316: Implements adding of new insured object during change process

--- projects/fsastorefrontlib/src/cms-components/change-process/change-car-details-navigation/change-car-details-navigation.component.ts
+++ projects/fsastorefrontlib/src/cms-components/change-process/change-car-details-navigation/change-car-details-navigation.component.ts
@@ -6,8 +6,13 @@ import { ActivatedRoute } from '@angular/router';
 import { GlobalMessageService, RoutingService } from '@spartacus/core';
 import { AbstractChangeProcessStepComponent } from '../abstract-change-process-step/abstract-change-process-step.component';
 import { ChangePolicyService } from '../../../core/change-request/services/change-policy.service';
-import { YFormData, FormDataService } from '@fsa/dynamicforms';
+import {
+  YFormData,
+  FormDataService,
+  FormDataStorageService,
+} from '@fsa/dynamicforms';
 import { map, take, filter } from 'rxjs/operators';
+import { RequestType } from './../../../occ/occ-models';
 
 @Component({
   selector: 'cx-fs-change-car-details-navigation',
@@ -21,7 +26,8 @@ export class ChangeCarDetailsNavigationComponent extends AbstractChangeProcessSt
     protected routingService: RoutingService,
     protected globalMessageService: GlobalMessageService,
     protected changePolicyService: ChangePolicyService,
-    protected formDataService: FormDataService
+    protected formDataService: FormDataService,
+    protected formDataStoragetService: FormDataStorageService
   ) {
     super(
       userRequestNavigationService,
@@ -34,11 +40,7 @@ export class ChangeCarDetailsNavigationComponent extends AbstractChangeProcessSt
   }
 
   simulateChanges(changeRequest) {
-    if (
-      changeRequest?.insurancePolicy?.insuredObjectList?.insuredObjects.length >
-      0
-    ) {
-      let changedInsuredObject;
+    if (changeRequest?.insurancePolicy) {
       const yFormData: YFormData = {};
       this.formDataService.submit(yFormData);
       this.formDataService
@@ -47,35 +49,106 @@ export class ChangeCarDetailsNavigationComponent extends AbstractChangeProcessSt
           filter(formData => formData.content !== undefined),
           take(1),
           map(submittedFormData => {
-            const changeCarDetailsForm = JSON.parse(submittedFormData.content);
-            changeRequest.insurancePolicy.insuredObjectList.insuredObjects.forEach(
-              insuredObject => {
-                changedInsuredObject = {
-                  insuredObjectId: insuredObject.insuredObjectId,
-                  insuredObjectItems: [],
-                };
-                insuredObject.insuredObjectItems
-                  .filter(item => item.changeable)
-                  .forEach(item => {
-                    changedInsuredObject.insuredObjectItems.push({
-                      label: item.label,
-                      value: changeCarDetailsForm[item.label],
-                    });
-                  });
-              }
-            );
-            this.simulateChangeRequest({
-              requestId: changeRequest.requestId,
-              insurancePolicy: {
-                insuredObjectList: {
-                  insuredObjects: [changedInsuredObject],
+            const changeProcessForm = JSON.parse(submittedFormData.content);
+            if (
+              changeRequest.fsStepGroupDefinition?.requestType?.code ===
+              RequestType.INSURED_OBJECT_CHANGE
+            ) {
+              const changedInsuredObject = this.getChangedInsuredObject(
+                changeRequest,
+                changeProcessForm
+              );
+              this.simulateChangeRequest({
+                requestId: changeRequest.requestId,
+                insurancePolicy: {
+                  insuredObjectList: {
+                    insuredObjects: [changedInsuredObject],
+                  },
                 },
-              },
-              configurationSteps: changeRequest.configurationSteps,
-            });
+                configurationSteps: changeRequest.configurationSteps,
+              });
+            }
+            if (
+              changeRequest.fsStepGroupDefinition?.requestType?.code ===
+              RequestType.INSURED_OBJECT_ADD
+            ) {
+              const addedInsuredObject = this.getAddedInsuredObject(
+                changeRequest,
+                changeProcessForm
+              );
+              this.simulateChangeRequest({
+                requestId: changeRequest.requestId,
+                insurancePolicy: {
+                  insuredObjectList: {
+                    insuredObjects: [
+                      {
+                        insuredObjectId: this.getMainInsuredObjectId(changeRequest),
+                        childInsuredObjectList: {
+                          insuredObjects: [addedInsuredObject],
+                        },
+                      },
+                    ],
+                  },
+                },
+                configurationSteps: changeRequest.configurationSteps,
+              });
+            }
+            this.formDataStoragetService.clearFormDataIdFromLocalStorage(
+              submittedFormData.id
+            );
           })
         )
         .subscribe();
     }
   }
+
+  // pomeriti u servis
+  protected getChangedInsuredObject(
+    changeRequest: any,
+    changeProcessForm: any
+  ) {
+    let changedInsuredObject;
+    changeRequest.insurancePolicy?.insuredObjectList?.insuredObjects?.forEach(
+      insuredObject => {
+        changedInsuredObject = {
+          insuredObjectId: insuredObject.insuredObjectId,
+          insuredObjectItems: [],
+        };
+        insuredObject.insuredObjectItems
+          .filter(item => item.changeable)
+          .forEach(item => {
+            changedInsuredObject.insuredObjectItems.push({
+              label: item.label,
+              value: changeProcessForm[item.label],
+            });
+          });
+      }
+    );
+    return changedInsuredObject;
+  }
+
+  // stay here
+  protected getAddedInsuredObject(changeRequest, changeProcessForm) {
+    const newInsuredObject = {
+      insuredObjectType: this.getTypeOfChidlInsuredObject(changeRequest),
+      insuredObjectItems: [],
+    };
+    Object.keys(changeProcessForm).forEach(key => {
+      newInsuredObject.insuredObjectItems.push({
+        key: key,
+        label: key,
+        value: changeProcessForm[key],
+      });
+    });
+    return newInsuredObject;
+  }
+
+  private getMainInsuredObjectId(changeRequest) {
+    return changeRequest.insurancePolicy?.insuredObjectList?.insuredObjects[0]?.insuredObjectId;
+  }
+
+  private getTypeOfChidlInsuredObject(changeRequest) {
+    return changeRequest.insurancePolicy?.insuredObjectList?.insuredObjects[0]?.childInsuredObjectList?.insuredObjects[0]?.insuredObjectType;
+  }
+
 }
