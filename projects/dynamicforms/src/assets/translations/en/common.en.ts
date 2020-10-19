ba9bf7f99de0b5eeb175d428cf9ebfee6e6d1e4a
Ivan Ilic <ivan.ilic@msg-global.com>
2020-10-19 16:15:07 +0200

    FSA-5357:Adds translations to dynamicforms, moves relevant translations, deletes some duplicates

---
+++ projects/dynamicforms/src/assets/translations/en/common.en.ts
@@ -0,0 +1,13 @@
+export const dynamicforms = {
+  dynamicforms: {
+    optional: '(Optional)',
+    maxFileSize: 'Maximum file size allowed',
+    chooseFile: 'Choose file(s)',
+    startUpload: 'Start upload',
+    removeAll: 'Remove all',
+    upload: 'Upload',
+    pleaseSelect: 'Please select:',
+    enterValidValue: 'Please enter valid value',
+    validationErrors: 'There are validation errors',
+  },
+};
