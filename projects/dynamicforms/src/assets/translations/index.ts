a3682b740a4ae5558f583b373a5fed8ef4c3010d
Ivan Ilic <ivan.ilic@msg-global.com>
2020-10-19 16:15:07 +0200

    FSA-5357:Adds translations to dynamicforms, moves relevant translations, deletes some duplicates

---
+++ projects/dynamicforms/src/assets/translations/index.ts
@@ -0,0 +1,6 @@
+import { TranslationResources } from '@spartacus/core';
+import { dynamicforms } from './en/common.en';
+
+export const dynamicformsTranslations: TranslationResources = {
+  dynamicforms,
+};
