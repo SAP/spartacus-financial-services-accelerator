b72706d9b29c1f31c382739ab864101607458e4d
Bojan Jevtic <bojan.jevtic@msg-global.com>
2020-11-09 12:28:15 +0100

    NO-TASK: Removes upload component

--- projects/dynamicforms/src/assets/translations/de/common.de.ts
+++ projects/dynamicforms/src/assets/translations/de/common.de.ts
@@ -1,11 +1,6 @@
 export const dynamicforms = {
   dynamicforms: {
     optional: '(Optional)',
-    maxFileSize: '[DE] Maximum file size allowed',
-    chooseFile: '[DE] Choose file(s)',
-    startUpload: '[DE] Start upload',
-    removeAll: '[DE] Remove all',
-    upload: '[DE] Upload',
     pleaseSelect: 'Wählen Sie:',
     enterValidValue: 'Bitte geben Sie einen gültigen Wert ein',
     validationErrors: '[DE] There are validation errors',
