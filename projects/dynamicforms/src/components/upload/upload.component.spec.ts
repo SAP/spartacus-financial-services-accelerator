a94a0a08adb1e8374db90e95cebaff11349a28a6
Bojan Jevtic <bojan.jevtic@msg-global.com>
2020-12-02 16:30:49 +0100

    Revert "NO-TASK: Fixes failing test on develop"
    This reverts commit 7a44f995

--- projects/dynamicforms/src/components/upload/upload.component.spec.ts
+++ projects/dynamicforms/src/components/upload/upload.component.spec.ts
@@ -16,6 +16,7 @@ import { Observable, of } from 'rxjs';
 import { DynamicFormsConfig } from '../../core/config/form-config';
 import { FieldConfig } from '../../core/models/form-config.interface';
 import { FileService } from '../../core/services/file/file.service';
+import { OccValueListService } from '../../occ/services/occ-value-list.service';
 import { FormService } from './../../core/services/form/form.service';
 import { UploadComponent } from './upload.component';
 
@@ -133,6 +134,7 @@ describe('UploadComponent', () => {
         declarations: [UploadComponent, MockErrorNoticeComponent],
         imports: [ReactiveFormsModule, I18nTestingModule],
         providers: [
+          { provide: OccValueListService, useClass: MockOccValueListService },
           { provide: LanguageService, useClass: MockLanguageService },
           {
             provide: DynamicFormsConfig,
