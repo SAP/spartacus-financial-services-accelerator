2055c859e3ed8cda456f930d18921563e42db88c
Srdjan Damnjanovic <Srdjan.damnjanovic@msg-global.com>
2020-12-09 22:06:39 +0100

    FSA-5401: removed excess export from index.ts

--- projects/fsastorefrontlib/src/cms-components/my-account/order/index.ts
+++ projects/fsastorefrontlib/src/cms-components/my-account/order/index.ts
@@ -1,4 +1,3 @@
 export * from './order-details/order-detail-items/order-detail-items.component';
 export * from './order-details/order-detail-totals/order-detail-totals.component';
-export * from './order-details/order-overview/order-overview.component';
 export * from './order.module';
