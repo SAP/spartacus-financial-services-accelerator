daca6cc57e06e60dc46aeb9e0a47d0260678c8a0
Srdjan Damnjanovic <Srdjan.damnjanovic@msg-global.com>
2020-12-09 16:41:52 +0100

    FSA-5401: removed unnecessary component

--- projects/fsastorefrontlib/src/cms-components/my-account/order/index.ts
+++ projects/fsastorefrontlib/src/cms-components/my-account/order/index.ts
@@ -1,5 +1,4 @@
 export * from './order-details/order-detail-items/order-detail-items.component';
-export * from './order-details/order-detail-shipping/order-detail-shipping.component';
 export * from './order-details/order-detail-totals/order-detail-totals.component';
 export * from './order-details/order-overview/order-overview.component';
 export * from './order.module';
--- projects/fsastorefrontlib/src/cms-components/my-account/order/order-details/order-details.module.ts
+++ projects/fsastorefrontlib/src/cms-components/my-account/order/order-details/order-details.module.ts
@@ -18,12 +18,10 @@ import {
   CardModule,
 } from '@spartacus/storefront';
 import { FSOrderDetailItemsComponent } from './order-detail-items/order-detail-items.component';
-import { FSOrderDetailShippingComponent } from './order-detail-shipping/order-detail-shipping.component';
 import { FSOrderDetailTotalsComponent } from './order-detail-totals/order-detail-totals.component';
 import { FSOrderOverviewComponent } from './order-overview/order-overview.component';
 
 const moduleComponents = [
-  FSOrderDetailShippingComponent,
   FSOrderDetailItemsComponent,
   FSOrderDetailTotalsComponent,
   FSOrderOverviewComponent,
