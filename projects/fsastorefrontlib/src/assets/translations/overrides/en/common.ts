bc91f5bbed306f4fccb9e14c3da349ef827b6ee9
Bojan Jevtic <bojan.jevtic@msg-global.com>
2020-12-09 12:58:26 +0100

    NO-TASK: Reverts common.ts file

---
+++ projects/fsastorefrontlib/src/assets/translations/overrides/en/common.ts
@@ -0,0 +1,56 @@
+export const common = {
+  miniCart: {
+    insurances_travel: {
+      tripEndDate: 'End Date',
+      Travellers: 'Number of Travelers',
+    },
+    insurances_savings: {
+      annualContributionIncrease: 'Annual Contribution Increase',
+      retirementAge: 'Retirement Age',
+    },
+    insurances_life: {
+      lifeWhoCovered: 'Insured',
+      lifeCoverageRequire: 'Coverage Amount',
+      lifeCoverageLast: 'Coverage Period',
+    },
+    insurances_auto: {
+      vehicleMake: 'Vehicle Make',
+      vehicleModel: 'Vehicle Model',
+      vehicleType: 'Vehicle Type',
+      vehicleYear: 'Vehicle Year',
+      vehicleValue: 'Vehicle Value',
+    },
+    insurances_property_homeowners: {
+      'property-address-line-1': 'Property Address',
+      propertyType: 'Property Type',
+      propertyValue: 'Property Value',
+    },
+    insurances_property_renters: {
+      'property-address-line-1': 'Property Address',
+      propertyType: 'Property Type',
+      propertyValue: 'Property Value',
+    },
+    banking_credit_card: {
+      'minimum-card-limit': 'Minimum Card Limit',
+      'debit-card-design': 'Debit Card Design',
+      'number-of-applicants': 'Number Of Applicants',
+    },
+    banking_current_account: {
+      'debit-card-design': 'Debit Card Design',
+      'apply-for-debit-card': 'Debit Card',
+      accountType: 'Number of Applicants',
+    },
+    banking_loans: {
+      loanPurpose: 'Loan Purpose',
+      'loan-amount': 'Loan Amount',
+      'loan-term': 'Loan Term',
+      numberOfApplicants: 'Number Of Applicants',
+      'repayment-frequency': 'Repayment Frequency',
+    },
+    banking_fixed_term_deposit: {
+      'maturity-option': 'Maturity Option',
+      'term-amount': 'Investment Amount',
+      'deposit-term': 'Deposit Term',
+    },
+  },
+};
