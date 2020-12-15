b518dca9792fdcefc8f7aa5b8067a9fe6438a5ad
Srdjan Damnjanovic <Srdjan.damnjanovic@msg-global.com>
2020-12-15 12:26:42 +0100

    FSA-5515: Removes excess code

--- projects/fsastorefrontlib/src/cms-components/form/cms-form-submit/cms-form-submit.component.ts
+++ projects/fsastorefrontlib/src/cms-components/form/cms-form-submit/cms-form-submit.component.ts
@@ -1,13 +1,12 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import {
   FormCMSComponent,
   FormDataService,
   FormDataStorageService,
 } from '@fsa/dynamicforms';
-import { CmsComponentConnector, PageContext, PageType } from '@spartacus/core';
+import { CmsComponentConnector } from '@spartacus/core';
 import { CmsComponentData } from '@spartacus/storefront';
-import { map } from 'rxjs/operators';
 import { CmsFormSubmitComponent } from '../../../occ/occ-models';
 
 @Component({
@@ -23,28 +22,5 @@ export class CMSFormSubmitComponent extends FormCMSComponent implements OnInit {
     protected formDataStorageService: FormDataStorageService
   ) {
     super(componentData, formDataService, formDataStorageService);
-  }
-
-  routeParamId = 'formCode';
-  pageContext: PageContext;
-
-  ngOnInit() {
-    this.subscription.add(
-      this.activatedRoute.params
-        .pipe(
-          map(routeParam => {
-            this.pageContext = new PageContext(
-              routeParam[this.routeParamId],
-              PageType.CATEGORY_PAGE
-            );
-            return (this.component$ = this.cmsComponentConnector.get(
-              this.componentData.uid,
-              this.pageContext
-            ));
-          })
-        )
-        .subscribe()
-    );
-    this.loadForm();
   }
 }
