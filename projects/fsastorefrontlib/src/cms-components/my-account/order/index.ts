6e079a4cb0b3eb731378f842fbb8fac94aea753d
Srdjan Damnjanovic <Srdjan.damnjanovic@msg-global.com>
2021-02-26 15:35:58 +0100

    FSA-5518: Added component to public_api

--- projects/fsastorefrontlib/src/cms-components/my-account/order/index.ts
+++ projects/fsastorefrontlib/src/cms-components/my-account/order/index.ts
@@ -1,3 +1,4 @@
 export * from './order-details/order-detail-items/order-detail-items.component';
 export * from './order-details/order-detail-totals/order-detail-totals.component';
+export * from './order-history/order-history.component';
 export * from './order.module';
