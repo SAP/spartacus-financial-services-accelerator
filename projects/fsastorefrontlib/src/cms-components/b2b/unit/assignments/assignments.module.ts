267da583b852aafe17c81c897b5f0c93795c62d5
Srdjan Damnjanovic <Srdjan.damnjanovic@msg-global.com>
2021-03-24 16:22:08 +0100

    FSA-5651: added router module

--- projects/fsastorefrontlib/src/cms-components/b2b/unit/assignments/assignments.module.ts
+++ projects/fsastorefrontlib/src/cms-components/b2b/unit/assignments/assignments.module.ts
@@ -5,9 +5,10 @@ import { I18nModule } from '@spartacus/core';
 import { AssignmentsComponent } from './assignments.component';
 import { ActivateProductCellComponent } from './cells/activate-product-cell/activate-product-cell.component';
 import { RemoveProductCellComponent } from './cells/remove-product-cell/remove-product-cell.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
-  imports: [CommonModule, SubListModule, I18nModule],
+  imports: [CommonModule, SubListModule, I18nModule, RouterModule],
   declarations: [
     AssignmentsComponent,
     ActivateProductCellComponent,
