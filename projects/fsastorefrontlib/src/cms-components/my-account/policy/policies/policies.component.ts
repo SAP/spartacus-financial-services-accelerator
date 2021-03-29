c55d0f8940aa5e301d38d87859ca2f35b22eea61
Jelena Zobenica <jelena.zobenica@msg-global.com>
2021-03-29 11:53:23 +0200

    FSA-5736: Add card view for chart

--- projects/fsastorefrontlib/src/cms-components/my-account/policy/policies/policies.component.ts
+++ projects/fsastorefrontlib/src/cms-components/my-account/policy/policies/policies.component.ts
@@ -14,10 +14,17 @@ import {
   AllowedFSRequestType,
   RequestType,
 } from './../../../../occ/occ-models/occ.models';
-import { map } from 'rxjs/operators';
+import { filter, map, tap } from 'rxjs/operators';
 import { Subscription } from 'rxjs';
 import { EChartsOption } from 'echarts';
 
+let COLOR_PALETTE = [
+  ['Travel', { color: '#000066' }],
+  ['Auto', { color: '#0066CC', opacity: 0.85 }],
+  ['Life', { color: '#0066CC' }],
+  ['Renters', { color: '#3498DB' }],
+  ['Savings', { color: '#000033', opacity: 0.9 }]
+]
 @Component({
   selector: 'cx-fs-policies',
   templateUrl: './policies.component.html',
@@ -35,86 +42,28 @@ export class PoliciesComponent implements OnInit, OnDestroy {
   policies$;
   policiesLoaded$;
   baseUrl: string;
-  chartOption: EChartsOption = {
-    title: {
-      show: true,
-      text: 'Insurance premium amount by policies',
-      left: 'center'
-    },
-    tooltip: {
-        trigger: 'item',
-        position: 'inside'
-    },
-    legend: {
-        top: 'middle',
-        // align: 'left',
-        left: 'right',
-        // bottom: 20,
-        orient: 'vertical',
-        itemGap: 25,
-        itemWidth: 30
-        // overflow: {
-        //   verticalAlign: 'middle'
-        // }
-        // verticalyAlign: 'middle'
-    },
-    toolbox: {
-      show: true,
-      // left: 'left',
-      // iconStyle: {
-      //   color: '#0066cc'
-      // },
-      feature: {
-          // mark: {show: true},
-          // dataView: {show: true, readOnly: false},
-          // restore: {show: true},
-          saveAsImage: {show: true}
-      }
-  },
-    series: [
-        {
-            name: 'Insurance',
-            type: 'pie',
-            radius: ['40%', '70%'],
-            avoidLabelOverlap: false,
-            itemStyle: {
-                borderRadius: 10,
-                borderColor: '#fff',
-                borderWidth: 2
-            },
-            label: {
-                show: true,
-                position: 'outside'
-            },
-            emphasis: {
-                label: {
-                    show: true,
-                    fontSize: '20',
-                    fontWeight: 'bold',
-                    shadowOffsetY: 10,
-                    textShadowOffsetY: 10
-                }
-            },
-            labelLine: {
-                show: false
-            },
-            data: [
-                {value: 1048, name: 'Travel'},
-                {value: 735, name: 'Auto'},
-                {value: 580, name: 'Life'},
-                {value: 484, name: 'Renters'},
-                {value: 300, name: 'Savings'}
-            ]
-        }
-    ]
-};
+  colorPaletteMap = new Map<string, any>();
+  chartOption: EChartsOption;
+  options: any[];
+  dynamicData: EChartsOption;
+  isChartVisbible: boolean;
+  selectedFrequency: string = 'Single';
+  on: boolean;
 
   private subscription = new Subscription();
 
   ngOnInit() {
+    this.options = [
+      {name: 'Single', label: 'Single'},
+      {name: 'Monthly', label: 'Monthly'},
+      {name: 'Quarterly', label: 'Quarterly'},
+      {name: 'Half-yearly', label: 'Half-yearly'},
+      {name: 'Annually', label: 'Annually'},
+    ]
+    this.setColorPalette();
+    this.initChart();
     this.policyService.loadPolicies();
     this.policies$ = this.policyService.getPolicies();
-    this.policies$.subscribe(data => console.log(data, 'policies'))
     this.policiesLoaded$ = this.policyService.getLoaded();
     this.baseUrl = this.config.backend.occ.baseUrl || '';
   }
@@ -152,6 +101,111 @@ export class PoliciesComponent implements OnInit, OnDestroy {
           .map(allowedRequestType => allowedRequestType.requestType.code)
           .indexOf(RequestType.FSCLAIM) > -1
       );
+    }
+  }
+
+  setColorPalette() {
+    // dinamicaly set
+    this.colorPaletteMap.set('Travel', { color: '#000066', opacity: 0.8 });
+    this.colorPaletteMap.set('Auto', { color: '#0066CC', opacity: 0.85 });
+    this.colorPaletteMap.set('Life', { color: '#0066CC' });
+    this.colorPaletteMap.set('Renters', { color: '#3498DB' });
+    this.colorPaletteMap.set('Savings', { color: '#000033', opacity: 0.9 });
+    this.colorPaletteMap.set('Event', { color: '#000066' });
+    this.colorPaletteMap.set('Homeowners', { color: '#3498DB', opacity: 0.8 });
+    this.colorPaletteMap.set('Selected', { color: '#D3D6DB' });
+  }
+
+  initChart() {
+    this.chartOption = {
+      title: {
+        show: true,
+        text: 'Insurance premium amount by policies',
+        left: 'center',
+        padding: 20
+      },
+      tooltip: {
+          trigger: 'item',
+          position: 'inside',
+          formatter: '{b}: {d}%'
+      },
+      legend: {
+          top: 'middle',
+          left: 'right',
+          orient: 'vertical',
+          itemGap: 25,
+          itemWidth: 40
+      },
+      toolbox: {
+        show: true,
+        feature: {
+            saveAsImage: {show: true}
+        }
+    },
+      series: [
+          {
+              name: 'Insurance',
+              type: 'pie',
+              radius: ['40%', '70%'],
+              avoidLabelOverlap: false,
+              itemStyle: {
+                  borderRadius: 10,
+                  borderColor: '#fff',
+                  borderWidth: 2,
+              },
+              label: {
+                  show: true,
+                  position: 'outside',
+                  // formatter: '{b}: {d}%'
+                  formatter: function(params) {
+                    let res = '';
+                    res += params.name + ': ' + params.value.toLocaleString();
+                    return res;
+                  }
+              },
+              emphasis: {
+                  label: {
+                      show: true,
+                      fontSize: '20',
+                      fontWeight: 'bold',
+                  }
+              },
+              labelLine: {
+                  show: true
+              },
+              data: [
+                  {value: 1048, name: 'Travel', itemStyle: this.colorPaletteMap.get('Travel')},
+                  {value: 735, name: 'Auto', itemStyle: this.colorPaletteMap.get('Auto')},
+                  {value: 580, name: 'Life', itemStyle: this.colorPaletteMap.get('Life')},
+                  {value: 484, name: 'Renters', itemStyle: this.colorPaletteMap.get('Renters')},
+                  {value: 300, name: 'Savings', itemStyle: this.colorPaletteMap.get('Savings')}
+              ]
+          }
+      ]
+  };
+  }
+
+  selectPaymentFrequency() {
+    if(!this.selectedFrequency) {
+      this.isChartVisbible = false;
+      return;
+    }
+    this.dynamicData = this.chartOption;
+    // reinitialize data for chart
+    this.dynamicData.series[0].data = [
+      {value: 900, name: 'Travel', itemStyle: this.colorPaletteMap.get('Travel')},
+      {value: 200, name: 'Auto', itemStyle: this.colorPaletteMap.get('Auto')},
+      {value: 350, name: 'Life', itemStyle: this.colorPaletteMap.get('Life')},
+      {value: 500, name: 'Renters', itemStyle: this.colorPaletteMap.get('Renters')},
+      {value: 650, name: 'Savings', itemStyle: this.colorPaletteMap.get('Savings')},
+      {value: 150, name: 'Event', itemStyle: this.colorPaletteMap.get('Event')}
+    ];
+  }
+
+  toggleChartVisibility() {
+    this.isChartVisbible = !this.isChartVisbible;
+    if (!this.isChartVisbible) {
+      this.selectedFrequency = 'Single';
     }
   }
 
