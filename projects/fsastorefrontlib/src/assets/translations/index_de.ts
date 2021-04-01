5bb402866c3ffe923c221fa36728460ac9e3ee36
Bojan Jevtic <bojan.jevtic@msg-global.com>
2021-04-01 21:45:46 +0200

    FSA-56751: Merge conflict resolved

--- projects/fsastorefrontlib/src/assets/translations/index_de.ts
+++ projects/fsastorefrontlib/src/assets/translations/index_de.ts
@@ -1,5 +1,10 @@
 import { TranslationResources } from '@spartacus/core';
-import { b2b, organization, orgUnitAssignments } from './de/b2b.de';
+import {
+  b2b,
+  organization,
+  productAssignments,
+  potentialProductAssignments,
+} from './de/b2b.de';
 import { changeRequest } from './de/changeRequest.de';
 import { claim } from './de/claim.de';
 import { fscommon } from './de/common.de';
@@ -32,7 +37,8 @@ export const fstranslationsDe: TranslationResources = {
   quoteReview,
   orderConfirmation,
   organization,
-  orgUnitAssignments,
+  productAssignments,
+  potentialProductAssignments,
   document,
 };
 
