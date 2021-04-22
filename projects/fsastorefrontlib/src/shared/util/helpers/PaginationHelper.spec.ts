ef0737d700c8326f44aa1e028beeb55b4a09ee12
Divna Popovic <divna.popovic@msg-global.com>
2021-04-22 11:14:58 +0200

    FSA-5759: Test fix

--- projects/fsastorefrontlib/src/shared/util/helpers/PaginationHelper.spec.ts
+++ projects/fsastorefrontlib/src/shared/util/helpers/PaginationHelper.spec.ts
@@ -5,7 +5,14 @@ describe('PaginationHelper', () => {
   let paginationHelper: PaginationHelper;
   const mockCollection = ['0', '1', '2'];
   const mockCollection2 = ['0', '1', '2', '3', '4', '5'];
-
+  const paginationconfig = {
+    pageSize: 2,
+    currentPage: 0,
+  };
+  const paginationconfig2 = {
+    pageSize: 2,
+    currentPage: 1,
+  };
   beforeEach(() => {
     TestBed.configureTestingModule({
       providers: [PaginationHelper],
@@ -16,25 +23,23 @@ describe('PaginationHelper', () => {
   describe('transform', () => {
     it('should return results', () => {
       const results = PaginationHelper.getPaginationResults(
-        2,
-        0,
-        mockCollection
+        paginationconfig,
+        mockCollection,
       );
       expect(results.values).not.toBe(null);
       expect(results.values.length).toEqual(2);
     });
     it('should return second page of results', () => {
       const results = PaginationHelper.getPaginationResults(
-        2,
-        1,
-        mockCollection2
+        paginationconfig2,
+        mockCollection2,
       );
       expect(results.values).not.toBe(null);
       expect(results.values[0]).toEqual('2');
       expect(results.values[1]).toEqual('3');
     });
     it('should return empty list', () => {
-      const results = PaginationHelper.getPaginationResults(2, 0, []);
+      const results = PaginationHelper.getPaginationResults(paginationconfig, []);
       expect(results.values.length).toBe(0);
     });
   });
