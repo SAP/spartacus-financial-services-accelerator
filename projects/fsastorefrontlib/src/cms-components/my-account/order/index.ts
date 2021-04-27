34ac95282e0bdaecbde37ca442420b44c02792e9
Srdjan Damnjanovic <Srdjan.damnjanovic@msg-global.com>
2021-04-27 10:21:01 +0200

    FSA-5758: Added order-summary to index file

--- projects/fsastorefrontlib/src/cms-components/my-account/order/index.ts
+++ projects/fsastorefrontlib/src/cms-components/my-account/order/index.ts
@@ -1,4 +1,5 @@
 export * from './order-details/order-detail-items/order-detail-items.component';
 export * from './order-details/order-detail-totals/order-detail-totals.component';
+export * from './order-details/order-summary/order-summary.component';
 export * from './order-history/order-history.component';
 export * from './order.module';
