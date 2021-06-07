8ee80002e8cb6d4a2281b4f4157fb36c024d1226
Jelena Zobenica <jelena.zobenica@msg-global.com>
2021-06-07 12:47:08 +0200

    FSA-5783: Replace Units with Organization on the group policy page

--- projects/fsastorefrontlib/src/assets/translations/en/b2b.en.ts
+++ projects/fsastorefrontlib/src/assets/translations/en/b2b.en.ts
@@ -12,8 +12,84 @@ export const organization = {
     productAssignment: 'Product Assignment',
   },
   orgUnit: {
+    header: 'All organization ({{count}})',
+    unit: 'Organization',
+    parentUnit: 'Parent Organization',
+    details: {
+      title: 'Organization Details',
+      subtitle: 'Organization: {{ item.name }}',
+    },
+    edit: {
+      title: 'Edit Organization',
+      subtitle: 'Organization: {{ item.name }}',
+    },
+    create: {
+      title: 'Create Organization',
+      subtitle: '',
+    },
+    messages: {
+      deactivate: 'Are you sure you want to disable this organization?',
+      confirmEnabled: 'Organization {{item.name}} enabled successfully',
+      confirmDisabled: 'Organization {{item.name}} disabled successfully',
+      update: 'Organization {{ item.name }} updated successfully',
+      create: 'Organization {{ item.name }} created successfully',
+    },
+    links: {
+      units: 'Child Organization',
+    },
+    children: {
+      create: {
+        title: 'Create child Organization',
+        subtitle: '',
+      },
+      messages: {
+        create: 'Organization {{ item.name }} created successfully',
+      },
+    },
+    form: {
+      parentOrgUnit: 'Parent business organization',
+      create: 'Create Organization',
+    },
+    assignApprovers: {
+      instructions: {
+        check: `To assign an approver to this organization, select the user's check box.`,
+      },
+    },
     breadcrumbs: {
+      list: 'All organization',
+      children: 'Child organization',
       productAssignment: 'Product Assignment',
+    },
+  },
+  orgUnitChildren: {
+    title: 'Child organization',
+    subtitle: 'Organization: {{item.name}}',
+  },
+  orgUnitApprovers: {
+    subtitle: 'Organization: {{item.name}}',
+  },
+  orgUnitAssignedApprovers: {
+    subtitle: 'Organization: {{item.name}}',
+  },
+  orgUnitAssignedUsers: {
+    subtitle: 'Organization: {{item.name}}',
+  },
+  orgUnitUsers: {
+    subtitle: 'Organization: {{item.name}}',
+  },
+  orgUnitCostCenters: {
+    subtitle: 'Organization: {{item.name}}',
+  },
+  orgUnitAddress: {
+    subtitle: 'Organization: {{item.name}}',
+    details: {
+      subtitle: 'Organization: {{item.name}} (TODO)',
+    },
+    edit: {
+      subtitle: 'Organization: {{ item.name }} (TODO)',
+    },
+    create: {
+      subtitle: 'Organization: {{ item.name }} (TODO)',
     },
   },
 };
