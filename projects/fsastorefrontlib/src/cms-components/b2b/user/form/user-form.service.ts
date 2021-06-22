6d7a1fdb430a0c64e6f5b6afd1164fe3ed96cc9d
Bojan Jevtic <bojan.jevtic@msg-global.com>
2021-06-22 15:45:47 +0200

    FSA-5847: Refactors build method of b2b user creation form

--- projects/fsastorefrontlib/src/cms-components/b2b/user/form/user-form.service.ts
+++ projects/fsastorefrontlib/src/cms-components/b2b/user/form/user-form.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
-import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { UserFormService } from '@spartacus/organization/administration/components';
-import { CustomFormValidators } from '@spartacus/storefront';
 import { DefaultFormValidators } from '@spartacus/dynamicforms';
 
 @Injectable({
@@ -9,32 +8,13 @@ import { DefaultFormValidators } from '@spartacus/dynamicforms';
 })
 export class FSUserFormService extends UserFormService {
   protected build() {
-    const form = new FormGroup({});
-    form.setControl('customerId', new FormControl(''));
-    form.setControl('titleCode', new FormControl(''));
-    form.setControl('firstName', new FormControl('', Validators.required));
-    form.setControl('lastName', new FormControl('', Validators.required));
-    form.setControl(
+    super.build();
+    this.form.setControl(
       'dateOfBirth',
       new FormControl('', [
         Validators.required,
         DefaultFormValidators.dateOfBirthValidator(18),
       ])
     );
-    form.setControl(
-      'email',
-      new FormControl('', [
-        Validators.required,
-        CustomFormValidators.emailValidator,
-      ])
-    );
-    form.setControl(
-      'orgUnit',
-      new FormGroup({
-        uid: new FormControl(undefined, Validators.required),
-      })
-    );
-    form.setControl('roles', new FormArray([]));
-    this.form = form;
   }
 }
