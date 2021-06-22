357a0d5639614b887a21030eeafe021716f6c0b0
Srdjan Damnjanovic <Srdjan.damnjanovic@msg-global.com>
2021-06-22 16:40:33 +0200

    FSA-5818: Removed unnecessary module

--- projects/fsastorefrontlib/src/cms-components/my-account/policy/policy.module.ts
+++ projects/fsastorefrontlib/src/cms-components/my-account/policy/policy.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { FormsModule } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
 
 import {
@@ -50,7 +49,6 @@ const routes: Routes = [
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
     RouterModule,
     I18nModule,
     NgSelectModule,
