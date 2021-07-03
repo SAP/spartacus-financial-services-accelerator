77f4ee1129907a27330005cdf6ade49eb207f909
Srdjan Damnjanovic <srdjan.damnjanovic@msg-global.com>
2021-07-03 11:54:08 +0200

    Cypress-improvements: added banking buttons

--- projects/fsastorefront-e2e-cypress/cypress/helpers/constants.ts
+++ projects/fsastorefront-e2e-cypress/cypress/helpers/constants.ts
@@ -1,2 +1,2 @@
 export const insuranceButtons = ['Get a Quote', 'Retrieve a Quote'];
-export const bankingButtons = ['Get a Quote', 'Retrieve a Quote'];
+export const bankingButtons = ['Request a product', 'Retrieve an Application'];
