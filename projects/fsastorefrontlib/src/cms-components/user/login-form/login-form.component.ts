d665fb3c97894c2fbeb0db86ac27532d2de47628
Bojan Jevtic <bojan.jevtic@msg-global.com>
2021-09-27 13:13:02 +0200

    CXFSA-75: Refactors Login Form

--- projects/fsastorefrontlib/src/cms-components/user/login-form/login-form.component.ts
+++ projects/fsastorefrontlib/src/cms-components/user/login-form/login-form.component.ts
@@ -1,19 +1,10 @@
-import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
-import { GlobalMessageService, WindowRef, AuthService } from '@spartacus/core';
-import { LoginFormComponent } from '@spartacus/storefront';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { LoginFormComponent } from '@spartacus/user/account/components';
 
 @Component({
   selector: 'cx-fs-login-form',
   templateUrl: './login-form.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FSLoginFormComponent extends LoginFormComponent {
-  constructor(
-    protected auth: AuthService,
-    protected globalMessageService: GlobalMessageService,
-    protected fb: FormBuilder,
-    protected winRef: WindowRef
-  ) {
-    super(auth, globalMessageService, fb, winRef);
-  }
 }
