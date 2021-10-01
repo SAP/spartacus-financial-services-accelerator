1819a523b7c5286f526a16cb99fb9b98703fcfab
Jelena Zobenica <jelena.zobenica@msg-global.com>
2021-10-01 16:38:03 +0200

    CXFSA-5: Revert expected value for test

--- projects/fsastorefrontlib/src/cms-components/comparison-table/comparison-table-panel/comparison-table-panel.component.spec.ts
+++ projects/fsastorefrontlib/src/cms-components/comparison-table/comparison-table-panel/comparison-table-panel.component.spec.ts
@@ -289,6 +289,6 @@ describe('ComparisonTablePanelComponent', () => {
     fixture.detectChanges();
     expect(
       comparisonTableService.highestElement.nativeElement.clientHeight
-    ).toEqual(39);
+    ).toEqual(38);
   });
 });
