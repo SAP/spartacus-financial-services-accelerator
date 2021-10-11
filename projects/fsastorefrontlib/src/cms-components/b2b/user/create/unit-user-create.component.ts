aa1d7ce4994ee41da3b20a869b3c92a1c7c69a53
Bojan Jevtic <bojan.jevtic@msg-global.com>
2021-10-11 12:04:49 +0200

    CXFSA-71: Fixes invalid imports

--- projects/fsastorefrontlib/src/cms-components/b2b/user/create/unit-user-create.component.ts
+++ projects/fsastorefrontlib/src/cms-components/b2b/user/create/unit-user-create.component.ts
@@ -1,20 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import {
   UnitUserCreateComponent,
-  UserItemService,
 } from '@spartacus/organization/administration/components';
-import { UnitUserItemService } from '@spartacus/organization/administration/components/unit/links/users/create/unit-user-item.service';
 
 @Component({
   selector: 'cx-fs-org-unit-user-create',
   templateUrl: './unit-user-create.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
-  host: { class: 'content-wrapper' },
-  providers: [
-    {
-      provide: UserItemService,
-      useExisting: UnitUserItemService,
-    },
-  ],
+  host: { class: 'content-wrapper' }
 })
 export class FSUnitUserCreateComponent extends UnitUserCreateComponent {}
