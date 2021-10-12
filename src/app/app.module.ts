aa6aae4f585ca9e783d8398fbd8f2c7a8e22a9e9
Srdjan Damnjanovic <Srdjan.damnjanovic@msg-global.com>
2021-10-12 12:48:37 +0200

    CXFSA-71-modules: new structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FSStorefrontModule } from '@spartacus/fsa-storefront';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from 'src/environments/environment';
-import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
 import { SiteContextModule } from '@spartacus/core';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FSModule } from './spartacus/fs.module';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
-    FSStorefrontModule.withConfig({
-      backend: {
-        occ: {
-          prefix: '/occ/v2/',
-          baseUrl: environment.occBaseUrl,
-        },
-      },
-      context: {
-        baseSite: ['financial'],
-        language: ['en', 'de'],
-        currency: ['EUR'],
-        urlParameters: ['baseSite', 'language', 'currency'],
-      },
-      authentication: {
-        client_id: 'financial_customer',
-        client_secret: 'secret',
-      },
-      features: {
-        consignmentTracking: true,
-      },
-    }),
+    HttpClientModule,
+    AppRoutingModule,
+    StoreModule.forRoot({}),
+    EffectsModule.forRoot([]),
     StoreDevtoolsModule.instrument({
       maxAge: 25,
     }),
     SiteContextModule.forRoot(),
+    FSModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
