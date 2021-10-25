63ae81238c592e49e73944dd95167886791e976f
Jelena Zobenica <jelena.zobenica@msg-global.com>
2021-10-25 11:25:23 +0200

    CXFSA-125: Adjust karma config files to Spartacus configuration

--- projects/fsastorefrontlib/karma.conf.js
+++ projects/fsastorefrontlib/karma.conf.js
@@ -1,37 +1,41 @@
-/*
- * SPDX-FileCopyrightText: 2020 SAP SE or an SAP affiliate company and contributors.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
 // Karma configuration file, see link for more information
+
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: ['parallel', 'jasmine', '@angular-devkit/build-angular'],
     plugins: [
+      require('karma-parallel'),
       require('karma-jasmine'),
       require('karma-coverage'),
       require('karma-chrome-launcher'),
       require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
       require('@angular-devkit/build-angular/plugins/karma'),
     ],
+    parallelOptions: {
+      executors: 2,
+      shardStrategy: 'round-robin',
+    },
     client: {
       clearContext: false, // leave Jasmine Spec Runner output visible in browser
-    },
-    reporters: ['progress', 'kjhtml', 'coverage-istanbul', 'dots'],
-    coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../../coverage'),
-      reports: ['lcov', 'cobertura', 'text-summary'],
-      thresholds: {
-        statements: 80,
-        lines: 80,
-        branches: 60,
-        functions: 80,
+      jasmine: {
+        random: false,
       },
-      fixWebpackSourcePaths: true,
+    },
+    reporters: ['progress', 'kjhtml', 'dots'],
+    coverageReporter: {
+      dir: require('path').join(__dirname, '../../coverage/fsastrorefrontlib'),
+      reporters: [{ type: 'lcov', subdir: '.' }, { type: 'text-summary' }],
+      check: {
+        global: {
+          statements: 80,
+          lines: 80,
+          branches: 60,
+          functions: 80,
+        },
+      },
     },
     port: 9876,
     colors: true,
