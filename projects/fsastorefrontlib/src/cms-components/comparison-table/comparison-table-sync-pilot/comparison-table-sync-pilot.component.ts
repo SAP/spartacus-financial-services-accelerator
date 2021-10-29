393be53a08bd092361b40c107c8d62ae736fd492
Srdjan Damnjanovic <Srdjan.damnjanovic@msg-global.com>
2021-10-29 16:20:37 +0200

    CXFSA-94: small refactore

--- projects/fsastorefrontlib/src/cms-components/comparison-table/comparison-table-sync-pilot/comparison-table-sync-pilot.component.ts
+++ projects/fsastorefrontlib/src/cms-components/comparison-table/comparison-table-sync-pilot/comparison-table-sync-pilot.component.ts
@@ -1,7 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { CmsService, WindowRef } from '@spartacus/core';
-import { UserAccountFacade } from '@spartacus/user/account/root';
 import { Observable } from 'rxjs';
+import { CmsService, WindowRef } from '@spartacus/core';
+import { ModalService } from '@spartacus/storefront';
+import { UserAccountFacade } from '@spartacus/user/account/root';
+import { Service } from '@syncpilot/bpool-guest-lib';
+import { AgentSearchService } from '../../../core/agent/facade/agent-search.service';
 import { SyncPilotConnectionComponent } from '../../sync-pilot/sync-pilot-connection.component';
 
 @Component({
@@ -14,9 +17,12 @@ export class ComparisonTableSyncPilotComponent
   constructor(
     protected cmsService: CmsService,
     protected userAccountFacade: UserAccountFacade,
+    protected syncPilotService: Service,
+    protected modalService: ModalService,
+    protected agentService: AgentSearchService,
     protected winRef?: WindowRef
   ) {
-    super(userAccountFacade, winRef);
+    super(userAccountFacade, syncPilotService, modalService, agentService);
   }
 
   component$: Observable<any> = this.cmsService.getComponentData(
