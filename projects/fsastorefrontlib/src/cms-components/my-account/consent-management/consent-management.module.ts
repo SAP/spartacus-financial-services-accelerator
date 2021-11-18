89f0e2235351454ca0e28c771c01b3acc1c552a0
Predrag Jovicic <predrag.jovicic@msg-global.com>
2021-11-18 14:38:30 +0100

    CXFSA-86: Rerun prettier-fix and lint

--- projects/fsastorefrontlib/src/cms-components/my-account/consent-management/consent-management.module.ts
+++ projects/fsastorefrontlib/src/cms-components/my-account/consent-management/consent-management.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
-import { MediaModule,SpinnerModule } from '@spartacus/storefront';
+import { MediaModule, SpinnerModule } from '@spartacus/storefront';
 import { I18nModule, UrlModule } from '@spartacus/core';
 import { ConsentService } from 'projects/fsastorefrontlib/src/core/my-account/facade/consent.service';
 import { FSConsentManagementComponent } from './consent-management.component';
