8d8eec5686d2d884d375005be4474c1ec4c4b186
Predrag Jovicic <predrag.jovicic@msg-global.com>
2021-11-18 14:20:55 +0100

    CXFSA-86: Fixing sonar issues

--- projects/fsastorefrontlib/src/core/my-account/facade/consent.service.ts
+++ projects/fsastorefrontlib/src/core/my-account/facade/consent.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { take } from 'rxjs/operators';
 import * as fromAction from './../store/actions';
 import { StateWithMyAccount } from '../store/my-account-state';
 import { select, Store } from '@ngrx/store';
