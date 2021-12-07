5d6613656f5bb00ca91fe9994eac8650859ad033
Jelena Zobenica <jelena.zobenica1@gmail.com>
2021-12-07 14:19:38 +0100

    CXFSA-320: Replace HTML element to fix Sonar error

--- projects/fsastorefrontlib/src/assets/translations/en/policy.en.ts
+++ projects/fsastorefrontlib/src/assets/translations/en/policy.en.ts
@@ -6,7 +6,7 @@ export const policy = {
     policyId: 'Policy ID',
     expiredPolicyId: 'Expired Policy ID',
     policyNo: 'Policy No',
-    existingPolicyNo: '[DE] Existing Policy No.',
+    existingPolicyNo: 'Existing Policy No.',
     myPolicy: 'My Policy',
     policyNumberIs: 'Policy number is',
     policyHolder: 'Policy Holder',
