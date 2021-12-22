3fd74d11c210bc20134b74ec2c1c81afcf8a23aa
Sofija Trajkovic <sofija.trajkovic@msg-global.com>
2021-12-22 10:45:54 +0100

    CXFSA-289: Changed contains to contain

--- projects/fsastorefront-e2e-cypress/cypress/helpers/checkout/shared-checkout.ts
+++ projects/fsastorefront-e2e-cypress/cypress/helpers/checkout/shared-checkout.ts
@@ -34,7 +34,7 @@ export function checkAddOptionsPageContent(addOptions: AddOptions) {
 
 export function checkMiniCart(miniCart: MiniCart) {
   cy.get('.highlighted .short-overview-value').should(
-    'contains.text',
+    'contain.text',
     miniCart.price
   );
   cy.get('.short-overview-content')
@@ -49,11 +49,11 @@ export function checkMiniCart(miniCart: MiniCart) {
           .eq(index)
           .within(() => {
             cy.get('.short-overview-title').should(
-              'contains.text',
+              'contain.text',
               productItem.title
             );
             cy.get('.short-overview-value').should(
-              'contains.text',
+              'contain.text',
               productItem.value
             );
           });
