62beb720edf378a3676252a7352bc23df34b09de
Srdjan Damnjanovic <Srdjan.damnjanovic@msg-global.com>
2021-12-28 12:05:43 +0100

    CXFSA-107: added comment

--- projects/fsastorefrontlib/src/cms-components/assets-table/assets-table.component.ts
+++ projects/fsastorefrontlib/src/cms-components/assets-table/assets-table.component.ts
@@ -5,6 +5,14 @@ import {
   OnInit,
 } from '@angular/core';
 
+/**
+ * The Asstes Table component currently provides a somewhat generic table DOM structure. It accepts 4 strings used as headings
+ * and one object which can contain either Quotes, Policies or Claims. Based on what asset the user has chosen on user-profile component,
+ * that asset is being passed to this component and is displayed accordingly.
+ *
+ * TO-DO: Create more generic approach with some sort of configuration
+ */
+
 @Component({
   selector: 'cx-fs-assets-table',
   templateUrl: './assets-table.component.html',
