122a3835f92633ea334d02364c88acdd39d34c35
Srdjan Damnjanovic <Srdjan.damnjanovic@msg-global.com>
2022-01-31 16:21:34 +0100

    unused-imports-removal: Removed excess imports

--- projects/fsastorefrontlib/src/cms-components/my-account/consent-management/consent-management.module.ts
+++ projects/fsastorefrontlib/src/cms-components/my-account/consent-management/consent-management.module.ts
@@ -1,14 +1,8 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { FormsModule } from '@angular/forms';
-import { NgSelectModule } from '@ng-select/ng-select';
-import {
-  ConsentManagementModule,
-  MediaModule,
-  SpinnerModule,
-} from '@spartacus/storefront';
-import { I18nModule, UrlModule } from '@spartacus/core';
+import { ConsentManagementModule, SpinnerModule } from '@spartacus/storefront';
+import { I18nModule } from '@spartacus/core';
 import { ConsentService } from 'projects/fsastorefrontlib/src/core/my-account/facade/consent.service';
 import { FSConsentManagementComponent } from './consent-management.component';
 
@@ -17,11 +11,7 @@ import { FSConsentManagementComponent } from './consent-management.component';
     CommonModule,
     RouterModule,
     I18nModule,
-    FormsModule,
-    NgSelectModule,
     SpinnerModule,
-    MediaModule,
-    UrlModule,
     ConsentManagementModule,
   ],
   declarations: [FSConsentManagementComponent],
