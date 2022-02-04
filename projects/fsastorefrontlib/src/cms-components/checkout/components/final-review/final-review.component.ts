52de9cb0f98dfb159a7e8a26bce937918c5e77e9
Srdjan Damnjanovic <Srdjan.damnjanovic@msg-global.com>
2022-02-04 10:59:18 +0100

    CXFSA-479: "Security" fix

--- projects/fsastorefrontlib/src/cms-components/checkout/components/final-review/final-review.component.ts
+++ projects/fsastorefrontlib/src/cms-components/checkout/components/final-review/final-review.component.ts
@@ -36,8 +36,10 @@ export class FinalReviewComponent implements OnInit {
   }
 
   placeOrder(): void {
-    this.checkoutService.placeOrder(this.tAndCToggler);
-    this.checkoutService.orderPlaced = true;
-    this.routingService.go({ cxRoute: 'orderConfirmation' });
+    if (this.tAndCToggler) {
+      this.checkoutService.placeOrder(this.tAndCToggler);
+      this.checkoutService.orderPlaced = true;
+      this.routingService.go({ cxRoute: 'orderConfirmation' });
+    }
   }
 }
