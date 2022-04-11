20868a8fa90f988fedf410d7b29d3e4d32f5482e
Bobobo <71004931+bojanjevtic-spa@users.noreply.github.com>
2022-04-11 14:03:34 +0200

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,5 +3,5 @@ import { Environment } from './models/environment.model';
 export const environment: Environment = {
   production: false,
   occBaseUrl: 'https://financialservices.local:9002',
-  digitalPayments: false,
+  digitalPayments: true,
 };
