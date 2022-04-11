afbe81382344c22703797e91a3e53ee9ccd7c9dc
Bojan Jevtic <bojan.jevtic@msg-global.com>
2022-04-11 13:41:26 +0200

    NO-TASK: Enables digital payments

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,5 +3,5 @@ import { Environment } from './models/environment.model';
 export const environment: Environment = {
   production: true,
   occBaseUrl: 'https://10.27.241.19:9012',
-  digitalPayments: false,
+  digitalPayments: true,
 };
