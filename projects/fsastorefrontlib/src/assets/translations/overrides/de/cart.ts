1bc44bfd82e750770f508224412164e31a6f687c
Filip Hadzi-Ristic <filip.hadzi.ristic@msg-global.com>
2022-06-13 14:03:25 +0200

    CXFSA-584: Added translation for one sentence in german

--- projects/fsastorefrontlib/src/assets/translations/overrides/de/cart.ts
+++ projects/fsastorefrontlib/src/assets/translations/overrides/de/cart.ts
@@ -2,6 +2,7 @@ export const cart = {
   voucher: {
     vouchersApplied: 'Eingelöste Gutscheine:',
     applyVoucherSuccess: '{{voucherCode}} wurde eingelöst.',
+    removeVoucherSuccess: '{{voucherCode}} wurde entfernt.',
   },
   orderCost: {
     discount: 'Sie sparen',
