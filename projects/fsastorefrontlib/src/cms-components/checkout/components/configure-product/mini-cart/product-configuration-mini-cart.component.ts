4cb6922e448e7f3b5cf0afc2153fa3a16c4ddd5a
Filip Hadzi-Ristic <filip.hadzi.ristic@msg-global.com>
2022-07-19 22:02:53 +0200

    CXFSA-51: Added type to priceAttribute and created method for currency formatting

--- projects/fsastorefrontlib/src/cms-components/checkout/components/configure-product/mini-cart/product-configuration-mini-cart.component.ts
+++ projects/fsastorefrontlib/src/cms-components/checkout/components/configure-product/mini-cart/product-configuration-mini-cart.component.ts
@@ -18,6 +18,7 @@ import {
 import { FSProduct, PricingData } from '../../../../../occ/occ-models';
 import { FSTranslationService } from '../../../../../core/i18n/facade/translation.service';
 import { PAY_NOW_BILLING_TIME_CODE } from './../../../../../core/general-config/defalut-general-config';
+import { CURRENCY_CODE } from './../../../../../core/general-config/defalut-general-config';
 
 @Component({
   selector: 'cx-fs-product-configuration-mini-cart',
@@ -61,8 +62,9 @@ export class ProductConfigurationMiniCartComponent
           .pipe(
             map(formData => {
               if (formData && formData.content) {
-                this.pricingData = this.pricingService.buildPricingData(
-                  JSON.parse(formData.content)
+                this.pricingData = this.pricingService.buildPricingDataWithformDefinition(
+                  JSON.parse(formData.content),
+                  JSON.parse(formData.formDefinition.content)
                 );
                 this.product$ = this.productService.getCalculatedProductData(
                   this.productId,
@@ -91,6 +93,13 @@ export class ProductConfigurationMiniCartComponent
       }
     });
     return paynowFormattedPrice;
+  }
+
+  getFormatedValue(entry: any): string{
+    if(entry.type == CURRENCY_CODE){
+      return '€' + entry.value;
+    }
+    return entry.value;
   }
 
   ngOnDestroy() {
--- projects/fsastorefrontlib/src/core/product-pricing/facade/pricing.service.ts
+++ projects/fsastorefrontlib/src/core/product-pricing/facade/pricing.service.ts
@@ -25,4 +25,33 @@ export class PricingService {
     });
     return pricingAttributesData;
   }
+
+  buildPricingDataWithformDefinition(formData: { [name: string]: Object }, formDefinition: { [name: string]: Object }): PricingData {
+    const pricingAttributesData: PricingData = {
+      priceAttributeGroups: [],
+    };
+    Object.keys(formData).forEach(groupCode => {
+      let groupIndex = Object.keys(formDefinition?.formGroups).findIndex((group:any)=>{
+        return formDefinition?.formGroups[group].groupCode == groupCode;
+      });
+      const priceAttributeGroup: PriceAttributeGroup = {
+        priceAttributes: [],
+      };
+      priceAttributeGroup.name = groupCode;
+      Object.keys(formData[groupCode]).forEach(inputName => {
+        
+        let fieldIndex = Object.keys(formDefinition?.formGroups[groupIndex].fieldConfigs).findIndex((field:any) =>{
+          return formDefinition?.formGroups[groupIndex].fieldConfigs[field].name == inputName;
+        })
+        let field = formDefinition?.formGroups[groupIndex].fieldConfigs[fieldIndex];
+        priceAttributeGroup.priceAttributes.push({
+          type: field?.fieldType,
+          key: inputName,
+          value: formData[groupCode][inputName],
+        });
+      });
+      pricingAttributesData.priceAttributeGroups.push(priceAttributeGroup);
+    });
+    return pricingAttributesData;
+  }
 }
