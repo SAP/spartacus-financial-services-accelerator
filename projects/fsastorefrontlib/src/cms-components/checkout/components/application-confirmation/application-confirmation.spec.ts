f388c84ba43ae0213a2699c8e74f977d797cb961
micovicn <91135017+micovicn@users.noreply.github.com>
2022-12-22 14:19:33 +0100

    CXFSA-1096: Test fixing

--- projects/fsastorefrontlib/src/cms-components/checkout/components/application-confirmation/application-confirmation.spec.ts
+++ projects/fsastorefrontlib/src/cms-components/checkout/components/application-confirmation/application-confirmation.spec.ts
@@ -2,8 +2,18 @@ import { TestBed, ComponentFixture } from '@angular/core/testing';
 import { DomSanitizer } from '@angular/platform-browser';
 import { RoutingService } from '@spartacus/core';
 import { ApplicationConfirmationComponent } from './application-confirmation.component';
+import { of } from 'rxjs';
 
 const mockImageSrc = 'data:image/svg+xml;base64, sampleImageCode';
+
+const mockRoutingData = {
+  state: {
+    params: {
+      quoteId: 'testQuoteId',
+      productName: 'testProductName',
+    },
+  },
+};
 
 class MockDomSanitizer {
   bypassSecurityTrustUrl() {
@@ -12,7 +22,12 @@ class MockDomSanitizer {
   sanitize() {}
 }
 
-class MockRoutingService {}
+class MockRoutingService {
+  getRouterState() {
+    return of(mockRoutingData);
+  }
+  go() {}
+}
 
 describe('ApplicationConfirmationComponent', () => {
   let component: ApplicationConfirmationComponent;
@@ -34,9 +49,22 @@ describe('ApplicationConfirmationComponent', () => {
     fixture = TestBed.createComponent(ApplicationConfirmationComponent);
     component = fixture.componentInstance;
     routingService = TestBed.inject(RoutingService);
+    localStorage.setItem('bankingApplicationPrice', '€100');
   });
 
   it('should create', () => {
     expect(component).toBeDefined();
+  });
+
+  it('should navigate to quote details page', () => {
+    spyOn(routingService, 'go').and.callThrough();
+    component.navigateQuoteDetails();
+    expect(routingService.go).toHaveBeenCalled();
+  });
+
+  it('should navigate to inbox page', () => {
+    spyOn(routingService, 'go').and.callThrough();
+    component.navigateInbox();
+    expect(routingService.go).toHaveBeenCalled();
   });
 });
