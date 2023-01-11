807f164c614f2166ce6fa9504b38d4b19d4ee583
micovicn <91135017+micovicn@users.noreply.github.com>
2023-01-11 14:37:15 +0100

    CXFSA-1159: Policy service test fixed

--- projects/fsastorefrontlib/src/core/checkout/guards/application-confirmation.guard.spec.ts
+++ projects/fsastorefrontlib/src/core/checkout/guards/application-confirmation.guard.spec.ts
@@ -29,13 +29,12 @@ describe(`ApplicationConfirmationGuard`, () => {
   });
 
   it(`should redirect to homepage when application checkout is not active `, () => {
-    localStorage.setItem('applicationConfirmation', 'false');
+    spyOn(localStorage, 'getItem').and.returnValue('false');
     guard.canActivate();
     expect(routing.go).toHaveBeenCalled();
   });
 
   it(`should not redirect to the homepage when application checkout is active `, () => {
-    localStorage.setItem('applicationConfirmation', 'true');
     guard.canActivate();
     expect(routing.go).not.toHaveBeenCalled();
   });
