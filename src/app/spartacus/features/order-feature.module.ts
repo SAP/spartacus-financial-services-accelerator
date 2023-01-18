3916897cb7ecb184cfdacaa15fc87a5fcd497df1
Filip Hadzi-Ristic <filip.hadzi.ristic@msg-global.com>
2023-01-18 11:38:28 +0100

    CXFSA-1082: Expanded OrdersFeatureModule for translations

--- src/app/spartacus/features/order-feature.module.ts
+++ src/app/spartacus/features/order-feature.module.ts
@@ -1,19 +1,30 @@
 import { NgModule } from '@angular/core';
-import { provideConfig } from '@spartacus/core';
 import { ORDER_FEATURE } from '@spartacus/order/core';
 import { OrderRootModule } from '@spartacus/order/root';
+import { CmsConfig, I18nConfig, provideConfig } from '@spartacus/core';
+import {
+  orderTranslationChunksConfig,
+  orderTranslations,
+} from '@spartacus/order/assets';
 
 @NgModule({
   imports: [OrderRootModule],
   providers: [
-    provideConfig({
+    provideConfig(<CmsConfig>{
       featureModules: {
         [ORDER_FEATURE]: {
-          module: () =>
-            import('@spartacus/order').then((m) => m.OrderModule),
+          module: () => import('@spartacus/order').then((m) => m.OrderModule),
         },
+      },
+    }),
+    provideConfig(<I18nConfig>{
+      i18n: {
+        resources: orderTranslations,
+        chunks: orderTranslationChunksConfig,
+        fallbackLang: 'en',
       },
     }),
   ],
 })
-export class OrderFeatureModule { }
+export class OrderFeatureModule {}
+
