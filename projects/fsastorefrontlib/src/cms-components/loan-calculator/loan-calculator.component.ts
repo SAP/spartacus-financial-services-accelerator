19aa0a24c1647cfacbd6334ba7b69409d3713daa
velibor_k <velibor.krivokuca@msg-global.com>
2023-01-19 12:07:23 +0100

    Refactored ternary operator

--- projects/fsastorefrontlib/src/cms-components/loan-calculator/loan-calculator.component.ts
+++ projects/fsastorefrontlib/src/cms-components/loan-calculator/loan-calculator.component.ts
@@ -92,35 +92,41 @@ export class LoanCalculatorComponent implements OnInit, OnDestroy {
 
   changeLoanDuration(change: string, min: number, max: number) {
     if (change === 'decrease') {
-      this.sliderForm.get('loanDuration').value - 1 >= min
-        ? this.sliderForm.patchValue({
-            loanDuration: this.sliderForm.get('loanDuration').value - 1,
-          })
-        : false;
+      if (this.sliderForm.get('loanDuration').value - 1 >= min) {
+        this.sliderForm.patchValue({
+          loanDuration: this.sliderForm.get('loanDuration').value - 1,
+        });
+      }
     } else {
-      this.sliderForm.get('loanDuration').value + 1 <= max
-        ? this.sliderForm.patchValue({
-            loanDuration: this.sliderForm.get('loanDuration').value + 1,
-          })
-        : false;
+      if (this.sliderForm.get('loanDuration').value + 1 <= max) {
+        this.sliderForm.patchValue({
+          loanDuration: this.sliderForm.get('loanDuration').value + 1,
+        });
+      }
     }
   }
 
   changeLoanAmount(change: string, min: number, max: number) {
     if (change === 'decrease') {
-      this.sliderForm.get('loanAmount').value - this.inputRangeSteps >= min
-        ? this.sliderForm.patchValue({
-            loanAmount:
-              this.sliderForm.get('loanAmount').value - this.inputRangeSteps,
-          })
-        : false;
+      if (
+        this.sliderForm.get('loanAmount').value - this.inputRangeSteps >=
+        min
+      ) {
+        this.sliderForm.patchValue({
+          loanAmount:
+            this.sliderForm.get('loanAmount').value - this.inputRangeSteps,
+        });
+      }
     } else {
-      this.sliderForm.get('loanAmount').value + this.inputRangeSteps <= max
-        ? this.sliderForm.patchValue({
-            loanAmount:
-              this.sliderForm.get('loanAmount').value + this.inputRangeSteps,
-          })
-        : false;
+      if (
+        this.sliderForm.get('loanAmount').value + this.inputRangeSteps <=
+        max
+      ) {
+        this.sliderForm.patchValue({
+          loanAmount:
+            this.sliderForm.get('loanAmount').value + this.inputRangeSteps,
+        });
+      }
     }
   }
 
