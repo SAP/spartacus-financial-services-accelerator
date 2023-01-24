15b994752cad54b1691c2ef9ad1b088be107612f
velibor_k <velibor.krivokuca@msg-global.com>
2023-01-24 12:43:19 +0100

    Added DE translations

---
+++ projects/fsastorefrontlib/src/assets/translations/de/loan.de.ts
@@ -0,0 +1,12 @@
+export const loan = {
+  loan: {
+    loanCalculatorTitle: '[DE] Loan Calculator',
+    loanAmount: '[DE] Choose Loan Amount',
+    loanRepaymentFrequency: '[DE] Choose Repayment Frequency',
+    loanDuration: '[DE] Choose Loan Duration',
+    loanInterestRate: '[DE] Interest Loan Rate',
+    loanCalculate: '[DE] Calculate',
+    loanAnnuity: '[DE] Annuity',
+    clearAll: '[DE] Clear All',
+  },
+};
