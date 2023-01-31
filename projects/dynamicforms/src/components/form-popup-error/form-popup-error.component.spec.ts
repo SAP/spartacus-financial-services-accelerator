daf4faa361f06f4bf8c7031986911f66cf31e28f
Filip Hadzi-Ristic <filip.hadzi.ristic@msg-global.com>
2023-01-31 14:12:39 +0100

    CXFSA-1082: Fixed form-popup-error component test

--- projects/dynamicforms/src/components/form-popup-error/form-popup-error.component.spec.ts
+++ projects/dynamicforms/src/components/form-popup-error/form-popup-error.component.spec.ts
@@ -5,7 +5,8 @@ import { I18nTestingModule, LanguageService } from '@spartacus/core';
 import { FormComponentService } from '../form-component.service';
 import { FormPopupErrorComponent } from './form-popup-error.component';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
-import { LaunchDialogService } from '@spartacus/storefront';
+import { LaunchDialogService, LAUNCH_CALLER } from '@spartacus/storefront';
+import { ElementRef, ViewContainerRef } from '@angular/core';
 
 class MockLanguageService {
   getActive() {
@@ -20,6 +21,17 @@ export class MockNgbModalRef {
 export class MockFormComponentService {
   isPopulatedFormInvalidSource = new BehaviorSubject<boolean>(true);
   isPopulatedFormInvalid = of(false);
+}
+
+class MockLaunchDialogService implements Partial<LaunchDialogService> {
+  openDialog(
+    _caller: LAUNCH_CALLER,
+    _openElement?: ElementRef,
+    _vcr?: ViewContainerRef
+  ) {
+    return of();
+  }
+  closeDialog(_reason: string): void {}
 }
 
 describe('FormPopupErrorComponent', () => {
@@ -46,6 +58,10 @@ describe('FormPopupErrorComponent', () => {
           {
             provide: FormComponentService,
             useValue: mockFormComponentService,
+          },
+          {
+            provide: LaunchDialogService,
+            useClass: MockLaunchDialogService,
           },
           {
             provide: NgbActiveModal,
