e84b5f6c4dff1853835ed06cb0ebc67c178f8dc3
velibor_k <velibor.krivokuca@msg-global.com>
2023-02-23 13:20:18 +0100

    Reverted env base url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ import { Environment } from './models/environment.model';
 
 export const environment: Environment = {
   production: false,
-  occBaseUrl: 'https://api.c5qs19f9s2-financial1-s3-public.model-t.myhybris.cloud',
+  occBaseUrl: 'https://financialservices.local:9002',
   digitalPayments: false,
 };
